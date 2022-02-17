2717c1952cf84525f2e5018f934d1530e76c75cf
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-02-18 00:06:49 +0300

    BlogApp.Core.Entities.Base BaseEntity created

---
+++ BlogApp.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Core.Entities.Abstract;
+using BlogApp.Core.Entities.Enums;
+
+namespace BlogApp.Core.Entities.Base;
+
+public abstract class BaseEntity : IEntity, ICreatedEntity, IModifiedEntity, IDeletedEntity
+{
+    public Guid Id { get; set; }
+    public Status Status { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Guid? ModifiedBy { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public Guid? DeletedBy { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    public int AutoId { get; set; }
+}

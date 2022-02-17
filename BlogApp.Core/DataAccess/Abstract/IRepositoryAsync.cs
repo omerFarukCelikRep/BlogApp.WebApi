a73044818b954442d5a94cfd37a65ce26758b182
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-02-18 00:04:03 +0300

    BlogApp.Core.DataAccess.Abstract IRepositoryAsync<> created

---
+++ BlogApp.Core/DataAccess/Abstract/IRepositoryAsync.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Core.Entities.Base;
+using System.Linq.Expressions;
+
+namespace BlogApp.Core.DataAccess.Abstract;
+public interface IRepositoryAsync<TEntity> where TEntity : BaseEntity
+{
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> expression);
+    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task<TEntity> AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}

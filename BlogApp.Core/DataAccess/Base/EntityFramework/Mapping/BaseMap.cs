9295900a1e2a4bcc1c057db038e4a7bd15bab730
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-02-18 00:04:24 +0300

    BlogApp.Core.DataAccess.Base.EntityFramework.Mapping BaseMap<> created

---
+++ BlogApp.Core/DataAccess/Base/EntityFramework/Mapping/BaseMap.cs
@@ -0,0 +1,24 @@
+﻿using BlogApp.Core.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
+public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T: BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.HasAlternateKey(x => x.AutoId);
+
+        builder.Property(x => x.Status).IsRequired(true);
+
+        builder.Property(x => x.CreatedBy).IsRequired(true);
+        builder.Property(x => x.CreatedDate).IsRequired(true);
+        builder.Property(x => x.ModifiedBy).IsRequired(false);
+        builder.Property(x => x.ModifiedDate).IsRequired(false);
+        builder.Property(x => x.DeletedBy).IsRequired(false);
+        builder.Property(x => x.DeletedDate).IsRequired(false);
+
+        builder.Property(x => x.AutoId).ValueGeneratedOnAdd();
+    }
+}

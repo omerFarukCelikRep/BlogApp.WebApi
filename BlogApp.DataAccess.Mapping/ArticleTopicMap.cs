9d312f87d21759e61865d1e445766e6dd99a1012
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-02-18 00:07:45 +0300

    BlogApp.DataAccess.Mapping map classes added

---
+++ BlogApp.DataAccess.Mapping/ArticleTopicMap.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
+using BlogApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.DataAccess.Mapping;
+public class ArticleTopicMap : BaseMap<ArticleTopic>
+{
+    public override void Configure(EntityTypeBuilder<ArticleTopic> builder)
+    {
+        base.Configure(builder);
+
+        builder.HasOne(x => x.Article).WithMany(x => x.ArticleTopics).HasForeignKey(x => x.ArticleId);
+        builder.HasOne(x => x.Topic).WithMany(x => x.ArticleTopics).HasForeignKey(x => x.TopicId);
+    }
+}
---
+++ BlogApp.DataAccess.Mapping/MemberMap.cs
@@ -0,0 +1,18 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
+using BlogApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.DataAccess.Mapping;
+public class MemberMap : BaseMap<Member>
+{
+    public override void Configure(EntityTypeBuilder<Member> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.Biography).IsRequired(false);
+        builder.Property(x => x.ProfilePicture).IsRequired(false);
+        builder.Property(x => x.Url).IsRequired(false);
+        builder.Property(x => x.IdentityId).IsRequired();
+    }
+}

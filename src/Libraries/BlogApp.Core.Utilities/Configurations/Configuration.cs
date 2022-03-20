cc42b64080042fe18f9df5c2e1e9bc875d6ba1f1
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:51:50 +0300

    BlogApp.Core.Utilities.Configurations Configuration added

---
+++ src/Libraries/BlogApp.Core.Utilities/Configurations/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BlogApp.Core.Utilities.Configurations;
+public class Configuration
+{
+    private static ConfigurationManager ConfigurationManager {
+        get
+        {
+            ConfigurationManager configurationManager = new();
+
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Libraries/BlogApp.API"));
+            configurationManager.AddJsonFile("appsettings.Development.json");
+
+            return configurationManager;
+        }
+    }
+
+    public static string GetConnectionString(string connection)
+    {
+        return ConfigurationManager.GetConnectionString(connection);
+    }
+
+    public static string GetValue(string section)
+    {
+        return ConfigurationManager.GetSection(section).Value;
+    }
+}

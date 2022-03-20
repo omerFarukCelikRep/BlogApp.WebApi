d4d0b7c7de2b3b5a184fc12e03975a863c50c6cf
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:40:14 +0300

    BlogApp.Entities.Dtos.Members MemberDto added

---
+++ src/Libraries/BlogApp.Entities.Dtos/Members/MemberDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Entities.Dtos.Members;
+public class MemberDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Biography { get; set; }
+    public byte[]? ProfilePicture { get; set; }
+    public string? Url { get; set; }
+}

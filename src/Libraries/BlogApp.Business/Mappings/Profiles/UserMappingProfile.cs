8dd24371f5fc766e914daaf982802ee019a2505f
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:02:46 +0300

    BlogApp.Business.Mappings.Profiles UserMappingProfile added

---
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/UserMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BlogApp.Authentication.Dtos.Incoming;
+using BlogApp.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogApp.Business.Mappings.Profiles;
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<UserRegistrationRequestDto, IdentityUser<Guid>>()
+            .AfterMap((s,d) => d.EmailConfirmed = true)
+            .AfterMap((s,d) => d.UserName = s.Email);
+
+        CreateMap<UserRegistrationRequestDto, Member>();
+    }
+}

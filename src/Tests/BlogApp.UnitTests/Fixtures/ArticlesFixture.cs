6d1512789e8c69a4014684ad3cfc857d50358b54
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:30:51 +0300

    moved to ./src/Tests

---
+++ src/Tests/BlogApp.UnitTests/Fixtures/ArticlesFixture.cs
@@ -0,0 +1,51 @@
+﻿using BlogApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace BlogApp.UnitTests.Fixtures;
+public static class ArticlesFixture
+{
+    public static List<Article> GetTestArticles() => new()
+    {
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Article 1",
+            Content = "Test Article 1 Content",
+            ReadTime = 10,
+            MemberId = Guid.NewGuid()
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Article 2",
+            Content = "Test Article 2 Content",
+            ReadTime = 10,
+            MemberId = Guid.NewGuid()
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Article 3",
+            Content = "Test Article 3 Content",
+            ReadTime = 10,
+            MemberId = Guid.NewGuid()
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Article 4",
+            Content = "Test Article 4 Content",
+            ReadTime = 10,
+            MemberId = Guid.NewGuid()
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Article 5",
+            Content = "Test Article 5 Content",
+            ReadTime = 10,
+            MemberId = Guid.NewGuid()
+        }
+    };
+}
---
+++ src/Tests/BlogApp.UnitTests/Systems/Repositories/TestArticleRepository.cs
@@ -0,0 +1,28 @@
+﻿using BlogApp.Entities.Concrete;
+using BlogApp.UnitTests.Fixtures;
+using BlogApp.UnitTests.Helpers;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlogApp.UnitTests.Systems.Repositories;
+
+public class TestArticleRepository
+{
+    [Fact]
+    public async Task GetAll_WhenCalled_InvokesHttpGetRequest()
+    {
+        //Arrange
+        var expectedResponse = ArticlesFixture.GetTestArticles();
+        var endpoint = "https://example.com/articles";
+
+        var handlerMock = MockHttpMessageHandler<Article>.SetupBasicGetResourceList(expectedResponse);
+
+        var httpClient = new HttpClient(handlerMock.Object);
+
+        var config = Options.Create(endpoint);
+
+        //var sut = new 
+    }
+}

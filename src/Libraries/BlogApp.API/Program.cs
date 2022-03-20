cbc6911a4731eb991611e798483d1a46380c0298
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:29:27 +0300

    DataAccessServices, AuthenticationServices, BusinessServices and Versioning added

---
+++ src/Libraries/BlogApp.API/Program.cs
@@ -0,0 +1,46 @@
+﻿using BlogApp.API.Extensions;
+using BlogApp.Authentication;
+using BlogApp.Authentication.Configurations;
+using BlogApp.Business;
+using BlogApp.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("Jwt"));
+
+builder.Services.AddDataAccessServices();
+
+builder.Services.AddAuthenticationServices();
+
+builder.Services.AddBusinessServices();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCustomVersioning();
+//TODO: Özel ayarlara bakılacak
+builder.Services.AddHealthChecks();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseHealthChecks("/health");
+
+app.MapControllers();
+
+app.Run();

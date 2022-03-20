78e65deda28ca93ea64412d22d5105d4bc2b53e0
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:24:41 +0300

    BlogApp.Authentication AuthenticationServiceRegistration added

---
+++ src/Libraries/BlogApp.Authentication/AuthenticationServiceRegistration.cs
@@ -0,0 +1,55 @@
+﻿using BlogApp.Authentication.Services.Abstract;
+using BlogApp.Authentication.Services.Concrete;
+using BlogApp.Core.Utilities.Configurations;
+using BlogApp.DataAccess.Contexts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BlogApp.Authentication;
+public static class AuthenticationServiceRegistration
+{
+    public static void AddAuthenticationServices(this IServiceCollection services)
+    {
+        var secret = Configuration.GetValue("Jwt:Secret");
+
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false, //TODO:Update this later
+            ValidateAudience = false, //TODO:Update this later
+            RequireExpirationTime = false, //TODO: Update this later
+            ValidateLifetime = true
+        };
+
+        services.AddSingleton(tokenValidationParameters);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = tokenValidationParameters;
+            });
+
+        services.AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.SignIn.RequireConfirmedAccount = true;
+        })
+            .AddEntityFrameworkStores<BlogAppDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<ITokenService, TokenService>();
+    }
+}

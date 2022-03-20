13783a13559c30abebd00481012ecfde5e1ab8df
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:02:59 +0300

    BlogApp.Business BusinessServiceRegistration added

---
+++ src/Libraries/BlogApp.Business/BusinessServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Business.Abstract;
+using BlogApp.Business.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.Business;
+public static class BusinessServiceRegistration
+{
+    public static void AddBusinessServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+    }
+}

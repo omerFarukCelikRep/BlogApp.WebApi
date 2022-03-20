b3bf97fc27a6b24dfb6599d469a4f63c0e779b7b
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:56:27 +0300

    BlogApp.Business.Abstract IUserService added

---
+++ src/Libraries/BlogApp.Business/Abstract/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Authentication.Dtos.Generic;
+using BlogApp.Authentication.Dtos.Incoming;
+using BlogApp.Core.Utilities.Results.Abstract;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogApp.Business.Abstract;
+public interface IUserService
+{
+    Task<IDataResult<TokenData>> AddAsync(UserRegistrationRequestDto registrationRequestDto);
+    Task<IResult> CheckPasswordAsync(IdentityUser<Guid> user, string password);
+    Task<IDataResult<IdentityUser<Guid>>> FindByEmailAsync(string email);
+}

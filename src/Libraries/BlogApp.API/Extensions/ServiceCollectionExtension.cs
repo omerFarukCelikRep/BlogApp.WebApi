80787bc49526652d604567b33a54d3d6b8a839e7
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:28:10 +0300

    BlogApp.API.Extensions ServiceCollectionExtension api versioning added

---
+++ src/Libraries/BlogApp.API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogApp.API.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddCustomVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+        });
+    }
+}

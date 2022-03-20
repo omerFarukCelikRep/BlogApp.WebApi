4058200e56c8c77febfebb23fcea91fc9bb9aca6
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:56:10 +0300

    BlogApp.Business.Abstract ITopicService added

---
+++ src/Libraries/BlogApp.Business/Abstract/ITopicService.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.Core.Utilities.Results.Concrete;
+using BlogApp.Entities.Concrete;
+using BlogApp.Entities.Dtos.Topics;
+using System.Linq.Expressions;
+
+namespace BlogApp.Business.Abstract;
+public interface ITopicService
+{
+    Task<DataResult<IEnumerable<ListTopicDto>>> GetAllAsync();
+    Task<DataResult<IEnumerable<ListTopicDto>>> GetAllAsync(Expression<Func<Topic, bool>> expression);
+    Task<DataResult<TopicDto>> GetAsync(Expression<Func<Topic, bool>> expression);
+    Task<DataResult<TopicDto>> GetByIdAsync(Guid id);
+    Task<DataResult<TopicDto>> AddAsync(CreateTopicDto createDto);
+    Task<DataResult<TopicDto>> UpdateAsync(UpdateTopicDto updateDto);
+    Task<Result> DeleteAsync(Guid id);
+}

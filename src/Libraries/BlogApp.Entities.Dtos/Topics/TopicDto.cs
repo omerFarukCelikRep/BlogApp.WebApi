c5245dc255b69af65bf74bec4dcb0631d16acc38
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:41:56 +0300

    BlogApp.Entities.Dtos.Topics => ListTopicDto, TopicDto, UpdateTopicDto added

---
+++ src/Libraries/BlogApp.Entities.Dtos/Topics/TopicDto.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogApp.Entities.Dtos.Topics;
+public class TopicDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public byte[]? Thumbnail { get; set; }
+}

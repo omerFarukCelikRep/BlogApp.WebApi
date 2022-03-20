3ff4b7a8166ce60bb7ef0bb9bc6a75be35cc9b2c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:42:57 +0300

    BlogApp.DataAccess.Mapping mapping classes added

---
+++ src/Libraries/BlogApp.DataAccess.Mapping/MemberFollowedTopicMap.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
+using BlogApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.DataAccess.Mapping;
+public class MemberFollowedTopicMap : BaseMap<MemberFollowedTopic>
+{
+    public override void Configure(EntityTypeBuilder<MemberFollowedTopic> builder)
+    {
+        base.Configure(builder);
+
+        builder.HasOne(x => x.Member).WithMany(x => x.MemberFollowedTopics).HasForeignKey(x => x.MemberId);
+        builder.HasOne(x => x.Topic).WithMany(x => x.MemberFollowedTopics).HasForeignKey(x => x.TopicId);
+    }
+}

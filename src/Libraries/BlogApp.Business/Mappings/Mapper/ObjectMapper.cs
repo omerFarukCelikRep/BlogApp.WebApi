846ddaac32ef111dd605f67aace41ea4c790cbe2
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:02:00 +0300

    BlogApp.Business.Mappings.Mapper ObjectMapper added

---
+++ src/Libraries/BlogApp.Business/Mappings/Mapper/ObjectMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlogApp.Business.Mappings.Profiles;
+
+namespace BlogApp.Business.Mappings.Mapper;
+public static class ObjectMapper
+{
+    private static readonly Lazy<IMapper> _mapper = new Lazy<IMapper>(() =>
+    {
+        var configuration = new MapperConfiguration(config =>
+        {
+            config.AddProfile<TopicMappingProfile>();
+            config.AddProfile<UserMappingProfile>();
+        });
+
+        return configuration.CreateMapper();
+    });
+
+    public static IMapper Mapper => _mapper.Value;
+}

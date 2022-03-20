733c35b36e203c3001fae89112db22dfb2e4afcf
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:02:31 +0300

    BlogApp.Business.Mappings.Profiles TopicMappingProfile added

---
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/TopicMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlogApp.Entities.Concrete;
+using BlogApp.Entities.Dtos.Topics;
+
+namespace BlogApp.Business.Mappings.Profiles;
+
+public class TopicMappingProfile : Profile
+{
+    public TopicMappingProfile()
+    {
+        CreateMap<CreateTopicDto, Topic>().ReverseMap();
+
+        CreateMap<UpdateTopicDto, Topic>().ReverseMap();
+
+        CreateMap<ListTopicDto, Topic>().ReverseMap();
+
+        CreateMap<DetailsTopicDto, Topic>().ReverseMap();
+
+        CreateMap<TopicDto, Topic>().ReverseMap();
+    }
+}

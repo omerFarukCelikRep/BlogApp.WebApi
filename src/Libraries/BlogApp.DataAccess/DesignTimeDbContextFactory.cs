aede20916c0f60a20e7a8dfac6d1b1126cf324b3
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 01:54:24 +0300

    BlogApp.DataAccess DesignTimeDbContextFactory added

---
+++ src/Libraries/BlogApp.DataAccess/DesignTimeDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Core.Utilities.Configurations;
+using BlogApp.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BlogApp.DataAccess;
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BlogAppDbContext>
+{
+    public BlogAppDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<BlogAppDbContext> optionsBuilder = new();
+
+        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+        return new(optionsBuilder.Options);
+    }
+}

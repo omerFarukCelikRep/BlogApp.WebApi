128a4b0232f72dc04e50a2103416164dfdd89498
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:25:08 +0300

    BlogApp.API.Controllers.v1 BaseController added

---
+++ src/Libraries/BlogApp.API/Controllers/v1/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.Core.Utilities.Results.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers.v1;
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+public class BaseController : ControllerBase
+{
+    protected Core.Utilities.Results.Abstract.IResult PopulateError(string message)
+    {
+        return new ErrorResult(message);
+    }
+}

66f83d7c1e763c1ea771b3b5392507e02430dd1d
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-21 02:27:47 +0300

    BlogApp.API.Controllers.v1 TopicsController GetAll, GetById and Create actions added

---
+++ src/Libraries/BlogApp.API/Controllers/v1/TopicsController.cs
@@ -0,0 +1,70 @@
+﻿using BlogApp.Business.Abstract;
+using BlogApp.Entities.Dtos.Topics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers.v1;
+public class TopicsController : BaseController
+{
+    private readonly ITopicService _topicService;
+
+    public TopicsController(ITopicService topicService)
+    {
+        _topicService = topicService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _topicService.GetAllAsync();
+
+        if (!result.IsSuccess)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id}", Name = "GetByIdAsync")]
+    public async Task<IActionResult> GetByIdAsync(Guid id)
+    {
+        var result = await _topicService.GetByIdAsync(id);
+
+        if (!result.IsSuccess)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateTopicDto createTopicDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(createTopicDto);
+        }
+
+        var result = await _topicService.AddAsync(createTopicDto);
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result);
+        }
+
+        return CreatedAtRoute(nameof(GetByIdAsync), new { Id = result.Data.Id}, result);
+    }
+
+    //[HttpPut("{id}")]
+    //public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateTopicDto updateTopicDto)
+    //{
+    //    var topic = await _topicService.GetByIdAsync(id);
+
+    //    if (topic is { Length > 0})
+    //    {
+
+    //    }
+    //}
+}

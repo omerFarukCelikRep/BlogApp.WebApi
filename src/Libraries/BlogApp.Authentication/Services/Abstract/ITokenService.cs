ee7f9de8ed98b6ae58d6bc16a29803d16660109a
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-26 23:47:14 +0300

    GenerateJwtToken return type changed to AuthResult, UpdateToken and VerifyToken methods added

--- src/Libraries/BlogApp.Authentication/Services/Abstract/ITokenService.cs
+++ src/Libraries/BlogApp.Authentication/Services/Abstract/ITokenService.cs
@@ -1,8 +1,11 @@
-﻿using BlogApp.Authentication.Dtos.Generic;
+﻿using BlogApp.Authentication.Dtos.Incoming;
+using BlogApp.Authentication.Dtos.Outgoing;
 using Microsoft.AspNetCore.Identity;
 
 namespace BlogApp.Authentication.Services.Abstract;
 public interface ITokenService
 {
-    Task<TokenData> GenerateJwtToken(IdentityUser<Guid> user);
+    Task<AuthResult> GenerateJwtToken(IdentityUser<Guid> user);
+    Task<AuthResult> UpdateToken(string refreshToken);
+    Task<AuthResult?> VerifyToken(TokenRequestDto tokenRequestDto);
 }

46eb6e015109e148e2f6b3e0bbc18e3f130a5915
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-26 23:54:05 +0300

    GetByRefreshTokenAsync  and UpdateRefreshTokenAsUsed methods added

--- src/Libraries/BlogApp.DataAccess/Repositories/RefreshTokenRepository.cs
+++ src/Libraries/BlogApp.DataAccess/Repositories/RefreshTokenRepository.cs
@@ -2,9 +2,48 @@
 using BlogApp.DataAccess.Abstract;
 using BlogApp.DataAccess.Contexts;
 using BlogApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.DataAccess.Repositories;
 public class RefreshTokenRepository : EfBaseRepository<RefreshToken, BlogAppDbContext>, IRefreshTokenRepository
 {
     public RefreshTokenRepository(BlogAppDbContext context) : base(context) { }
+
+    public async Task<RefreshToken?> GetByRefreshTokenAsync(string refreshToken)
+    {
+        try
+        {
+            return await _table.Where(x => x.Token.ToLower() == refreshToken.ToLower())
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync();
+        }
+        catch (Exception)
+        {
+            //TODO: Add logger
+            return null;
+        }
+    }
+
+    public async Task<bool> UpdateRefreshTokenAsUsed(RefreshToken refreshToken)
+    {
+        try
+        {
+            var token = await _table.Where(x => x.Token.ToLower() == refreshToken.Token.ToLower())
+                                    .AsNoTracking()
+                                    .FirstOrDefaultAsync();
+
+            if (token == null) return false;
+
+            token.IsUsed = true;
+
+            _ = await UpdateAsync(token);
+            return true;
+        }
+        catch (Exception)
+        {
+            //TODO:Add Logger
+            //TODO: Throw
+            return false;
+        }
+    }
 }

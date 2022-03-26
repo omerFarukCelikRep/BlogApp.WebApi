82db2dc041c474e7a58c1e1c7303fa0b1b7c5650
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-03-26 23:53:46 +0300

    GetByRefreshTokenAsync and UpdateRefreshTokenAsUsed methods added

--- src/Libraries/BlogApp.DataAccess/Abstract/IRefreshTokenRepository.cs
+++ src/Libraries/BlogApp.DataAccess/Abstract/IRefreshTokenRepository.cs
@@ -4,4 +4,6 @@ using BlogApp.Entities.Concrete;
 namespace BlogApp.DataAccess.Abstract;
 public interface IRefreshTokenRepository : IRepositoryAsync<RefreshToken>
 {
+    Task<RefreshToken?> GetByRefreshTokenAsync(string refreshToken);
+    Task<bool> UpdateRefreshTokenAsUsed(RefreshToken refreshToken);
 }

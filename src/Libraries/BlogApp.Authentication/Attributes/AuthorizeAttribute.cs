33f58fc29ae94a4655f208d3ecb2b974b1095ee8
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:19:04 +0300

    BlogApp.Authentication.Attributes AuthorizeAttribute added

---
+++ src/Libraries/BlogApp.Authentication/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BlogApp.Authentication.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute : Attribute
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+
+        if (allowAnonymous)
+        {
+            return;
+        }
+
+        var user = (IdentityUser<Guid>)context.HttpContext.Items["User"];
+
+        if (user == null)
+        {
+            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+    }
+}

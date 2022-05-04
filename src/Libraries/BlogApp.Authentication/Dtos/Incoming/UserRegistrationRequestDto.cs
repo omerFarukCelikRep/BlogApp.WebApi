3771b891546322e127099aa7b332cc2a0e72db75
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:19:38 +0300

    IpAddress property added

--- src/Libraries/BlogApp.Authentication/Dtos/Incoming/UserRegistrationRequestDto.cs
+++ src/Libraries/BlogApp.Authentication/Dtos/Incoming/UserRegistrationRequestDto.cs
@@ -6,4 +6,5 @@ public class UserRegistrationRequestDto
     public string Email { get; set; }
     public string Password { get; set; }
     public string ConfirmedPassword { get; set; }
+    public string IpAddress { get; set; }
 }

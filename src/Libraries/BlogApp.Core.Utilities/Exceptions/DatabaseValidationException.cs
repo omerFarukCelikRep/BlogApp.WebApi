098cb0a127e664363036d0dc24bc92f5eaac346f
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:42:53 +0300

    BlogApp.Core.Utilities.Exceptions  DatabaseValidationException added

---
+++ src/Libraries/BlogApp.Core.Utilities/Exceptions/DatabaseValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace BlogApp.Core.Utilities.Exceptions;
+public class DatabaseValidationException : Exception
+{
+    public DatabaseValidationException() :base() { }
+
+    public DatabaseValidationException(string? message) : base(message) { }
+
+    public DatabaseValidationException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    public DatabaseValidationException(string message, params object[] args) : base(String.Format(CultureInfo.CurrentCulture, message, args)) { }
+
+    protected DatabaseValidationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}

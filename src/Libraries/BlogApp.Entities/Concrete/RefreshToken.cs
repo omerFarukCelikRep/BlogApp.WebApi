4fb2f8d5a27f72618031937eca5b35f31d8fc1d5
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:49:59 +0300

    RevokedDate, CreatedByIp, IsExpired, IsRevoked and IsActive properties added

--- src/Libraries/BlogApp.Entities/Concrete/RefreshToken.cs
+++ src/Libraries/BlogApp.Entities/Concrete/RefreshToken.cs
@@ -2,15 +2,36 @@
 using Microsoft.AspNetCore.Identity;
 
 namespace BlogApp.Entities.Concrete;
+
 public class RefreshToken : BaseEntity
 {
     public string? Token { get; set; }
-    public string? JwtId { get; set; }
-    public bool IsUsed { get; set; }
-    public bool IsRevoked { get; set; }
     public DateTime ExpiryDate { get; set; }
+    public DateTime? RevokedDate { get; set; }
+    public string? CreatedByIp { get; set; }
+    public bool IsExpired
+    {
+        get
+        {
+            return DateTime.UtcNow >= ExpiryDate;
+        }
+    }
+    public bool IsRevoked
+    {
+        get
+        {
+            return RevokedDate != null;
+        }
+    }
+    public bool IsActive
+    {
+        get
+        {
+            return RevokedDate == null && !IsExpired;
+        }
+    }
 
     //Navigation Prop.
-    public Guid? UserId { get; set; }
+    public Guid UserId { get; set; }
     public virtual IdentityUser<Guid>? User { get; set; }
 }

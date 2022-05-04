9e21bdc4ea684532414e40f3825c04c93db43b92
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:44:01 +0300

    GetByIdentityId method added

--- src/Libraries/BlogApp.DataAccess/Repositories/MemberRepository.cs
+++ src/Libraries/BlogApp.DataAccess/Repositories/MemberRepository.cs
@@ -2,9 +2,15 @@
 using BlogApp.DataAccess.Abstract;
 using BlogApp.DataAccess.Contexts;
 using BlogApp.Entities.Concrete;
+using Microsoft.Extensions.Logging;
 
 namespace BlogApp.DataAccess.Repositories;
 public class MemberRepository : EfBaseRepository<Member, BlogAppDbContext>, IMemberRepository
 {
-    public MemberRepository(BlogAppDbContext context) : base(context) { }
+    public MemberRepository(BlogAppDbContext context, ILogger<MemberRepository> logger) : base(context, logger) { }
+
+    public async Task<Member> GetByIdentityId(Guid identityId)
+    {
+        return await GetAsync(x => x.IdentityId == identityId);
+    }
 }

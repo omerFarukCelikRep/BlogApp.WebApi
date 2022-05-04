919e0a37d2ac041731731b3c6a19e4a65be48df8
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:26:00 +0300

    AddAsync return type changed, CheckPasswordAsync method removed, AuthenticateAsync method added, RefreshTokenAsync method added

--- src/Libraries/BlogApp.Business/Abstract/IUserService.cs
+++ src/Libraries/BlogApp.Business/Abstract/IUserService.cs
@@ -1,12 +1,13 @@
-﻿using BlogApp.Authentication.Dtos.Generic;
-using BlogApp.Authentication.Dtos.Incoming;
+﻿using BlogApp.Authentication.Dtos.Incoming;
+using BlogApp.Authentication.Dtos.Outgoing;
 using BlogApp.Core.Utilities.Results.Abstract;
 using Microsoft.AspNetCore.Identity;
 
 namespace BlogApp.Business.Abstract;
 public interface IUserService
 {
-    Task<IDataResult<TokenData>> AddAsync(UserRegistrationRequestDto registrationRequestDto);
-    Task<IResult> CheckPasswordAsync(IdentityUser<Guid> user, string password);
+    Task<AuthResult> AddAsync(UserRegistrationRequestDto registrationRequestDto);
+    Task<AuthResult> AuthenticateAsync(UserLoginRequestDto loginRequestDto, string ipAddress);
     Task<IDataResult<IdentityUser<Guid>>> FindByEmailAsync(string email);
+    Task<AuthResult> RefreshTokenAsync(TokenRequestDto tokenRequestDto);
 }

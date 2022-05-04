46646b75bda5f7f7a8542265cbe4d0047400b9dc
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:30:07 +0300

    BlogApp.Business.Validations.TopicValidators UpdateTopicValidator added

---
+++ src/Libraries/BlogApp.Business/Validations/TopicValidators/UpdateTopicValidator.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Entities.Dtos.Topics;
+using FluentValidation;
+
+namespace BlogApp.Business.Validations.TopicValidators;
+public class UpdateTopicValidator : AbstractValidator<UpdateTopicDto>
+{
+    public UpdateTopicValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .WithMessage("Konu ismi boş geçilemez") //TODO: Magic string
+            .NotEmpty()
+            .WithMessage("Konu ismi boş geçilemez") //TODO: Magic string
+            .MinimumLength(3)
+            .WithMessage("Konu ismi minimum 3 karakter olmalıdır."); // TODO: Magic string
+    }
+}

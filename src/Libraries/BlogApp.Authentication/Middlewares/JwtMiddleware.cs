e3f507e7e55ccd2446c15bd642be360127ef49df
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:20:31 +0300

    BlogApp.Authentication.Middlewares JwtMiddleware added

---
+++ src/Libraries/BlogApp.Authentication/Middlewares/JwtMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Authentication.Services.Abstract;
+using Microsoft.AspNetCore.Http;
+
+namespace BlogApp.Authentication.Middlewares;
+public class JwtMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public JwtMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context, ITokenService tokenService)
+    {
+        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+        var userId = await tokenService.ValidateJwtTokenAsync(token);
+
+        if (userId == null || userId == Guid.Empty)
+        {
+            context.Items["User"] = userId;
+        }
+
+        await _next(context);
+    }
+}

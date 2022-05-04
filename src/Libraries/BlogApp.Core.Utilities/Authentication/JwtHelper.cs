8e9c6d0ec7c54fc7cd0c065600c75be3a43e5a55
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:34:49 +0300

    BlogApp.Core.Utilities.Authentication JwtHelper static class and GetUserIdByToken method added

---
+++ src/Libraries/BlogApp.Core.Utilities/Authentication/JwtHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace BlogApp.Core.Utilities.Authentication;
+public class JwtHelper
+{
+    public static Guid GetUserIdByToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var decodedToken = tokenHandler.ReadJwtToken(token);
+        return Guid.Parse(decodedToken.Claims.FirstOrDefault(x => x.Type == "Id")?.Value);
+    }
+}

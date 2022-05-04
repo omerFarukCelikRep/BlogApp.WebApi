9174264ad53f192770f7c9f34e25fbd51344c074
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:18:05 +0300

    Swagger security and HttpContextAccessor added

--- src/Libraries/BlogApp.API/Program.cs
+++ src/Libraries/BlogApp.API/Program.cs
@@ -6,6 +6,7 @@ using BlogApp.Business;
 using BlogApp.Business.Validations;
 using BlogApp.DataAccess;
 using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,11 +28,24 @@ builder.Services.AddControllers()
     });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+    });
+});
 
 builder.Services.AddCustomVersioning();
 //TODO: Özel ayarlara bakılacak
 builder.Services.AddHealthChecks();
+
+builder.Services.AddHttpContextAccessor();
 
 
 var app = builder.Build();

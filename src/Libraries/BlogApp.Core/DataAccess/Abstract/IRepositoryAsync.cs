fd76b69194a9bb11f203019ca20f4f6cb1d03e06
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:30:55 +0300

    tracking parameter added

--- src/Libraries/BlogApp.Core/DataAccess/Abstract/IRepositoryAsync.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Abstract/IRepositoryAsync.cs
@@ -4,12 +4,13 @@ using System.Linq.Expressions;
 namespace BlogApp.Core.DataAccess.Abstract;
 public interface IRepositoryAsync<TEntity> where TEntity : BaseEntity
 {
-    Task<IEnumerable<TEntity>> GetAllAsync();
-    Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> expression);
-    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false);
-    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
-    Task<TEntity> GetByIdAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetAllAsync(bool tracking = true);
+    Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> expression, bool tracking = true);
+    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression, bool tracking = true);
+    Task<TEntity> GetByIdAsync(Guid id, bool tracking = true);
     Task<TEntity> AddAsync(TEntity entity);
-    Task DeleteAsync(TEntity entity);
+    Task<bool> DeleteAsync(TEntity entity);
     Task<TEntity> UpdateAsync(TEntity entity);
+    Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression);
 }

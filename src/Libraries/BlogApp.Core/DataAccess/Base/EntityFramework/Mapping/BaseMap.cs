c4632cd18d6ba2f6646d341630a84a841eec0981
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:31:18 +0300

    ValueGeneratedOnAdd added for Id

--- src/Libraries/BlogApp.Core/DataAccess/Base/EntityFramework/Mapping/BaseMap.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Base/EntityFramework/Mapping/BaseMap.cs
@@ -3,12 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
-public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T: BaseEntity
+public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity
 {
     public virtual void Configure(EntityTypeBuilder<T> builder)
     {
         builder.HasKey(x => x.Id);
         builder.HasAlternateKey(x => x.AutoId);
+
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
 
         builder.Property(x => x.Status).IsRequired(true);
 

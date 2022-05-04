bb15971e2698e6370e647d7bdaafdec49acce2b0
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:45:00 +0300

    HttpContextAccessor added, SaveChanges methods overrided and AssignBaseProperties method added

--- src/Libraries/BlogApp.DataAccess/Contexts/BlogAppDbContext.cs
+++ src/Libraries/BlogApp.DataAccess/Contexts/BlogAppDbContext.cs
@@ -1,14 +1,23 @@
-﻿using BlogApp.DataAccess.Mapping;
+﻿using BlogApp.Core.Entities.Base;
+using BlogApp.Core.Utilities.Authentication;
+using BlogApp.DataAccess.Mapping;
 using BlogApp.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace BlogApp.DataAccess.Contexts;
 
 public class BlogAppDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
 {
-    public BlogAppDbContext(DbContextOptions<BlogAppDbContext> options) : base(options) { }
+    private readonly IHttpContextAccessor _context;
+
+    public BlogAppDbContext(DbContextOptions<BlogAppDbContext> options, IHttpContextAccessor context) : base(options)
+    {
+        _context = context;
+    }
 
     public DbSet<Article>? Articles { get; set; }
     public DbSet<ArticleTopic>? ArticleTopics { get; set; }
@@ -30,5 +39,73 @@ public class BlogAppDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRo
         builder.ApplyConfigurationsFromAssembly(typeof(IMappingMaker).Assembly);
 
         base.OnModelCreating(builder);
+    }
+
+    public override int SaveChanges()
+    {
+        AssignBaseProperties();
+        return base.SaveChanges();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        AssignBaseProperties();
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void AssignBaseProperties()
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+
+        var token = _context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+        var userId = Guid.Empty;
+
+        if (token != null)
+        {
+            userId = JwtHelper.GetUserIdByToken(token);
+        }
+
+        foreach (var entry in entries)
+        {
+            SetIfAdded(entry, userId);
+
+            SetIfModified(entry, userId);
+
+            SetIfDeleted(entry, userId);
+        }
+    }
+
+    private void SetIfDeleted(EntityEntry<BaseEntity> entry, Guid userId)
+    {
+        if (entry.State == EntityState.Deleted)
+        {
+            entry.State = EntityState.Modified;
+            entry.Entity.Status = Core.Entities.Enums.Status.Deleted;
+            entry.Entity.DeletedDate = DateTime.Now;
+            entry.Entity.DeletedBy = userId;
+        }
+    }
+
+    private void SetIfAdded(EntityEntry<BaseEntity> entry, Guid userId)
+    {
+        if (entry.State == EntityState.Added)
+        {
+            entry.Entity.CreatedBy = userId;
+            entry.Entity.CreatedDate = DateTime.Now;
+            entry.Entity.Status = Core.Entities.Enums.Status.Added;
+        }
+    }
+
+    private void SetIfModified(EntityEntry<BaseEntity> entry, Guid userId)
+    {
+        if (entry.State == EntityState.Modified)
+        {
+            entry.Entity.Status = Core.Entities.Enums.Status.Modified;
+        }
+
+        entry.Entity.ModifiedBy = userId;
+        entry.Entity.ModifiedDate = DateTime.Now;
     }
 }

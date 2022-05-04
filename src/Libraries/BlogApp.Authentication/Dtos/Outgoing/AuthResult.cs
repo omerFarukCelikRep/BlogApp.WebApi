d638c3e6c2291623a6c7702d39bf5584fb252e46
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-04 16:20:13 +0300

    constructors added

--- src/Libraries/BlogApp.Authentication/Dtos/Outgoing/AuthResult.cs
+++ src/Libraries/BlogApp.Authentication/Dtos/Outgoing/AuthResult.cs
@@ -1,6 +1,20 @@
 ﻿namespace BlogApp.Authentication.Dtos.Outgoing;
 public class AuthResult
 {
+    public AuthResult() { }
+    
+    public AuthResult(string token, string refreshToken, bool success, params string[] errors)
+    {
+        Token = token;
+        RefreshToken = refreshToken;
+        Success = success;
+
+        foreach (var error in errors)
+        {
+            Errors.Add(error);
+        }
+    }
+
     public string Token { get; set; }
     public string RefreshToken { get; set; }
     public bool Success { get; set; }

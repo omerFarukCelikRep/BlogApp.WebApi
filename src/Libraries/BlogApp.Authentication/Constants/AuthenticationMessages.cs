dee5e9729f409448938b917a3b3d469a9a7f4d44
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-07 02:41:44 +0300

    BlogApp.Authentication.Constants AuthenticationMessages added

---
+++ src/Libraries/BlogApp.Authentication/Constants/AuthenticationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Authentication.Constants;
+public class AuthenticationMessages
+{
+    public const string InvalidRequest = "Invalid authentication request";
+
+    public const string JWTTokenNotExpired = "JWT token has not expired";
+    public const string InvalidRefreshToken = "Invalid Refresh Token";
+    public const string UsedRefreshToken = "Token has been used";
+    public const string RevokedRefreshToken = "Refresh Token has been revoked, it cannot be used";
+
+    public const string EmailAlredyTaken = "Email already taken";
+}

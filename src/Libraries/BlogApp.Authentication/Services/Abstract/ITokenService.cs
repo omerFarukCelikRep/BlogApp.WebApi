e4be99c103a1b69b25d546104d7e51342d231dee
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-07 02:42:23 +0300

    GetActiveRefreshTokenAsync method added

--- src/Libraries/BlogApp.Authentication/Services/Abstract/ITokenService.cs
+++ src/Libraries/BlogApp.Authentication/Services/Abstract/ITokenService.cs
@@ -8,6 +8,7 @@ public interface ITokenService
 {
     string GenerateJwtToken(IdentityUser<Guid> user);
     Task<RefreshToken> GenerateRefreshTokenAsync(IdentityUser<Guid> user, string ipAddress);
+    Task<RefreshToken> GetActiveRefreshTokenAsync(IdentityUser<Guid> user);
     Task<bool> UpdateRefreshTokenAsUsedAsync(string token);
     Task<Guid?> ValidateJwtTokenAsync(string token);
     Task<AuthResult?> VerifyTokenAsync(TokenRequestDto tokenRequestDto);

ad33f63583f3b6130084293e9cfbd99abe39604e
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-14 01:18:56 +0300

    BlogApp.API.Controllers.v1 MembersController added

---
+++ src/Libraries/BlogApp.API/Controllers/v1/MembersController.cs
@@ -0,0 +1,27 @@
+﻿using BlogApp.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers.v1;
+
+public class MembersController : BaseController
+{
+    private readonly IMemberService _memberService;
+
+    public MembersController(IMemberService memberService)
+    {
+        _memberService = memberService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _memberService.GetAllAsync();
+
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+}

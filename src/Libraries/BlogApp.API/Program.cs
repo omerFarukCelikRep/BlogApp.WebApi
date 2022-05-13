fb3ffec3052587c8156016cad1caf4b15f0db3d7
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-14 01:20:11 +0300

    jwtconfig, swagger and validation moved to extension class

--- src/Libraries/BlogApp.API/Program.cs
+++ src/Libraries/BlogApp.API/Program.cs
@@ -1,16 +1,10 @@
 ﻿using BlogApp.API.Extensions;
 using BlogApp.API.Middlewares;
 using BlogApp.Authentication;
-using BlogApp.Authentication.Configurations;
 using BlogApp.Business;
-using BlogApp.Business.Validations;
 using BlogApp.DataAccess;
-using FluentValidation.AspNetCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("Jwt"));
 
 builder.Services.AddDataAccessServices();
 
@@ -19,27 +13,11 @@ builder.Services.AddAuthenticationServices();
 builder.Services.AddBusinessServices();
 
 builder.Services.AddControllers()
-    .AddFluentValidation(options =>
-    {
-        options.ImplicitlyValidateChildProperties = true;
-        options.ImplicitlyValidateRootCollectionElements = true;
-
-        options.RegisterValidatorsFromAssembly(typeof(IValidationMaker).Assembly);
-    });
+    .AddCustomValidation();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c=>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-    });
-});
+
+builder.Services.AddCustomSwagger();
 
 builder.Services.AddCustomVersioning();
 //TODO: Özel ayarlara bakılacak

c8f977b14c8dc8574b06781af81ed06809868e15
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-16 01:00:21 +0300

    IArticleService added

---
+++ src/Libraries/BlogApp.Business/Abstract/IArticleService.cs
@@ -0,0 +1,8 @@
+﻿using BlogApp.Core.Utilities.Results.Abstract;
+using BlogApp.Entities.Dtos.Articles;
+
+namespace BlogApp.Business.Abstract;
+public interface IArticleService
+{
+    Task<IDataResult<List<ArticleDto>>> GetTrends();
+}

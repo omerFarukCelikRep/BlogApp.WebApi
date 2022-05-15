0b1e9c57ae1684150697c70d426b710edcc8be4e
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-05-16 01:02:31 +0300

    ServiceMessages added

---
+++ src/Libraries/BlogApp.Business/Constants/ServiceMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Business.Constants;
+public static class ServiceMessages
+{
+    public static readonly string MembersListed = "Members Listed";
+    public static readonly string MemberGetted = "Member Getted";
+    public static readonly string MemberNotFound = "Member Not Found";
+    public static readonly string MemberUpdateSuccess = "Member Updated Successfully";
+
+    public static readonly string ArticlesListed = "Articles Listed";
+    public static readonly string ArticleNotFound = "Article Not Found";
+    public static readonly string ArticlePublished = "Article Published Successfully";
+}

0239a2023f7b090f259662c1b494a5262ee7adec
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:10:12 +0300

    added

---
+++ src/Libraries/BlogApp.Core.Utilities/Constants/DatabaseConstants.cs
@@ -0,0 +1,5 @@
+﻿namespace BlogApp.Core.Utilities.Constants;
+public class DatabaseConstants
+{
+    public const string DefaultConnectionString = "Default";
+}
--- src/Libraries/BlogApp.Core.Utilities/Constants/DatabaseContants.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BlogApp.Core.Utilities.Constants;
-public class DatabaseContants
-{
-    public static readonly string DefaultConnectionString = "Default";
-}

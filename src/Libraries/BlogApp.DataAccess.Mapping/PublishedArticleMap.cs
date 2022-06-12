923f97e0f6aa86aa4821f5efc1cf579d07c764a2
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:18:46 +0300

    inherit class changed

--- src/Libraries/BlogApp.DataAccess.Mapping/PublishedArticleMap.cs
+++ src/Libraries/BlogApp.DataAccess.Mapping/PublishedArticleMap.cs
@@ -4,16 +4,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.DataAccess.Mapping;
-public class PublishedArticleMap : BaseMap<PublishedArticle>
+public class PublishedArticleMap : AuditableEntityMap<PublishedArticle>
 {
     public override void Configure(EntityTypeBuilder<PublishedArticle> builder)
     {
-        base.Configure(builder);
-
         builder.Property(x => x.PublishDate).IsRequired();
         builder.Property(x => x.ReadingCount).HasDefaultValue(0);
         builder.Property(x => x.LikeCount).HasDefaultValue(0);
 
         builder.HasOne(x => x.Article).WithMany().HasForeignKey(x => x.Id);
+
+        base.Configure(builder);
     }
 }

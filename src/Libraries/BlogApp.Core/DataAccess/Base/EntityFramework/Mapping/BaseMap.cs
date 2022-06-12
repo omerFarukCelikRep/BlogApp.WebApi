17ee47270aa270bcbcf6e28c119e51f03dd17da1
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:07:09 +0300

    refactoring

--- src/Libraries/BlogApp.Core/DataAccess/Base/EntityFramework/Mapping/BaseMap.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Base/EntityFramework/Mapping/BaseMap.cs
@@ -3,24 +3,19 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
-public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+public abstract class BaseEntityMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity
 {
     public virtual void Configure(EntityTypeBuilder<T> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.HasAlternateKey(x => x.AutoId);
 
         builder.Property(x => x.Id).ValueGeneratedOnAdd();
 
         builder.Property(x => x.Status).IsRequired(true);
 
-        builder.Property(x => x.CreatedBy).IsRequired(true);
+        builder.Property(x => x.CreatedBy).HasMaxLength(128).IsRequired(true);
         builder.Property(x => x.CreatedDate).IsRequired(true);
-        builder.Property(x => x.ModifiedBy).IsRequired(false);
+        builder.Property(x => x.ModifiedBy).HasMaxLength(128).IsRequired(false);
         builder.Property(x => x.ModifiedDate).IsRequired(false);
-        builder.Property(x => x.DeletedBy).IsRequired(false);
-        builder.Property(x => x.DeletedDate).IsRequired(false);
-
-        builder.Property(x => x.AutoId).ValueGeneratedOnAdd();
     }
 }

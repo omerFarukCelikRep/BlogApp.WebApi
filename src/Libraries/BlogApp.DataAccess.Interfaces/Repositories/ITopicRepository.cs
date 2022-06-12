bd0c0d4b86e28fb583290169daaf30561d85dfa7
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:17:13 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/ITopicRepository.cs
@@ -0,0 +1,7 @@
+﻿using BlogApp.Core.DataAccess.Interfaces;
+using BlogApp.Entities.Concrete;
+
+namespace BlogApp.DataAccess.Interfaces.Repositories;
+public interface ITopicRepository : IInsertableRepositoryAsync<Topic>, IUpdateableRepositoryAsync<Topic>, IDeleteableRepositoryAsync<Topic>, IQueryableRepositoryAsync<Topic>, IFindableRepositoryAsync<Topic>, IRepositoryAsync
+{
+}

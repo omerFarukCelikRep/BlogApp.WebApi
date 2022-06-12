71d04af9f42ac2b3caff56a8426fcbc338e4dfc8
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:18:54 +0300

    inherit class changed

--- src/Libraries/BlogApp.DataAccess.Mapping/RefreshTokenMap.cs
+++ src/Libraries/BlogApp.DataAccess.Mapping/RefreshTokenMap.cs
@@ -3,17 +3,17 @@ using BlogApp.Entities.Concrete;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.DataAccess.Mapping;
-public class RefreshTokenMap : BaseMap<RefreshToken>
+public class RefreshTokenMap : BaseEntityMap<RefreshToken>
 {
     public override void Configure(EntityTypeBuilder<RefreshToken> builder)
     {
-        base.Configure(builder);
-
         builder.Property(x => x.Token).IsRequired();
         builder.Property(x => x.UserId).IsRequired();
         builder.Property(x => x.ExpiryDate).IsRequired();
         builder.Property(x => x.CreatedByIp).IsRequired(false); //TODO:Zorunlu olacak
 
         builder.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId);
+
+        base.Configure(builder);
     }
 }

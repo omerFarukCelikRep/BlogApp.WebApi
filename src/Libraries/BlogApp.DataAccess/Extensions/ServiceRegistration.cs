8e6716e4ce5967b121bff5d30fe4b0c15964c1d0
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:14:04 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess/Extensions/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Core.Utilities.Configurations;
+using BlogApp.Core.Utilities.Constants;
+using BlogApp.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.DataAccess.Extensions;
+public static class ServiceRegistration
+{
+    public static void AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddDbContext<BlogAppDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString(DatabaseConstants.DefaultConnectionString), builder => builder.MigrationsAssembly(typeof(BlogAppDbContext).Assembly.FullName));
+        });
+    }
+}

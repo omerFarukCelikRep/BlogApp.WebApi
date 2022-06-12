bb84d5281080c6ede04e73df686678647ac05ee3
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:04:32 +0300

    static readonly fields changed to const

--- src/Libraries/BlogApp.Business/Constants/ServiceMessages.cs
+++ src/Libraries/BlogApp.Business/Constants/ServiceMessages.cs
@@ -1,12 +1,12 @@
 ﻿namespace BlogApp.Business.Constants;
 public static class ServiceMessages
 {
-    public static readonly string MembersListed = "Members Listed";
-    public static readonly string MemberGetted = "Member Getted";
-    public static readonly string MemberNotFound = "Member Not Found";
-    public static readonly string MemberUpdateSuccess = "Member Updated Successfully";
+    public const string MembersListed = "Members Listed";
+    public const string MemberGetted = "Member Getted";
+    public const string MemberNotFound = "Member Not Found";
+    public const string MemberUpdateSuccess = "Member Updated Successfully";
 
-    public static readonly string ArticlesListed = "Articles Listed";
-    public static readonly string ArticleNotFound = "Article Not Found";
-    public static readonly string ArticlePublished = "Article Published Successfully";
+    public const string ArticlesListed = "Articles Listed";
+    public const string ArticleNotFound = "Article Not Found";
+    public const string ArticlePublished = "Article Published Successfully";
 }

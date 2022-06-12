ab47848e684909e784accad14dbe7eeb154cfca2
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:53 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IArticleRepository.cs
@@ -0,0 +1,7 @@
+﻿using BlogApp.Core.DataAccess.Interfaces;
+using BlogApp.Entities.Concrete;
+
+namespace BlogApp.DataAccess.Interfaces.Repositories;
+public interface IArticleRepository : IFindableRepositoryAsync<Article>, IQueryableRepositoryAsync<Article>, IRepositoryAsync
+{
+}

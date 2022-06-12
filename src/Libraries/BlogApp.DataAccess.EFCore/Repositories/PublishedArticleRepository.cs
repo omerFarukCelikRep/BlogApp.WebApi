a981781a2f20183705f56fb0306df527519e2aeb
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:25 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/PublishedArticleRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
+using BlogApp.DataAccess.Contexts;
+using BlogApp.DataAccess.Interfaces;
+using BlogApp.DataAccess.Interfaces.Repositories;
+using BlogApp.Entities.Concrete;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.DataAccess.EFCore.Repositories;
+public class PublishedArticleRepository : EfBaseRepository<PublishedArticle>, IPublishedArticleRepository
+{
+    public PublishedArticleRepository(BlogAppDbContext context, ILogger<PublishedArticleRepository> logger) : base(context, logger) { }
+}

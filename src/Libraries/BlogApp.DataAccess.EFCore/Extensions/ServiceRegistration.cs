c9188bd5ab192a5841a656d571694e11780610ca
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:00 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/Extensions/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.DataAccess.EFCore.Repositories;
+using BlogApp.DataAccess.Interfaces.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.DataAccess.EFCore.Extensions;
+public static class ServiceRegistration
+{
+    public static void AddDataAccessEFCoreServices(this IServiceCollection services)
+    {
+        services.AddTransient<ITopicRepository, TopicRepository>();
+        services.AddTransient<IMemberRepository, MemberRepository>();
+        services.AddTransient<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddTransient<IArticleRepository, ArticleRepository>();
+        services.AddTransient<IPublishedArticleRepository, PublishedArticleRepository>();
+    }
+}

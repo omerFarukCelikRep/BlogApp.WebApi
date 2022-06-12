d1bd498a694039ccd472c053761fcf2bffe9a629
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:18:38 +0300

    inherit class changed

--- src/Libraries/BlogApp.DataAccess.Mapping/MemberMap.cs
+++ src/Libraries/BlogApp.DataAccess.Mapping/MemberMap.cs
@@ -3,17 +3,17 @@ using BlogApp.Entities.Concrete;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.DataAccess.Mapping;
-public class MemberMap : BaseMap<Member>
+public class MemberMap : AuditableEntityMap<Member>
 {
     public override void Configure(EntityTypeBuilder<Member> builder)
     {
-        base.Configure(builder);
-
         builder.Property(x => x.FirstName).HasMaxLength(256).IsRequired();
         builder.Property(x => x.LastName).HasMaxLength(256).IsRequired();
         builder.Property(x => x.Biography).HasMaxLength(512).IsRequired(false);
         builder.Property(x => x.ProfilePicture).IsRequired(false);
         builder.Property(x => x.Url).HasMaxLength(512).IsRequired(false);
         builder.Property(x => x.IdentityId).IsRequired();
+
+        base.Configure(builder);
     }
 }

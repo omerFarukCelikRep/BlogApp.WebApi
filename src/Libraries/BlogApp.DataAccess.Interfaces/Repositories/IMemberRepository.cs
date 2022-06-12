fdaa67350beb10550bc57b2ff3b608d524b3ad4b
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:59 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IMemberRepository.cs
@@ -0,0 +1,8 @@
+﻿using BlogApp.Core.DataAccess.Interfaces;
+using BlogApp.Entities.Concrete;
+
+namespace BlogApp.DataAccess.Interfaces.Repositories;
+public interface IMemberRepository : IInsertableRepositoryAsync<Member>, IUpdateableRepositoryAsync<Member>, IQueryableRepositoryAsync<Member>, IFindableRepositoryAsync<Member>, IRepositoryAsync
+{
+    Task<Member> GetByIdentityId(Guid identityId);
+}

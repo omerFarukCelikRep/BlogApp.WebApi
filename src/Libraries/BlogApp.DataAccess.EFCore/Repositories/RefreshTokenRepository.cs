3f8e5b23f5402c8ee5e468a1e5b04ff1ccc50d5f
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:31 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,50 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
+using BlogApp.DataAccess.Contexts;
+using BlogApp.DataAccess.Interfaces.Repositories;
+using BlogApp.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.DataAccess.EFCore.Repositories;
+public class RefreshTokenRepository : EfBaseRepository<RefreshToken>, IRefreshTokenRepository
+{
+    public RefreshTokenRepository(BlogAppDbContext context, ILogger<RefreshTokenRepository> logger) : base(context, logger) { }
+
+    public async Task<RefreshToken?> GetByRefreshTokenAsync(string refreshToken)
+    {
+        try
+        {
+            return await _table.Where(x => x.Token == refreshToken)
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.InnerException, ex.Message);
+            return null;
+        }
+    }
+
+    public async Task<bool> UpdateRefreshTokenAsUsedAsync(RefreshToken refreshToken)
+    {
+        try
+        {
+            var token = await _table.Where(x => x.Token == refreshToken.Token)
+                                    .AsNoTracking()
+                                    .FirstOrDefaultAsync();
+
+            if (token == null) return false;
+
+            token.RevokedDate = DateTime.Now;
+
+            _ = await UpdateAsync(token);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.InnerException, ex.Message);
+            return false;
+        }
+    }
+}

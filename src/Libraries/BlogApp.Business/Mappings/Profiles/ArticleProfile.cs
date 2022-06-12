4c31a926567e475b97c61a84742b9484215d7905
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:05:07 +0300

    added

---
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using BlogApp.Entities.Concrete;
+using BlogApp.Entities.Dtos.Articles;
+
+namespace BlogApp.Business.Mappings.Profiles;
+public class ArticleProfile : Profile
+{
+    public ArticleProfile()
+    {
+        CreateMap<Article, ArticleDto>();
+    }
+}

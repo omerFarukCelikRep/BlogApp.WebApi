75a4432ff7dd330a73534b330ce43cd39bf610f0
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:18 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/MemberRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
+using BlogApp.DataAccess.Contexts;
+using BlogApp.DataAccess.Interfaces.Repositories;
+using BlogApp.Entities.Concrete;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.DataAccess.EFCore.Repositories;
+public class MemberRepository : EfBaseRepository<Member>, IMemberRepository
+{
+    public MemberRepository(BlogAppDbContext context, ILogger<MemberRepository> logger) : base(context, logger) { }
+
+    public async Task<Member> GetByIdentityId(Guid identityId)
+    {
+        return await GetAsync(x => x.IdentityId == identityId);
+    }
+}

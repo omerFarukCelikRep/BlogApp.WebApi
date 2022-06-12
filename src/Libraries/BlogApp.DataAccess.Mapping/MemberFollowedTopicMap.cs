5ac585896f65895e4531e7e8770b9959cf791702
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:18:29 +0300

    inherit class changed

--- src/Libraries/BlogApp.DataAccess.Mapping/MemberFollowedTopicMap.cs
+++ src/Libraries/BlogApp.DataAccess.Mapping/MemberFollowedTopicMap.cs
@@ -3,13 +3,13 @@ using BlogApp.Entities.Concrete;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.DataAccess.Mapping;
-public class MemberFollowedTopicMap : BaseMap<MemberFollowedTopic>
+public class MemberFollowedTopicMap : BaseEntityMap<MemberFollowedTopic>
 {
     public override void Configure(EntityTypeBuilder<MemberFollowedTopic> builder)
     {
-        base.Configure(builder);
-
         builder.HasOne(x => x.Member).WithMany(x => x.MemberFollowedTopics).HasForeignKey(x => x.MemberId);
         builder.HasOne(x => x.Topic).WithMany(x => x.MemberFollowedTopics).HasForeignKey(x => x.TopicId);
+
+        base.Configure(builder);
     }
 }

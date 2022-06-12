262061a22572430d6095203fbbf32dd9faabcfbb
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:19:05 +0300

    inherit class changed

--- src/Libraries/BlogApp.DataAccess.Mapping/TopicMap.cs
+++ src/Libraries/BlogApp.DataAccess.Mapping/TopicMap.cs
@@ -3,13 +3,13 @@ using BlogApp.Entities.Concrete;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.DataAccess.Mapping;
-public class TopicMap : BaseMap<Topic>
+public class TopicMap : BaseEntityMap<Topic>
 {
     public override void Configure(EntityTypeBuilder<Topic> builder)
     {
-        base.Configure(builder);
-
         builder.Property(x => x.Name).HasMaxLength(256).IsRequired();
         builder.Property(x => x.Thumbnail).IsRequired(false); //TODO: Zorunlu olacak
+
+        base.Configure(builder);
     }
 }

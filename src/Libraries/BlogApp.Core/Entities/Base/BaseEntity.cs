8eb9e0a7beebe184232df0c039fdf45022dc5229
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:10:48 +0300

    refactoring

--- src/Libraries/BlogApp.Core/Entities/Base/BaseEntity.cs
+++ src/Libraries/BlogApp.Core/Entities/Base/BaseEntity.cs
@@ -1,17 +1,14 @@
-﻿using BlogApp.Core.Entities.Abstract;
-using BlogApp.Core.Entities.Enums;
+﻿using BlogApp.Core.Entities.Enums;
+using BlogApp.Core.Entities.Interfaces;
 
 namespace BlogApp.Core.Entities.Base;
 
-public abstract class BaseEntity : IEntity, ICreatedEntity, IModifiedEntity, IDeletedEntity
+public abstract class BaseEntity : IEntity, ICreatableEntity, IUpdateableEntity
 {
     public Guid Id { get; set; }
     public Status Status { get; set; }
-    public Guid CreatedBy { get; set; }
+    public string CreatedBy { get; set; }
     public DateTime CreatedDate { get; set; }
-    public Guid? ModifiedBy { get; set; }
+    public string? ModifiedBy { get; set; }
     public DateTime? ModifiedDate { get; set; }
-    public Guid? DeletedBy { get; set; }
-    public DateTime? DeletedDate { get; set; }
-    public int AutoId { get; set; }
 }

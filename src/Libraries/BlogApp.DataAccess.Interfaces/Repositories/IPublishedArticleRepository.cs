1e4a868ebdd6435057a85e3edf32b63c04d2a97f
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:16:37 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IPublishedArticleRepository.cs
@@ -0,0 +1,7 @@
+﻿using BlogApp.Core.DataAccess.Interfaces;
+using BlogApp.Entities.Concrete;
+
+namespace BlogApp.DataAccess.Interfaces.Repositories;
+public interface IPublishedArticleRepository : IInsertableRepositoryAsync<PublishedArticle>, IOrderableRepositoryAsync<PublishedArticle>, IQueryableRepositoryAsync<PublishedArticle>, IRepositoryAsync
+{
+}

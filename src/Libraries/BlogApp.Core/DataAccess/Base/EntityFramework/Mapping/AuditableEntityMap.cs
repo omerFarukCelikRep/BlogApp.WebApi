2b1573356cf22b9520ac455d118da259aa9e9741
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:06:51 +0300

    added

---
+++ src/Libraries/BlogApp.Core/DataAccess/Base/EntityFramework/Mapping/AuditableEntityMap.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.Core.Entities.Base;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.Core.DataAccess.Base.EntityFramework.Mapping;
+public class AuditableEntityMap<TEntity> : BaseEntityMap<TEntity> where TEntity : AuditableEntity
+{
+    public override void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.DeletedBy).HasMaxLength(128).IsRequired(false);
+        builder.Property(x => x.DeletedDate).IsRequired(false);
+    }
+}

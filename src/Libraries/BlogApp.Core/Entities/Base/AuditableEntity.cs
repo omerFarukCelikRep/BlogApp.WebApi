d07244f5806ab14fb4d322e44ccbca0d6045f674
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:10:25 +0300

    added

---
+++ src/Libraries/BlogApp.Core/Entities/Base/AuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿using BlogApp.Core.Entities.Interfaces;
+
+namespace BlogApp.Core.Entities.Base;
+public abstract class AuditableEntity : BaseEntity, IEntity, ICreatableEntity, IUpdateableEntity, ISoftDeleteableEntity
+{
+    public string? DeletedBy { get; set; }
+    public DateTime? DeletedDate { get; set; }
+}

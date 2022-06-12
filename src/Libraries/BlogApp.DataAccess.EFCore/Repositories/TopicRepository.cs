4a13a297bc91797673b256c4bc37c302c35a4d9c
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:15:37 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/TopicRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
+using BlogApp.DataAccess.Contexts;
+using BlogApp.DataAccess.Interfaces.Repositories;
+using BlogApp.Entities.Concrete;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.DataAccess.EFCore.Repositories;
+public class TopicRepository : EfBaseRepository<Topic>, ITopicRepository
+{
+    public TopicRepository(BlogAppDbContext context, ILogger<TopicRepository> logger) : base(context, logger) { }
+}

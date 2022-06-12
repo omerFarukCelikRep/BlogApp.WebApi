670cd71c8eafc5d610e6a7ea5fdf3e2caded28ab
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:16:50 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Core.DataAccess.Interfaces;
+using BlogApp.Entities.Concrete;
+
+namespace BlogApp.DataAccess.Interfaces.Repositories;
+public interface IRefreshTokenRepository : IInsertableRepositoryAsync<RefreshToken>, IUpdateableRepositoryAsync<RefreshToken>, IQueryableRepositoryAsync<RefreshToken>, IFindableRepositoryAsync<RefreshToken>, IRepositoryAsync
+{
+    Task<RefreshToken?> GetByRefreshTokenAsync(string refreshToken);
+    Task<bool> UpdateRefreshTokenAsUsedAsync(RefreshToken refreshToken);
+}

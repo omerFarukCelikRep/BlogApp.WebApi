88ff3b07cf8ac251203551645179f2db9decb03d
Ömer Faruk Çelik <ofarukcelik@outlook.com>
2022-06-13 01:18:03 +0300

    inherit class changed

--- src/Libraries/BlogApp.DataAccess.Mapping/ArticleMap.cs
+++ src/Libraries/BlogApp.DataAccess.Mapping/ArticleMap.cs
@@ -3,17 +3,17 @@ using BlogApp.Entities.Concrete;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.DataAccess.Mapping;
-public class ArticleMap : BaseMap<Article>
+public class ArticleMap : AuditableEntityMap<Article>
 {
     public override void Configure(EntityTypeBuilder<Article> builder)
     {
-        base.Configure(builder);
-
         builder.Property(x => x.Content).IsRequired();
         builder.Property(x => x.Title).HasMaxLength(256).IsRequired();
         builder.Property(x => x.Thumbnail).IsRequired();
         builder.Property(x => x.ReadTime).IsRequired();
 
         builder.HasOne(x => x.Member).WithMany(x => x.Articles).HasForeignKey(x => x.MemberId);
+
+        base.Configure(builder);
     }
 }

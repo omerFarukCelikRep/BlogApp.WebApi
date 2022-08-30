8c5453ae1959e1df0fee7ccdbe0043b5083d67ab
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 23:30:06 +0300

    added

---
+++ src/Libraries/BlogApp.Entities.Dtos/Articles/ArticleCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Entities.Dtos.Articles;
+public class ArticleCreateDto
+{
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public byte[]? Thumbnail { get; set; }
+    public Guid MemberId { get; set; }
+    public List<Guid> Topics { get; set; }
+}

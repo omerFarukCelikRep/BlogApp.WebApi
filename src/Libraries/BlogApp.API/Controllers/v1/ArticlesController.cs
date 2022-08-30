0e6785ad44e91a1a4bcd46d4a11ee0ddcc90b346
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 22:58:34 +0300

    Create and Publish actions added

--- src/Libraries/BlogApp.API/Controllers/v1/ArticlesController.cs
+++ src/Libraries/BlogApp.API/Controllers/v1/ArticlesController.cs
@@ -1,4 +1,5 @@
 ﻿using BlogApp.Business.Interfaces;
+using BlogApp.Entities.Dtos.Articles;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.API.Controllers.v1;
@@ -15,27 +16,17 @@ public class ArticlesController : BaseController
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var result = await _articleService.GetAllAsync();
+        var result = await _articleService.GetAllAsync(tracking: false);
 
-        if (!result.IsSuccess)
-        {
-            return BadRequest(result);
-        }
-
-        return Ok(result);
+        return GetDataResult(result);
     }
 
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
-        var result = await _articleService.GetByIdAsync(id);
+        var result = await _articleService.GetByIdAsync(id, tracking: false);
 
-        if (!result.IsSuccess)
-        {
-            return BadRequest(result);
-        }
-
-        return Ok(result);
+        return GetDataResult(result);
     }
 
     [HttpGet("Trends")]
@@ -43,11 +34,27 @@ public class ArticlesController : BaseController
     {
         var result = await _articleService.GetTrendsAsync();
 
-        if (!result.IsSuccess)
+        return GetDataResult(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] ArticleCreateDto createArticleDto)
+    {
+        if (!ModelState.IsValid)
         {
-            return BadRequest(result);
+            return BadRequest(ModelState);
         }
 
-        return Ok(result);
+        var result = await _articleService.AddAsync(createArticleDto);
+
+        return Ok();
+    }
+
+    [HttpPost("Publish")]
+    public async Task<IActionResult> Publish([FromQuery] Guid id)
+    {
+        var result = await _articleService.PublishAsync(id);
+
+        return GetResult(result);
     }
 }

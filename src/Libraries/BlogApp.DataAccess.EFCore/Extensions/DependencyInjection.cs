443ee8e9abf8933de06820a110857efb0a7b9c5b
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 23:28:47 +0300

    renamed

--- src/Libraries/BlogApp.DataAccess.EFCore/Extensions/DependencyInjection.cs
+++ src/Libraries/BlogApp.DataAccess.EFCore/Extensions/DependencyInjection.cs
@@ -3,14 +3,16 @@ using BlogApp.DataAccess.Interfaces.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlogApp.DataAccess.EFCore.Extensions;
-public static class ServiceRegistration
+public static class DependencyInjection
 {
-    public static void AddDataAccessEFCoreServices(this IServiceCollection services)
+    public static IServiceCollection AddDataAccessEFCoreServices(this IServiceCollection services)
     {
         services.AddTransient<ITopicRepository, TopicRepository>();
         services.AddTransient<IMemberRepository, MemberRepository>();
         services.AddTransient<IRefreshTokenRepository, RefreshTokenRepository>();
         services.AddTransient<IArticleRepository, ArticleRepository>();
         services.AddTransient<IPublishedArticleRepository, PublishedArticleRepository>();
+
+        return services;
     }
 }

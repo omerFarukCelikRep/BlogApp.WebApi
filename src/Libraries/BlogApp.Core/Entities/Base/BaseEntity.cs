bcc67cfc70025340c1e646b4c82dd94338a0fb00
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-31 00:01:21 +0300

    null safety added

--- src/Libraries/BlogApp.Core/Entities/Base/BaseEntity.cs
+++ src/Libraries/BlogApp.Core/Entities/Base/BaseEntity.cs
@@ -7,7 +7,7 @@ public abstract class BaseEntity : IEntity, ICreatableEntity, IUpdateableEntity
 {
     public Guid Id { get; set; }
     public Status Status { get; set; }
-    public string CreatedBy { get; set; }
+    public string CreatedBy { get; set; } = null!;
     public DateTime CreatedDate { get; set; }
     public string? ModifiedBy { get; set; }
     public DateTime? ModifiedDate { get; set; }

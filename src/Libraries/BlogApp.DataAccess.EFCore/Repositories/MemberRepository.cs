7c59a26e6ed29aafb5036ba46809118acffd6489
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-31 00:15:21 +0300

    logger removed

--- src/Libraries/BlogApp.DataAccess.EFCore/Repositories/MemberRepository.cs
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/MemberRepository.cs
@@ -2,12 +2,11 @@
 using BlogApp.DataAccess.Contexts;
 using BlogApp.DataAccess.Interfaces.Repositories;
 using BlogApp.Entities.Concrete;
-using Microsoft.Extensions.Logging;
 
 namespace BlogApp.DataAccess.EFCore.Repositories;
 public class MemberRepository : EfBaseRepository<Member>, IMemberRepository
 {
-    public MemberRepository(BlogAppDbContext context, ILogger<MemberRepository> logger) : base(context, logger) { }
+    public MemberRepository(BlogAppDbContext context) : base(context) { }
 
     public async Task<Member> GetByIdentityId(Guid identityId)
     {

8e8d521750e9c79236da84fcf3cdd6432240e953
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 23:24:03 +0300

    renamed

---
+++ src/Libraries/BlogApp.Business/Extensions/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Business.Concrete;
+using BlogApp.Business.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.Business.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessServices(this IServiceCollection services)
+    {
+        services.AddCustomValidation();
+
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ITopicService, TopicService>();
+        services.AddScoped<IMemberService, MemberService>();
+        services.AddScoped<IArticleService, ArticleService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomValidation(this IServiceCollection services)
+    {
+        services.AddFluentValidationClientsideAdapters();
+        services.AddValidatorsFromAssemblyContaining<IValidator>();
+
+        return services;
+    }
+}

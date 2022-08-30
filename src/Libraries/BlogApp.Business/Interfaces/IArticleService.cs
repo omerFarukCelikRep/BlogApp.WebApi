d1a3180e9999b0f06c69a2c30a2896b97ddc1042
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-31 00:11:08 +0300

    AddAsync added

--- src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
+++ src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
@@ -8,4 +8,5 @@ public interface IArticleService
     Task<IDataResult<List<ArticleDto>>> GetTrendsAsync();
     Task<IDataResult<ArticleDto>> GetByIdAsync(Guid id);
     Task<IResult> PublishAsync(Guid articleId);
+    Task<IResult> AddAsync(ArticleCreateDto createArticleDto);
 }

447ba1389cfe7f625bd11d6a390252d11b697276
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 23:01:35 +0300

    Result methods added

--- src/Libraries/BlogApp.API/Controllers/v1/BaseController.cs
+++ src/Libraries/BlogApp.API/Controllers/v1/BaseController.cs
@@ -1,4 +1,6 @@
-﻿using BlogApp.Core.Utilities.Results.Concrete;
+﻿using BlogApp.Business.Interfaces;
+using BlogApp.Core.Utilities.Authentication;
+using BlogApp.Core.Utilities.Results.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.API.Controllers.v1;
@@ -8,8 +10,20 @@ namespace BlogApp.API.Controllers.v1;
 [ApiVersion("1.0")]
 public class BaseController : ControllerBase
 {
-    protected Core.Utilities.Results.Abstract.IResult PopulateError(string message)
+    protected string UserIdentityId => JwtHelper.GetUserIdByToken(HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last());
+
+    protected async Task<Guid> GetUserId([FromServices] IUserService userService)
     {
-        return new ErrorResult(message);
+        return await userService.GetUserIdByIdentityId(Guid.Parse(UserIdentityId));
+    }
+
+    protected IActionResult GetResult(Core.Utilities.Results.Abstract.IResult result)
+    {
+        return result.IsSuccess ? Ok(result) : BadRequest(result);
+    }
+
+    protected IActionResult GetDataResult<T>(IDataResult<T> result)
+    {
+        return result.IsSuccess ? Ok(result) : BadRequest(result);
     }
 }

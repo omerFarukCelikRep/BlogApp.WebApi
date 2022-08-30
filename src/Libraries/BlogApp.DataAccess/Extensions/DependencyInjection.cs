d43136e993899a124366b800eb613585d7394737
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 23:29:57 +0300

    renamed

--- src/Libraries/BlogApp.DataAccess/Extensions/DependencyInjection.cs
+++ src/Libraries/BlogApp.DataAccess/Extensions/DependencyInjection.cs
@@ -5,13 +5,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlogApp.DataAccess.Extensions;
-public static class ServiceRegistration
+public static class DependencyInjection
 {
-    public static void AddDataAccessServices(this IServiceCollection services)
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
     {
         services.AddDbContext<BlogAppDbContext>(options =>
         {
             options.UseSqlServer(Configuration.GetConnectionString(DatabaseConstants.DefaultConnectionString), builder => builder.MigrationsAssembly(typeof(BlogAppDbContext).Assembly.FullName));
         });
+
+        return services;
     }
 }

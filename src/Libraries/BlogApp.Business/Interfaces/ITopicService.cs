3a0fbc03ec95c5c91c6491a9fe14cae6463e8a82
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-31 00:12:04 +0300

    return types changed

--- src/Libraries/BlogApp.Business/Interfaces/ITopicService.cs
+++ src/Libraries/BlogApp.Business/Interfaces/ITopicService.cs
@@ -1,4 +1,4 @@
-﻿using BlogApp.Core.Utilities.Results.Concrete;
+﻿using BlogApp.Core.Utilities.Results.Abstract;
 using BlogApp.Entities.Concrete;
 using BlogApp.Entities.Dtos.Topics;
 using System.Linq.Expressions;
@@ -6,11 +6,11 @@ using System.Linq.Expressions;
 namespace BlogApp.Business.Interfaces;
 public interface ITopicService
 {
-    Task<DataResult<IEnumerable<ListTopicDto>>> GetAllAsync();
-    Task<DataResult<IEnumerable<ListTopicDto>>> GetAllAsync(Expression<Func<Topic, bool>> expression);
-    Task<DataResult<TopicDto>> GetAsync(Expression<Func<Topic, bool>> expression);
-    Task<DataResult<TopicDto>> GetByIdAsync(Guid id);
-    Task<DataResult<TopicDto>> AddAsync(CreateTopicDto createDto);
-    Task<DataResult<TopicDto>> UpdateAsync(UpdateTopicDto updateDto);
-    Task<Result> DeleteAsync(Guid id);
+    Task<IDataResult<IEnumerable<ListTopicDto>>> GetAllAsync();
+    Task<IDataResult<IEnumerable<ListTopicDto>>> GetAllAsync(Expression<Func<Topic, bool>> expression);
+    Task<IDataResult<TopicDto>> GetAsync(Expression<Func<Topic, bool>> expression);
+    Task<IDataResult<TopicDto>> GetByIdAsync(Guid id);
+    Task<IDataResult<TopicDto>> AddAsync(CreateTopicDto createDto);
+    Task<IDataResult<TopicDto>> UpdateAsync(UpdateTopicDto updateDto);
+    Task<IResult> DeleteAsync(Guid id);
 }

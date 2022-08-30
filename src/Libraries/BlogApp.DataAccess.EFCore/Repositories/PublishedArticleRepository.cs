9fc8c94af748a826fdf0c5c038abe90901cb1e61
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-31 00:15:29 +0300

    logger removed

--- src/Libraries/BlogApp.DataAccess.EFCore/Repositories/PublishedArticleRepository.cs
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/PublishedArticleRepository.cs
@@ -1,12 +1,10 @@
 ﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
 using BlogApp.DataAccess.Contexts;
-using BlogApp.DataAccess.Interfaces;
 using BlogApp.DataAccess.Interfaces.Repositories;
 using BlogApp.Entities.Concrete;
-using Microsoft.Extensions.Logging;
 
 namespace BlogApp.DataAccess.EFCore.Repositories;
 public class PublishedArticleRepository : EfBaseRepository<PublishedArticle>, IPublishedArticleRepository
 {
-    public PublishedArticleRepository(BlogAppDbContext context, ILogger<PublishedArticleRepository> logger) : base(context, logger) { }
+    public PublishedArticleRepository(BlogAppDbContext context) : base(context) { }
 }

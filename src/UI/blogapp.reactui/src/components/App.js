e51a4c1e613ed1c85621553299352ce6351c16f7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-30 23:27:41 +0300

    react template

---
+++ src/UI/blogapp.reactui/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from './layout';
+
+const App = () => {
+    return (
+        <>
+            <Layout>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+            </Layout>
+        </>
+    );
+}
+
+export default App;

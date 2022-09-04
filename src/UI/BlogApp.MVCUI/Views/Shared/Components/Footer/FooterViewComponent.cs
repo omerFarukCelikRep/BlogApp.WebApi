5bcf5a9da909f745e4952069d91bc87a87f1e1db
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-04 23:22:21 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/Footer/FooterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVCUI.Views.Shared.Components.Footer;
+
+[ViewComponent(Name = "Footer")]
+public class FooterViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult(View());
+    }
+}
---
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/FooterPopularTopics/FooterPopularTopicsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.MVCUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVCUI.Views.Shared.Components.FooterPopularTopics;
+
+[ViewComponent(Name = "FooterPopularTopics")]
+public class FooterPopularTopicsViewComponent : ViewComponent
+{
+    //private readonly ITopicService _topicService;
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        await Task.CompletedTask;
+
+        return View(Enumerable.Empty<FooterTopicListVM>());
+    }
+}

04f10c0682bb2206168f9111ad627f7f1020d302
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-04 23:12:45 +0300

    deleted

--- src/UI/BlogApp.MVCUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BlogApp.MVCUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

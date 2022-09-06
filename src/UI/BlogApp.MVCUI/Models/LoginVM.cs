d4d4a4db5115945734c3e79cfe2e4d28fae237c3
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-06 22:59:26 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Models/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.MVCUI.Models;
+
+public class LoginVM
+{
+    [Required]
+    [EmailAddress]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}

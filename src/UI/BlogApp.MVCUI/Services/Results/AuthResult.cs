6dc304c2dfc8c630d152fe8727ad1fb94e1dd519
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-06 22:59:43 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Services/Results/AuthResult.cs
@@ -0,0 +1,26 @@
+﻿namespace BlogApp.MVCUI.Services.Results;
+
+public class AuthResult
+{
+    public AuthResult() { }
+
+    public AuthResult(bool success, params string[] errors)
+    {
+        Token = string.Empty;
+        RefreshToken = string.Empty;
+        Success = success;
+        Errors.AddRange(errors);
+    }
+
+    public AuthResult(string token, string refreshToken, bool success)
+    {
+        Token = token;
+        RefreshToken = refreshToken;
+        Success = success;
+    }
+
+    public string Token { get; set; }
+    public string RefreshToken { get; set; }
+    public bool Success { get; set; }
+    public List<string> Errors { get; set; } = new List<string>();
+}

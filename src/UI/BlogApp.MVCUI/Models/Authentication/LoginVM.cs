623549c9d6fb689b53ae05a51ee74082c1f7ba4a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-08 00:50:39 +0300

    moved

--- src/UI/BlogApp.MVCUI/Models/Authentication/LoginVM.cs
+++ src/UI/BlogApp.MVCUI/Models/Authentication/LoginVM.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace BlogApp.MVCUI.Models;
+namespace BlogApp.MVCUI.Models.Authentication;
 
 public class LoginVM
 {
     [Required]
+    [Display(Name = "Email")]
     [EmailAddress]
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
 
     [Required]
+    [Display(Name = "Şifre")]
     [DataType(DataType.Password)]
     public string Password { get; set; }
 }

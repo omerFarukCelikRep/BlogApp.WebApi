668e6bfc82aadcd72f5aa7f745a60df3a8d7fc55
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:35:12 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Services/Interfaces/ITopicService.cs
@@ -0,0 +1,11 @@
+﻿using BlogApp.Core.Utilities.Results.Abstract;
+using BlogApp.MVCUI.Models.Topics;
+using IResult = BlogApp.Core.Utilities.Results.Abstract.IResult;
+
+namespace BlogApp.MVCUI.Services.Interfaces;
+
+public interface ITopicService
+{
+    Task<IDataResult<List<TopicListVM>>> GetAll();
+    Task<IResult> AddAsync(TopicAddVM topicAddVM);
+}

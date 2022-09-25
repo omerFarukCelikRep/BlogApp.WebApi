a0d79c0204466bd8beadd07b17c6724e0b233b04
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:35:06 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Middlewares/UserClaimsMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BlogApp.MVCUI.Middlewares;
+
+public class UserClaimsMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public UserClaimsMiddleware(RequestDelegate next, IHttpContextAccessor httpContextAccessor)
+    {
+        _next = next;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        string token = _httpContextAccessor.HttpContext.Session.GetString("Token");
+        if (!string.IsNullOrEmpty(token))
+        {
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            httpContext.User = new(new ClaimsIdentity(jwtSecurityToken.Claims, "JwtAuthType"));
+
+        }
+
+        await _next(httpContext);
+    }
+}

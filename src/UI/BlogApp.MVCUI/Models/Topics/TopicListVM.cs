78b42ec2e5580771ee16913772ddf99732383cfa
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:34:39 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Models/Topics/TopicListVM.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApp.MVCUI.Models.Topics;
+
+public class TopicListVM
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Thumbnail { get; set; }
+}

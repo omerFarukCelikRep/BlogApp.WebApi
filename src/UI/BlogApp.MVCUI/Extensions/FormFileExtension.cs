4fdb6c682d8efab360bbad97ba4e2eb03b417be2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:33:52 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Extensions/FormFileExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogApp.MVCUI.Extensions;
+
+public static class FormFileExtension
+{
+    public static async Task<string> FileToString(this IFormFile file)
+    {
+        using (var stream = new MemoryStream())
+        {
+            await file.CopyToAsync(stream);
+            var fileAsByteArray = stream.ToArray();
+            return Convert.ToBase64String(fileAsByteArray);
+        }
+    }
+}

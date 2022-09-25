66cac2796821230b7972f8a6679b98cc16643472
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:34:21 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Filters/AuthorizationFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BlogApp.MVCUI.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizationFilter : Attribute, IAsyncAuthorizationFilter
+{
+    public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var token = context.HttpContext.Session.GetString("Token");
+        if (string.IsNullOrEmpty(token))
+        {
+            context.Result = new RedirectToRouteResult(
+                new RouteValueDictionary(new { controller = "Home", action = "Login" })
+            );
+        }
+        else
+        {
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            context.HttpContext.User = new(new ClaimsIdentity(jwtSecurityToken.Claims, "JwtAuthType"));
+        }
+
+        return Task.CompletedTask;
+    }
+}

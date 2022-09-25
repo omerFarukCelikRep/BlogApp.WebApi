57926ee45faa6e55d9577109d4d7d4e7388de7a2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:33:23 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Controllers/ArticleController.cs
@@ -0,0 +1,35 @@
+﻿using BlogApp.MVCUI.Models.Articles;
+using BlogApp.MVCUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlogApp.MVCUI.Controllers;
+public class ArticleController : Controller
+{
+    private readonly ITopicService _topicService;
+    public ArticleController(ITopicService topicService)
+    {
+        _topicService = topicService;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        var result = await _topicService.GetAll();
+        return View(new ArticleAddVM
+        {
+            Topics = result.Data.Select(x => new SelectListItem
+            {
+                Selected = false,
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }).ToList()
+        });
+    }
+}

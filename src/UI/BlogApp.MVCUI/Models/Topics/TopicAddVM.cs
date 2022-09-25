a4aba7eac7d63a7baa8c44869a83887ead5e5109
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:34:33 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Models/Topics/TopicAddVM.cs
@@ -0,0 +1,30 @@
+﻿using BlogApp.MVCUI.Extensions;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BlogApp.MVCUI.Models.Topics;
+
+public class TopicAddVM
+{
+    [Required]
+    [MinLength(0)]
+    [Display(Name = "İsim")]
+    public string Name { get; set; }
+
+    [Required]
+    [Display(Name = "Resim")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public IFormFile ThumbnailFile { get; set; }
+
+    public string Thumbnail
+    {
+        get
+        {
+            if (ThumbnailFile is null)
+            {
+                return string.Empty;
+            }
+            return ThumbnailFile.FileToString().GetAwaiter().GetResult();
+        }
+    }
+}

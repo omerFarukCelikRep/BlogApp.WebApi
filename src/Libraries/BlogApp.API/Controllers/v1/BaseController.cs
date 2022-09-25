1b13f365c89a8e6641d9405fbeaf4ae7f769d764
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-09-26 00:28:13 +0300

    Get UserId refactor

--- src/Libraries/BlogApp.API/Controllers/v1/BaseController.cs
+++ src/Libraries/BlogApp.API/Controllers/v1/BaseController.cs
@@ -1,18 +1,21 @@
 ﻿using BlogApp.Business.Interfaces;
-using BlogApp.Core.Utilities.Authentication;
 using BlogApp.Core.Utilities.Results.Abstract;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BlogApp.API.Controllers.v1;
 
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiController]
 [ApiVersion("1.0")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public class BaseController : ControllerBase
 {
-    protected string UserIdentityId => JwtHelper.GetUserIdByToken(HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last());
+    protected string UserIdentityId => User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-    protected async Task<Guid> GetUserId([FromServices] IUserService userService)
+    protected async Task<Guid?> GetUserId([FromServices] IUserService userService)
     {
         return await userService.GetUserIdByIdentityIdAsync(Guid.Parse(UserIdentityId));
     }

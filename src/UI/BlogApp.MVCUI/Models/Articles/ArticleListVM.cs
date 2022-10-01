59c7f2aef3847fa5a2246822f4c7c4ddcbbad33c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 01:01:54 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Models/Articles/ArticleListVM.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogApp.MVCUI.Models.Articles;
+
+public class ArticleListVM
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Thumbnail { get; set; }
+
+}

f25333f21e752c0750845052af748cb4339961ce
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 00:48:56 +0300

    Add post and Unpublished get actions added

--- src/UI/BlogApp.MVCUI/Controllers/ArticleController.cs
+++ src/UI/BlogApp.MVCUI/Controllers/ArticleController.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlogApp.MVCUI.Controllers;
-public class ArticleController : Controller
+public class ArticleController : BaseController
 {
     private readonly ITopicService _topicService;
-    public ArticleController(ITopicService topicService)
+    private readonly IArticleService _articleService;
+    public ArticleController(ITopicService topicService, IArticleService articleService)
     {
         _topicService = topicService;
+        _articleService = articleService;
     }
 
     [HttpGet]
@@ -21,15 +23,52 @@ public class ArticleController : Controller
     [HttpGet]
     public async Task<IActionResult> Add()
     {
-        var result = await _topicService.GetAll();
         return View(new ArticleAddVM
         {
-            Topics = result.Data.Select(x => new SelectListItem
-            {
-                Selected = false,
-                Text = x.Name,
-                Value = x.Id.ToString()
-            }).ToList()
+            Topics = await GetTopics()
+        });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(ArticleAddVM articleAddVM)
+    {
+        if (!ModelState.IsValid)
+        {
+            articleAddVM.Topics = await GetTopics();
+            return View(articleAddVM);
+        }
+
+        var result = await _articleService.AddAsync(articleAddVM);
+        if (!result.IsSuccess)
+        {
+            ModelState.AddModelError(string.Empty, result.Message);
+            return View(articleAddVM);
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Unpublished()
+    {
+        var result = await _articleService.GetAllUnpublished();
+        if (!result.IsSuccess)
+        {
+            ModelState.AddModelError(string.Empty, result.Message);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(result.Data);
+    }
+
+    public async Task<List<SelectListItem>> GetTopics(Guid? selectedId = null)
+    {
+        var topics = (await _topicService.GetAll()).Data;
+        return topics.ConvertAll(topic => new SelectListItem
+        {
+            Selected = selectedId is not null && selectedId == topic.Id,
+            Text = topic.Name,
+            Value = topic.Id.ToString()
         });
     }
 }

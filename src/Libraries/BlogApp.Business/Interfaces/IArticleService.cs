4c54e7e930d6cac96f52a672f081283f695485f0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 00:44:12 +0300

    GetAllUnpublishedByUserIdAsync added

--- src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
+++ src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
@@ -4,9 +4,10 @@ using BlogApp.Entities.Dtos.Articles;
 namespace BlogApp.Business.Interfaces;
 public interface IArticleService
 {
-    Task<IDataResult<List<ArticleDto>>> GetAllAsync();
+    Task<IDataResult<List<ArticlePublishedListDto>>> GetAllPublishedAsync();
     Task<IDataResult<List<ArticleDto>>> GetTrendsAsync();
     Task<IDataResult<ArticleDto>> GetByIdAsync(Guid id);
     Task<IResult> PublishAsync(Guid articleId);
     Task<IResult> AddAsync(ArticleCreateDto createArticleDto);
+    Task<IDataResult<ArticleUnpublishedListDto>> GetAllUnpublishedByUserIdAsync(Guid userId);
 }

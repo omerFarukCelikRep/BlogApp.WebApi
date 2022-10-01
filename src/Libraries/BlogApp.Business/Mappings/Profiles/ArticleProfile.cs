4661468cc0049120d35c8ed73408dbf19ec213b6
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 00:46:09 +0300

    mappings added

--- src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
@@ -7,7 +7,11 @@ public class ArticleProfile : Profile
 {
     public ArticleProfile()
     {
+        CreateMap<Article, ArticleListDto>()
+            .ForMember(dest => dest.AuthorName, x => x.MapFrom(src => $"{src.Member.FirstName} {src.Member.LastName}"));
+        CreateMap<PublishedArticle, ArticlePublishedListDto>();
+        CreateMap<Article, ArticleUnpublishedListDto>();
         CreateMap<Article, ArticleDto>();
-        CreateMap<ArticleCreateDto, Article>();
+        CreateMap<ArticleCreateDto, Article>().ForSourceMember(member => member.Topics, opt => opt.DoNotValidate());
     }
 }

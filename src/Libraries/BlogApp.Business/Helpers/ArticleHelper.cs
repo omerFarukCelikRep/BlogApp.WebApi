43963f7a99c4f473f31b1e9296980d62adfc181a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 00:43:50 +0300

    refactoring

--- src/Libraries/BlogApp.Business/Helpers/ArticleHelper.cs
+++ src/Libraries/BlogApp.Business/Helpers/ArticleHelper.cs
@@ -1,22 +1,21 @@
-﻿namespace BlogApp.Business.Helpers;
+﻿using System.Text.RegularExpressions;
+
+namespace BlogApp.Business.Helpers;
 public static class ArticleHelper
 {
     private static List<char> specialCharacters = new List<char>() { ' ', ',', '.', '@', '(', ')', '"' };
     public static int CalculateReadTime(string text)
     {
         List<char> letters = new List<char>();
+        text = Regex.Replace(text, "<.*?>", string.Empty);
 
         foreach (char item in text)
         {
-            if (specialCharacters.Contains(item))
-            {
-                continue;
-            }
+            if (specialCharacters.Contains(item)) continue;
+
             letters.Add(item);
         }
 
-        int readTime = letters.Count / 150;
-
-        return readTime;
+        return letters.Count / 150;
     }
 }

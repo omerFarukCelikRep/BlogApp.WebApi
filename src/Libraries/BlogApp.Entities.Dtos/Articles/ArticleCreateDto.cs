0241407444cfe30d81de51faed82fc15a72f786d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 00:46:45 +0300

    Thumbnail return type changed. JsonPropertyName attribute added for topics

--- src/Libraries/BlogApp.Entities.Dtos/Articles/ArticleCreateDto.cs
+++ src/Libraries/BlogApp.Entities.Dtos/Articles/ArticleCreateDto.cs
@@ -1,9 +1,15 @@
-﻿namespace BlogApp.Entities.Dtos.Articles;
+﻿using System.Text.Json.Serialization;
+
+namespace BlogApp.Entities.Dtos.Articles;
 public class ArticleCreateDto
 {
     public string? Title { get; set; }
     public string? Content { get; set; }
-    public byte[]? Thumbnail { get; set; }
+    public string? Thumbnail { get; set; }
+
+    [JsonIgnore]
     public Guid MemberId { get; set; }
+
+    [JsonPropertyName("topicIds")]
     public List<Guid> Topics { get; set; }
 }

a730d2121b2c9c4f74d26114cde509634581b471
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 00:49:02 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Core.Entities.Base;
+using BlogApp.MVCUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Dynamic;
+
+namespace BlogApp.MVCUI.Controllers;
+public class BaseController : Controller
+{
+}

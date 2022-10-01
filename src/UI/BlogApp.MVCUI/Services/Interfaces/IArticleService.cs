b14253f2cda4c5d155f4c5661a82c63c238e3316
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-02 01:02:22 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Services/Interfaces/IArticleService.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Core.Utilities.Results.Abstract;
+using BlogApp.MVCUI.Models.Articles;
+
+namespace BlogApp.MVCUI.Services.Interfaces;
+
+public interface IArticleService
+{
+    Task<Core.Utilities.Results.Abstract.IResult> AddAsync(ArticleAddVM articleAddVM);
+    Task<IDataResult<List<ArticleUnpublishedListVM>>> GetAllUnpublished();
+}

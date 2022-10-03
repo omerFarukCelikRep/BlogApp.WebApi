3cb2d453ecdd42cd04561fd919c3e5101972a70c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-04 01:24:37 +0300

    ckeditor added

---
+++ src/UI/BlogApp.MVCUI/wwwroot/lib/ckeditor/config.js
@@ -0,0 +1,33 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+    config.language = 'tr';
+    config.uiColor = '#AADC6E';
+
+    config.toolbarGroups = [
+        { name: 'clipboard', groups: ['clipboard', 'undo'] },
+        { name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
+        { name: 'links' },
+        { name: 'insert' },
+        { name: 'forms' },
+        { name: 'tools' },
+        { name: 'document', groups: ['mode', 'document', 'doctools'] },
+        { name: 'others' },
+        '/',
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
+        { name: 'styles' },
+        { name: 'colors' }
+    ];
+
+    extraPlugins: 'print,format,font,colorbutton,justify,uploadimage',
+
+    // Set the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;pre';
+
+    // Simplify the dialog windows.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
+};

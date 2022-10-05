718fb5000855c425078a0039ab887a3e4028ab22
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-06 00:48:41 +0300

    ArticleUnpublishedListDto mpa configuration added

--- src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
@@ -10,7 +10,19 @@ public class ArticleProfile : Profile
         CreateMap<Article, ArticleListDto>()
             .ForMember(dest => dest.AuthorName, x => x.MapFrom(src => $"{src.Member.FirstName} {src.Member.LastName}"));
         CreateMap<PublishedArticle, ArticlePublishedListDto>();
-        CreateMap<Article, ArticleUnpublishedListDto>();
+        CreateMap<Article, ArticleUnpublishedListDto>()
+                .ForMember(
+                    dest => dest.CreatedDate,
+                    config => config.MapFrom(src => DateOnly.FromDateTime(src.CreatedDate))
+                )
+                .ForMember(
+                    dest => dest.AuthorName,
+                    config => config.MapFrom(src => $"{src.Member.FirstName} {src.Member.LastName}")
+                )
+                .ForMember(
+                    dest => dest.Topics,
+                    config => config.MapFrom(src => string.Join(", ", src.ArticleTopics.Select(x => x.Topic.Name)))
+                );
         CreateMap<Article, ArticleDto>();
         CreateMap<ArticleCreateDto, Article>().ForSourceMember(member => member.Topics, opt => opt.DoNotValidate());
     }

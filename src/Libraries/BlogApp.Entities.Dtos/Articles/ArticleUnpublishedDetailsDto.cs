5a3e917a04145c3390d6a89e9a1d761c73b467fb
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-09 00:33:06 +0300

    added

---
+++ src/Libraries/BlogApp.Entities.Dtos/Articles/ArticleUnpublishedDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Entities.Dtos.Articles;
+public class ArticleUnpublishedDetailsDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int ReadTime { get; set; }
+    public string? Thumbnail { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string Topics { get; set; }
+    public string AuthorName { get; set; }
+}

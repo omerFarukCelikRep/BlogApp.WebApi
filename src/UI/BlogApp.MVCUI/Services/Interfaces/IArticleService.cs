1335abc9edca82ea498bcd97954cec62be65b097
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-14 23:42:23 +0300

    Publish method declared

--- src/UI/BlogApp.MVCUI/Services/Interfaces/IArticleService.cs
+++ src/UI/BlogApp.MVCUI/Services/Interfaces/IArticleService.cs
@@ -8,4 +8,5 @@ public interface IArticleService
     Task<Core.Utilities.Results.Interfaces.IResult> AddAsync(ArticleAddVM articleAddVM);
     Task<IDataResult<List<ArticleUnpublishedListVM>>> GetAllUnpublished();
     Task<IDataResult<ArticleUnpublishedDetailsVM>> GetUnpublishedById(Guid articleId);
+    Task<Core.Utilities.Results.Interfaces.IResult> Publish(Guid articleId);
 }

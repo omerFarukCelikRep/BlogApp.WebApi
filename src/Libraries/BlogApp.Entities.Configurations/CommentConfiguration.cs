bcdc6f5bb2c9f29f44bc053fb1f0b9c19733f78f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-16 00:31:53 +0300

    added

---
+++ src/Libraries/BlogApp.Entities.Configurations/CommentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Core.Entities.Configurations;
+using BlogApp.Entities.DbSets;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogApp.Entities.Configurations;
+public class CommentConfiguration : AuditableEntityConfiguration<Comment>
+{
+    public override void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.UserName).IsRequired();
+        builder.Property(x => x.UserIpAdress).IsRequired();
+        builder.Property(x => x.ArticleId).IsRequired();
+        builder.Property(x => x.MemberId).IsRequired(false);
+
+        builder.HasOne(x => x.Article).WithMany(x => x.Comments).HasForeignKey(x => x.ArticleId);
+    }
+}

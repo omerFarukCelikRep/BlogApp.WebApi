edbd79dac9689b23841b66242796694df044cac6
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-16 00:31:41 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/CommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
+using BlogApp.DataAccess.Contexts;
+using BlogApp.DataAccess.Interfaces.Repositories;
+using BlogApp.Entities.DbSets;
+
+namespace BlogApp.DataAccess.EFCore.Repositories;
+public class CommentRepository : EfBaseRepository<Comment>, ICommentRepository
+{
+    public CommentRepository(BlogAppDbContext context) : base(context) { }
+}

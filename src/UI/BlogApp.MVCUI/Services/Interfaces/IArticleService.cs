d29a2e3d4f0f87d4b54c22558e55882a9b5fd86c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-16 00:35:52 +0300

    GetAllPublished and GetPublishedById methods declared

--- src/UI/BlogApp.MVCUI/Services/Interfaces/IArticleService.cs
+++ src/UI/BlogApp.MVCUI/Services/Interfaces/IArticleService.cs
@@ -6,7 +6,9 @@ namespace BlogApp.MVCUI.Services.Interfaces;
 public interface IArticleService
 {
     Task<Core.Utilities.Results.Interfaces.IResult> AddAsync(ArticleAddVM articleAddVM);
+    Task<IDataResult<List<ArticlePublishedListVM>>> GetAllPublished();
     Task<IDataResult<List<ArticleUnpublishedListVM>>> GetAllUnpublished();
+    Task<IDataResult<ArticlePublishedDetailsVM>> GetPublishedById(Guid articleId);
     Task<IDataResult<ArticleUnpublishedDetailsVM>> GetUnpublishedById(Guid articleId);
     Task<Core.Utilities.Results.Interfaces.IResult> Publish(Guid articleId);
 }

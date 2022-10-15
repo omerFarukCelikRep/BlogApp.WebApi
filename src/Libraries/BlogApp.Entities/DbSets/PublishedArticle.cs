97feb7ce319bd8bc1562717ad9abe31f9b8f1649
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-16 00:33:04 +0300

    Comments added

--- src/Libraries/BlogApp.Entities/DbSets/PublishedArticle.cs
+++ src/Libraries/BlogApp.Entities/DbSets/PublishedArticle.cs
@@ -3,10 +3,16 @@
 namespace BlogApp.Entities.DbSets;
 public class PublishedArticle : AuditableEntity
 {
+    public PublishedArticle()
+    {
+        Comments = new HashSet<Comment>();
+    }
     public DateTime PublishDate { get; set; }
     public int ReadingCount { get; set; }
     public int LikeCount { get; set; }
 
     //Navigation Prop.
     public virtual Article? Article { get; set; }
+
+    public virtual ICollection<Comment> Comments { get; set; }
 }

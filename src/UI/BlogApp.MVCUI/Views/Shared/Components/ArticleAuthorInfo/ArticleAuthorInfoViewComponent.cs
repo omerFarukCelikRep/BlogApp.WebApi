cdc374f28724061442214e51924dde95aa98d3ae
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-17 00:19:49 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/ArticleAuthorInfo/ArticleAuthorInfoViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVCUI.Views.Shared.Components.ArticleAuthorInfo;
+
+public class ArticleAuthorInfoViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(Guid userId)
+    {
+        return View();
+    }
+}

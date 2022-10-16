76257750221afdb5f20c2d13ed960d037707c91f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-17 00:15:55 +0300

    Published article map removed

--- src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/ArticleProfile.cs
@@ -7,48 +7,15 @@ public class ArticleProfile : Profile
 {
     public ArticleProfile()
     {
-        CreateMap<Article, ArticleListDto>()
-            .ForMember(dest => dest.AuthorName, x => x.MapFrom(src => $"{src.Member.FirstName} {src.Member.LastName}"));
-
-        CreateMap<PublishedArticle, ArticlePublishedListDto>()
+        CreateMap<Article, ArticleUnpublishedListDto>()
             .ForMember(
                 dest => dest.AuthorName,
-                config => config.MapFrom(src => $"{src.Article.Member.FirstName} {src.Article.Member.LastName}")
+                config => config.MapFrom(src => $"{src.Member.FirstName} {src.Member.LastName}")
             )
-           .ForMember(
-                    dest => dest.Topics,
-                    config => config.MapFrom(src => src.Article.ArticleTopics.Select(x => x.Topic.Name).ToList())
-            )
-           .ForMember(
-                dest => dest.CommentCount,
-                config => config.MapFrom(src => src.Comments.Count)
-            )
-           .ForMember(
-                dest => dest.Title,
-                config => config.MapFrom(src => src.Article.Title)
-            )
-           .ForMember(
-                dest => dest.Content,
-                config => config.MapFrom(src => src.Article.Content)
-            )
-           .ForMember(
-                dest => dest.ReadTime,
-                config => config.MapFrom(src => src.Article.ReadTime)
-            )
-           .ForMember(
-                dest => dest.Thumbnail,
-                config => config.MapFrom(src => src.Article.Thumbnail)
+            .ForMember(
+                dest => dest.Topics,
+                config => config.MapFrom(src => src.ArticleTopics.Select(x => x.Topic.Name).ToList())
             );
-
-        CreateMap<Article, ArticleUnpublishedListDto>()
-                .ForMember(
-                    dest => dest.AuthorName,
-                    config => config.MapFrom(src => $"{src.Member.FirstName} {src.Member.LastName}")
-                )
-                .ForMember(
-                    dest => dest.Topics,
-                    config => config.MapFrom(src => src.ArticleTopics.Select(x => x.Topic.Name).ToList())
-                );
 
         CreateMap<Article, ArticleUnpublishedDetailsDto>()
             .ForMember(
@@ -62,6 +29,9 @@ public class ArticleProfile : Profile
 
         CreateMap<Article, ArticleDto>();
 
-        CreateMap<ArticleCreateDto, Article>().ForSourceMember(member => member.Topics, opt => opt.DoNotValidate());
+        CreateMap<ArticleCreateDto, Article>()
+            .ForSourceMember(
+                member => member.Topics,
+                opt => opt.DoNotValidate());
     }
 }

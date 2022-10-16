4e85c86e20bc9c648b5aa7188c4ac61269959a9f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-17 00:13:48 +0300

    methods return types changed

--- src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
+++ src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
@@ -1,15 +1,16 @@
 ﻿using BlogApp.Core.Utilities.Results.Interfaces;
 using BlogApp.Entities.Dtos.Articles;
+using BlogApp.Entities.Dtos.PublishedArticles;
 
 namespace BlogApp.Business.Interfaces;
 public interface IArticleService
 {
-    Task<IDataResult<List<ArticlePublishedListDto>>> GetAllPublishedAsync();
-    Task<IDataResult<List<ArticlePublishedListDto>>> GetAllPublishedByUserIdAsync(Guid userId);
+    Task<IDataResult<List<PublishedArticleByUserListDto>>> GetAllPublishedAsync();
+    Task<IDataResult<List<PublishedArticleByUserListDto>>> GetAllPublishedByUserIdAsync(Guid userId);
     Task<IDataResult<List<ArticleDto>>> GetTrendsAsync();
-    Task<IDataResult<ArticleDto>> GetByIdAsync(Guid id);
+    Task<IDataResult<PublishedArticleDetailsDto>> GetByIdAsync(Guid id);
     Task<IResult> PublishAsync(Guid articleId);
     Task<IResult> AddAsync(ArticleCreateDto createArticleDto);
     Task<IDataResult<List<ArticleUnpublishedListDto>>> GetAllUnpublishedByUserIdAsync(Guid userId);
-    Task<IDataResult<ArticleUnpublishedDetailsDto>> GetUnpublishedById(Guid id);
+    Task<IDataResult<ArticleUnpublishedDetailsDto>> GetUnpublishedByIdAsync(Guid id);
 }

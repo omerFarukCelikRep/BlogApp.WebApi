62eaab3a15bd109f192833d7f5effaa10274aee0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-17 00:19:43 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/ArticleComments/ArticleCommentsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.MVCUI.Models.Articles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVCUI.Views.Shared.Components.ArticleComments;
+
+public class ArticleCommentsViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(Guid articleId)
+    {
+        return View(Enumerable.Empty<ArticleCommentListVM>());
+    }
+}

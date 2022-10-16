9ab247b3290e22c18835405ba31f5a5a42333314
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-17 00:16:06 +0300

    added

---
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/PublishedArticleProfile.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using BlogApp.Entities.DbSets;
+using BlogApp.Entities.Dtos.PublishedArticles;
+
+namespace BlogApp.Business.Mappings.Profiles;
+public class PublishedArticleProfile : Profile
+{
+    public PublishedArticleProfile()
+    {
+        CreateMap<PublishedArticle, PublishedArticleByUserListDto>()
+            .ForMember(
+                dest => dest.AuthorName,
+                config => config.MapFrom(src => $"{src.Article.Member.FirstName} {src.Article.Member.LastName}")
+            )
+           .ForMember(
+                    dest => dest.Topics,
+                    config => config.MapFrom(src => src.Article.ArticleTopics.Select(x => x.Topic.Name).ToList())
+            )
+           .ForMember(
+                dest => dest.CommentCount,
+                config => config.MapFrom(src => src.Comments.Count)
+            )
+           .ForMember(
+                dest => dest.Title,
+                config => config.MapFrom(src => src.Article.Title)
+            )
+           .ForMember(
+                dest => dest.Content,
+                config => config.MapFrom(src => src.Article.Content)
+            )
+           .ForMember(
+                dest => dest.ReadTime,
+                config => config.MapFrom(src => src.Article.ReadTime)
+            )
+           .ForMember(
+                dest => dest.Thumbnail,
+                config => config.MapFrom(src => src.Article.Thumbnail)
+            );
+
+        CreateMap<PublishedArticle, PublishedArticleDetailsDto>()
+            .ForMember(
+                dest => dest.ReadTime,
+                config => config.MapFrom(src => src.Article.ReadTime)
+            )
+            .ForMember(
+                dest => dest.Title,
+                config => config.MapFrom(src => src.Article.Title)
+            )
+            .ForMember(
+                dest => dest.Content,
+                config => config.MapFrom(src => src.Article.Content)
+            )
+            .ForMember(
+                dest => dest.Thumbnail,
+                config => config.MapFrom(src => src.Article.Thumbnail)
+            )
+            .ForMember(
+                dest => dest.AuthorName,
+                config => config.MapFrom(src => $"{src.Article.Member.FirstName} {src.Article.Member.LastName}")
+            )
+            .ForMember(
+                dest => dest.UserId,
+                config => config.MapFrom(src => src.Article.MemberId)
+            )
+            .ForMember(
+                dest => dest.CommentCount,
+                config => config.MapFrom(src => src.Comments.Count)
+            )
+            .ForMember(
+                    dest => dest.Topics,
+                    config => config.MapFrom(src => src.Article.ArticleTopics.Select(x => x.Topic.Name).ToList())
+            );
+    }
+}

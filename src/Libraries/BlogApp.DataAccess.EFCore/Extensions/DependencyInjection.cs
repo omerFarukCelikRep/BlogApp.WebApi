4a7acd8cc6e262b22b54cd7410168481bfdf89bf
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-20 01:12:19 +0300

    class name changed

--- src/Libraries/BlogApp.DataAccess.EFCore/Extensions/DependencyInjection.cs
+++ src/Libraries/BlogApp.DataAccess.EFCore/Extensions/DependencyInjection.cs
@@ -8,7 +8,7 @@ public static class DependencyInjection
     public static IServiceCollection AddDataAccessEFCoreServices(this IServiceCollection services)
     {
         services.AddTransient<ITopicRepository, TopicRepository>();
-        services.AddTransient<IUserRepository, MemberRepository>();
+        services.AddTransient<IUserRepository, UserRepository>();
         services.AddTransient<IRefreshTokenRepository, RefreshTokenRepository>();
         services.AddTransient<IArticleRepository, ArticleRepository>();
         services.AddTransient<IPublishedArticleRepository, PublishedArticleRepository>();

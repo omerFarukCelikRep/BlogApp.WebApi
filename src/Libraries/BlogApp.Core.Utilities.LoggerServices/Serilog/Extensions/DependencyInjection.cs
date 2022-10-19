d6c42c60d985da0a13ec49343be096617345eea5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-20 01:11:29 +0300

    serilog added

---
+++ src/Libraries/BlogApp.Core.Utilities.LoggerServices/Serilog/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace BlogApp.Core.Utilities.LoggerServices.Serilog.Extensions;
+public static class DependencyInjection
+{
+    public static IHostBuilder UseCustomSerilog(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.UseSerilog(
+            (context, loggerConfiguration) =>
+                loggerConfiguration.ReadFrom.Configuration(context.Configuration)
+        );
+
+        return hostBuilder;
+    }
+}

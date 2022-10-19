1cb5d029986b81bd4d424aaf8e623262d14b4512
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-20 01:08:57 +0300

    renamed

--- src/Libraries/BlogApp.Business/Concrete/UserService.cs
+++ src/Libraries/BlogApp.Business/Concrete/UserService.cs
@@ -1,160 +1,55 @@
-﻿using BlogApp.Authentication.Constants;
-using BlogApp.Authentication.Dtos.Incoming;
-using BlogApp.Authentication.Dtos.Outgoing;
-using BlogApp.Authentication.Services.Abstract;
-using BlogApp.Business.Constants;
+﻿using BlogApp.Business.Constants;
 using BlogApp.Business.Interfaces;
 using BlogApp.Business.Mappings.Mapper;
-using BlogApp.Core.Utilities.Constants;
 using BlogApp.Core.Utilities.Results.Concrete;
 using BlogApp.Core.Utilities.Results.Interfaces;
 using BlogApp.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-using Microsoft.AspNetCore.Identity;
-using System.Text;
+using BlogApp.Entities.Dtos.Users;
 
 namespace BlogApp.Business.Concrete;
 public class UserService : IUserService
 {
-    private readonly UserManager<IdentityUser<Guid>> _userManager;
-    private readonly IUserRepository _memberRepository;
-    private readonly ITokenService _tokenService;
+    private readonly IUserRepository _userRepository;
 
-    public UserService(UserManager<IdentityUser<Guid>> userManager, IUserRepository memberRepository, ITokenService tokenService)
+    public UserService(IUserRepository userRepository)
     {
-        _userManager = userManager;
-        _memberRepository = memberRepository;
-        _tokenService = tokenService;
+        _userRepository = userRepository;
     }
-    public async Task<AuthResult> AddAsync(UserRegistrationRequestDto registrationRequestDto)
+
+    public async Task<IDataResult<List<UserDto>>> GetAllAsync()
     {
-        var identityCreateResult = await AddIdentityUser(registrationRequestDto);
-        if (!identityCreateResult.IsSuccess)
+        var members = await _userRepository.GetAllAsync(false);
+
+        if (!members.Any())
         {
-            return new AuthResult(false, identityCreateResult.Message);
+            return new ErrorDataResult<List<UserDto>>(ServiceMessages.UserNotFound);
         }
 
-        var member = await AddMember(registrationRequestDto, identityCreateResult.Data.Id);
-
-        var jwtToken = _tokenService.GenerateJwtToken(identityCreateResult.Data, member.Id);
-        var refreshToken = await _tokenService.GenerateRefreshTokenAsync(identityCreateResult.Data, registrationRequestDto.IpAddress);
-        await _memberRepository.SaveChangesAsync();
-
-        return new AuthResult
-        {
-            Success = true,
-            Token = jwtToken,
-            RefreshToken = refreshToken.Token
-        };
+        return new SuccessDataResult<List<UserDto>>(ObjectMapper.Mapper.Map<List<UserDto>>(members), ServiceMessages.UsersListed);
     }
 
-    public async Task<AuthResult> AuthenticateAsync(UserLoginRequestDto loginRequestDto, string ipAddress)
+    public async Task<IDataResult<UserDto>> GetByIdAsync(Guid id)
     {
-        var findUserResult = await FindByEmailAsync(loginRequestDto.Email);
-        if (!findUserResult.IsSuccess)
+        var member = await _userRepository.GetByIdAsync(id, false);
+
+        if (member is null)
         {
-            return new AuthResult(false, AuthenticationMessages.InvalidRequest);
+            return new ErrorDataResult<UserDto>(ServiceMessages.UserNotFound);
         }
 
-        var checkPasswordResult = await _userManager.CheckPasswordAsync(findUserResult.Data, loginRequestDto.Password);
-        if (!checkPasswordResult)
-        {
-            return new AuthResult(false, AuthenticationMessages.InvalidRequest);
-        }
-
-        var member = await _memberRepository.GetByIdentityId(findUserResult.Data.Id);
-
-        var jwtToken = _tokenService.GenerateJwtToken(findUserResult.Data, member.Id);
-        var refreshToken = await _tokenService.GetActiveRefreshTokenAsync(findUserResult.Data)
-                           ?? await _tokenService.GenerateRefreshTokenAsync(findUserResult.Data, ipAddress);
-
-        return new AuthResult
-        {
-            Success = true,
-            Token = jwtToken,
-            RefreshToken = refreshToken.Token
-        };
+        return new SuccessDataResult<UserDto>(ObjectMapper.Mapper.Map<UserDto>(member), ServiceMessages.UserGetted);
     }
 
-    public async Task<IDataResult<IdentityUser<Guid>>> FindByEmailAsync(string email)
+    public async Task<IDataResult<UserDto>> UpdateAsync(UserUpdateDto updateMemberDto)
     {
-        var user = await _userManager.FindByEmailAsync(email);
-        return user == null ? new ErrorDataResult<IdentityUser<Guid>>(ServiceMessages.UserNotFound) : new SuccessDataResult<IdentityUser<Guid>>(user); //TODO:Magic string
-    }
+        var member = await _userRepository.GetByIdAsync(updateMemberDto.Id);
 
-    public async Task<AuthResult> RefreshTokenAsync(TokenRequestDto tokenRequestDto)
-    {
-        var identityUserId = await _tokenService.ValidateJwtTokenAsync(tokenRequestDto.Token);
-        var verifyResult = await _tokenService.VerifyTokenAsync(tokenRequestDto);
-        if (!verifyResult.Success)
-        {
-            return verifyResult;
-        }
+        var mappedMember = ObjectMapper.Mapper.Map(updateMemberDto, member);
 
-        bool markedAsUsed = await _tokenService.UpdateRefreshTokenAsUsedAsync(tokenRequestDto.RefreshToken);
-        if (!markedAsUsed)
-        {
-            return new AuthResult(false, ExceptionMessages.SomethingWentWrong);
-        }
+        var updatedMember = await _userRepository.UpdateAsync(mappedMember);
 
-        var user = await _userManager.FindByIdAsync(identityUserId.ToString());
-        var member = await _memberRepository.GetByIdentityId(identityUserId.Value);
-        var jwtToken = _tokenService.GenerateJwtToken(user, member.Id);
-        var refreshToken = await _tokenService.GenerateRefreshTokenAsync(user, tokenRequestDto.IpAddress);
+        _ = await _userRepository.SaveChangesAsync();
 
-        return new AuthResult
-        {
-            Success = true,
-            Token = jwtToken,
-            RefreshToken = refreshToken.Token
-        };
-    }
-
-    public async Task<Guid> GetUserIdByIdentityIdAsync(Guid identityId)
-    {
-        return (await _memberRepository.GetByIdentityId(identityId)).Id;
-    }
-
-    private async Task<User> AddMember(UserRegistrationRequestDto registrationRequestDto, Guid identityId)
-    {
-        var member = ObjectMapper.Mapper.Map<User>(registrationRequestDto);
-
-        member.IdentityId = identityId;
-
-        return await _memberRepository.AddAsync(member);
-    }
-
-    private async Task<IDataResult<IdentityUser<Guid>>> AddIdentityUser(UserRegistrationRequestDto registrationRequestDto)
-    {
-        var identityUser = new IdentityUser<Guid>
-        {
-            Email = registrationRequestDto.Email,
-            EmailConfirmed = true, //TODO: EMail servisiyle beraber güncellenecek 
-            UserName = registrationRequestDto.Email,
-
-        };
-
-        var isCreated = await _userManager.CreateAsync(identityUser, registrationRequestDto.Password);
-
-        if (!isCreated.Succeeded)
-        {
-            return new ErrorDataResult<IdentityUser<Guid>>(ConcatIdentityErrors(isCreated.Errors));
-        }
-
-        return new SuccessDataResult<IdentityUser<Guid>>(identityUser);
-    }
-
-    private string ConcatIdentityErrors(IEnumerable<IdentityError> identityErrors)
-    {
-        StringBuilder stringBuilder = new();
-
-        foreach (var identityError in identityErrors)
-        {
-            stringBuilder.Append(identityError.Code);
-            stringBuilder.Append(" : ");
-            stringBuilder.AppendLine(identityError.Description);
-        }
-
-        return stringBuilder.ToString();
+        return new SuccessDataResult<UserDto>(ObjectMapper.Mapper.Map<UserDto>(updatedMember), ServiceMessages.UserUpdateSuccess);
     }
 }

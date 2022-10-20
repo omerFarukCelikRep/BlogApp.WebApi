4b59983cb6d11fc9f1a2dcf9840a72f87c14c31b
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-21 01:27:10 +0300

    added

---
+++ src/Libraries/BlogApp.API/Responses/IndividualHealthCheckResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApp.API.Responses;
+
+public class IndividualHealthCheckResponse
+{
+    public string Status { get; set; } = string.Empty;
+    public string Component { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}

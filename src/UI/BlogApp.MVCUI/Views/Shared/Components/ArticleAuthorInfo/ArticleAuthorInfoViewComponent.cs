9429d4e5479430bba5066b628a7bfc1a04f48d30
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-21 01:30:39 +0300

    data call added

--- src/UI/BlogApp.MVCUI/Views/Shared/Components/ArticleAuthorInfo/ArticleAuthorInfoViewComponent.cs
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/ArticleAuthorInfo/ArticleAuthorInfoViewComponent.cs
@@ -1,12 +1,21 @@
 ﻿using BlogApp.MVCUI.Models.Articles;
+using BlogApp.MVCUI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.MVCUI.Views.Shared.Components.ArticleAuthorInfo;
 
 public class ArticleAuthorInfoViewComponent : ViewComponent
 {
+    private readonly IUserService _userService;
+
+    public ArticleAuthorInfoViewComponent(IUserService userService)
+    {
+        _userService = userService;
+    }
+
     public async Task<IViewComponentResult> InvokeAsync(Guid userId)
     {
-        return View(new ArticleAuthorInfoVM());
+        var result = await _userService.GetArticleUserInfo(userId);
+        return View(result.IsSuccess ? result.Data : new ArticleAuthorInfoVM());
     }
 }

c6713bd84927c19842434cd7695634ee03fc653e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-21 01:29:45 +0300

    properties added

--- src/UI/BlogApp.MVCUI/Models/Articles/ArticleAuthorInfoVM.cs
+++ src/UI/BlogApp.MVCUI/Models/Articles/ArticleAuthorInfoVM.cs
@@ -2,4 +2,7 @@
 
 public class ArticleAuthorInfoVM
 {
+    public string Image { get; set; } = string.Empty;
+    public string AuthorName { get; set; } = string.Empty;
+    public string Biography { get; set; } = string.Empty;
 }

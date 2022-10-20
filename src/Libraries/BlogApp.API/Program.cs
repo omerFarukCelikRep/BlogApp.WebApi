199248fcf9d6651d7f6dd74339ec370f9b0d490d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-21 01:27:28 +0300

    HealthCheckOptions added

--- src/Libraries/BlogApp.API/Program.cs
+++ src/Libraries/BlogApp.API/Program.cs
@@ -1,10 +1,12 @@
 ﻿using BlogApp.API.Extensions;
 using BlogApp.API.Middlewares;
+using BlogApp.API.Responses;
 using BlogApp.Authentication.Extensions;
 using BlogApp.Business.Extensions;
 using BlogApp.Core.Utilities.LoggerServices.Serilog.Extensions;
 using BlogApp.DataAccess.EFCore.Extensions;
 using BlogApp.DataAccess.Extensions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,7 +28,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapHealthChecks("/healthz");
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var response = new HealthCheckReponse
+        {
+            Status = report.Status.ToString(),
+            HealthChecks = report.Entries.Select(x => new IndividualHealthCheckResponse
+            {
+                Component = x.Key,
+                Status = x.Value.Status.ToString(),
+                Description = x.Value.Description
+            }),
+            HealthCheckDuration = report.TotalDuration
+        };
+        await context.Response.WriteAsJsonAsync(response);
+    }
+});
 
 app.UseMiddleware<ErrorHandlerMiddleware>();
 

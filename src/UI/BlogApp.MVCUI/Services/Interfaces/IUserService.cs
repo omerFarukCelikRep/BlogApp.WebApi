62c322d0b38bb1dcfb6b57b527a98ab80f55754b
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-21 01:30:01 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Core.Utilities.Results.Interfaces;
+using BlogApp.MVCUI.Models.Articles;
+
+namespace BlogApp.MVCUI.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<IDataResult<ArticleAuthorInfoVM>> GetArticleUserInfo(Guid userId);
+}

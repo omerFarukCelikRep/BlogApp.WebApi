fbcf424a43ae373f39a17f36b64dc4017d9af7d5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-25 00:52:03 +0300

    CommentService registration added

--- src/Libraries/BlogApp.Business/Extensions/DependencyInjection.cs
+++ src/Libraries/BlogApp.Business/Extensions/DependencyInjection.cs
@@ -15,6 +15,7 @@ public static class DependencyInjection
         services.AddScoped<ITopicService, TopicService>();
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IArticleService, ArticleService>();
+        services.AddScoped<ICommentService, CommentService>();
 
         return services;
     }

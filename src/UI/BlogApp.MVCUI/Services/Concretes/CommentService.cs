2c153c5a6c2400a4874fae27d650cd488e8b39f2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-24 23:08:26 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Services/Concretes/CommentService.cs
@@ -0,0 +1,33 @@
+﻿using BlogApp.Core.Utilities.Results.Concrete;
+using BlogApp.MVCUI.Models.Comments;
+using BlogApp.MVCUI.Services.Interfaces;
+using System.Net;
+using IResult = BlogApp.Core.Utilities.Results.Interfaces.IResult;
+
+namespace BlogApp.MVCUI.Services.Concretes;
+
+public class CommentService : ICommentService
+{
+    private readonly HttpClient _httpClient;
+    public CommentService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IResult> AddAsync(CommentAddVM commentAddVM)
+    {
+        var responseMessage = await _httpClient.PostAsJsonAsync("/api/v1/Comments", commentAddVM);
+        if (responseMessage is null)
+        {
+            return new ErrorResult("İşlem Başarısız"); //TODO: Magic string
+        }
+
+        var response = await responseMessage.Content.ReadFromJsonAsync<DataResult<CommentAddVM>>();
+        if (responseMessage.StatusCode == HttpStatusCode.BadRequest || !responseMessage.IsSuccessStatusCode)
+        {
+            return new ErrorResult($"{responseMessage.ReasonPhrase} - {response.Message}");
+        }
+
+        return new SuccessResult();
+    }
+}

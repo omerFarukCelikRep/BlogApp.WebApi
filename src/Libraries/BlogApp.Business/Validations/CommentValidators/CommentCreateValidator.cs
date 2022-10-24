c1970dbb80d2d2d9d3d2238dfe943ae3a1386c59
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-25 00:49:58 +0300

    added

---
+++ src/Libraries/BlogApp.Business/Validations/CommentValidators/CommentCreateValidator.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.Entities.Dtos.Comments;
+using FluentValidation;
+
+namespace BlogApp.Business.Validations.CommentValidators;
+public class CommentCreateValidator : AbstractValidator<CommentCreateDto>
+{
+    public CommentCreateValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotNull().WithMessage("Boş Geçilemez") //TODO:Magic String
+            .NotEmpty().WithMessage("Boş Geçilemez") //TODO:Magic String
+            .MaximumLength(512);
+
+        RuleFor(x => x.Text)
+        .NotNull().WithMessage("Boş Geçilemez") //TODO:Magic String
+        .NotEmpty().WithMessage("Boş Geçilemez"); //TODO:Magic String
+
+        RuleFor(x=>x.ArticleId)
+            .NotNull().WithMessage("Boş Geçilemez"); //TODO:Magic String
+    }
+}

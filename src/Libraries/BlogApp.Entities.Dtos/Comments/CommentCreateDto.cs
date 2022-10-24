5d9da58e857edfd9f963d994290ff437fb2d11a0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-25 00:50:13 +0300

    added

---
+++ src/Libraries/BlogApp.Entities.Dtos/Comments/CommentCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Entities.Dtos.Comments;
+public class CommentCreateDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public Guid? UserId { get; set; }
+    public Guid ArticleId { get; set; }
+    public string UserIpAdress { get; set; } = string.Empty;
+}

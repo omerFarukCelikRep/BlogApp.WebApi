579f6e3d34432ad0e35ad10ef572b682d476de5c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-25 00:51:19 +0300

    Add action added

--- src/Libraries/BlogApp.API/Controllers/v1/CommentsController.cs
+++ src/Libraries/BlogApp.API/Controllers/v1/CommentsController.cs
@@ -1,5 +1,35 @@
-﻿namespace BlogApp.API.Controllers.v1;
+﻿using BlogApp.Business.Interfaces;
+using BlogApp.Entities.Dtos.Comments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers.v1;
 
 public class CommentsController : BaseController
 {
+    private readonly ICommentService _commentService;
+    public CommentsController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Add([FromBody] CommentCreateDto commentCreateDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if (User.Identity.IsAuthenticated)
+        {
+            commentCreateDto.UserId = UserId;
+        }
+
+        commentCreateDto.UserIpAdress = GetIpAddress();
+        var result = await _commentService.AddAsync(commentCreateDto);
+
+        return GetDataResult(result);
+    }
 }

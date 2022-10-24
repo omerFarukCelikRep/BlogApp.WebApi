ee1d89a21fd98ba7d115bbd9f53a17dc525a8183
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-25 00:51:43 +0300

    AddAsync method added

--- src/Libraries/BlogApp.Business/Concrete/CommentService.cs
+++ src/Libraries/BlogApp.Business/Concrete/CommentService.cs
@@ -1,12 +1,34 @@
 ﻿using BlogApp.Business.Interfaces;
+using BlogApp.Business.Mappings.Mapper;
+using BlogApp.Core.Utilities.Results.Concrete;
+using BlogApp.Core.Utilities.Results.Interfaces;
 using BlogApp.DataAccess.Interfaces.Repositories;
+using BlogApp.Entities.DbSets;
+using BlogApp.Entities.Dtos.Comments;
 
 namespace BlogApp.Business.Concrete;
 public class CommentService : ICommentService
 {
     private readonly ICommentRepository _commentRepository;
-    public CommentService(ICommentRepository commentRepository)
+    private readonly IUserRepository _userRepository;
+    public CommentService(ICommentRepository commentRepository, IUserRepository userRepository)
     {
         _commentRepository = commentRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task<IDataResult<CommentCreatedDto>> AddAsync(CommentCreateDto commentCreateDto)
+    {
+        var comment = ObjectMapper.Mapper.Map<Comment>(commentCreateDto);
+        if (commentCreateDto.UserId.HasValue)
+        {
+            var user = await _userRepository.GetByIdAsync(commentCreateDto.UserId.Value, false);
+            comment.UserName = $"{user.FirstName} {user.LastName}";
+        }
+
+        await _commentRepository.AddAsync(comment);
+        await _commentRepository.SaveChangesAsync();
+
+        return new SuccessDataResult<CommentCreatedDto>(ObjectMapper.Mapper.Map<CommentCreatedDto>(comment));
     }
 }

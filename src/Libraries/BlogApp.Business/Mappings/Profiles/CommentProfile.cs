ff787e3f204c3b6b71875241a66b16def9f53168
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-25 00:49:45 +0300

    added

---
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/CommentProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BlogApp.Entities.DbSets;
+using BlogApp.Entities.Dtos.Comments;
+
+namespace BlogApp.Business.Mappings.Profiles;
+public class CommentProfile : Profile
+{
+    public CommentProfile()
+    {
+        CreateMap<CommentCreateDto, Comment>();
+        CreateMap<Comment, CommentCreatedDto>();
+    }
+}

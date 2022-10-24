4b25a2e2064bdffba0bfd60c6e4d94eb5a507fce
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-24 23:07:39 +0300

    added

---
+++ src/Libraries/BlogApp.Business/Concrete/CommentService.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Business.Interfaces;
+using BlogApp.DataAccess.Interfaces.Repositories;
+
+namespace BlogApp.Business.Concrete;
+public class CommentService : ICommentService
+{
+    private readonly ICommentRepository _commentRepository;
+    public CommentService(ICommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+}

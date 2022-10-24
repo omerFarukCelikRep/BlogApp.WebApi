8b78f0889a196a15d73bb62d8e5aee09cc4707fe
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-24 23:08:43 +0300

    properties and attributes added

--- src/UI/BlogApp.MVCUI/Models/Comments/CommentAddVM.cs
+++ src/UI/BlogApp.MVCUI/Models/Comments/CommentAddVM.cs
@@ -1,5 +1,18 @@
-﻿namespace BlogApp.MVCUI.Models.Comments;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.MVCUI.Models.Comments;
 
 public class CommentAddVM
 {
+    [Required]
+    public Guid ArticleId { get; set; }
+
+    [Required]
+    [Display(Name = "Name")]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [Display(Name = "Comment")]
+    public string Text { get; set; } = string.Empty;
+
 }

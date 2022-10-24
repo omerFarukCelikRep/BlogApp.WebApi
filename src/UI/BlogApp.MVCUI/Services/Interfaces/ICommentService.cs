4dc88643c5bed6e96d496abf3b72de78d695be8e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-24 23:08:19 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,8 @@
+﻿using BlogApp.MVCUI.Models.Comments;
+
+namespace BlogApp.MVCUI.Services.Interfaces;
+
+public interface ICommentService
+{
+    Task<Core.Utilities.Results.Interfaces.IResult> AddAsync(CommentAddVM commentAddVM);
+}

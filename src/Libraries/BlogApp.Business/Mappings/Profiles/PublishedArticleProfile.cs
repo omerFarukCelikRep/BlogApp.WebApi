30d622f05b29a83167db7237754acc3c6e2de571
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-27 01:06:06 +0300

    PublishedArticleListDto map added

--- src/Libraries/BlogApp.Business/Mappings/Profiles/PublishedArticleProfile.cs
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/PublishedArticleProfile.cs
@@ -37,6 +37,36 @@ public class PublishedArticleProfile : Profile
                 config => config.MapFrom(src => src.Article.Thumbnail)
             );
 
+        CreateMap<PublishedArticle, PublishedArticleListDto>()
+            .ForMember(
+                dest => dest.AuthorName,
+                config => config.MapFrom(src => $"{src.Article.User.FirstName} {src.Article.User.LastName}")
+            )
+           .ForMember(
+                    dest => dest.Topics,
+                    config => config.MapFrom(src => src.Article.ArticleTopics.Select(x => x.Topic.Name).ToList())
+            )
+           .ForMember(
+                dest => dest.CommentCount,
+                config => config.MapFrom(src => src.Comments.Count)
+            )
+           .ForMember(
+                dest => dest.Title,
+                config => config.MapFrom(src => src.Article.Title)
+            )
+           .ForMember(
+                dest => dest.Content,
+                config => config.MapFrom(src => src.Article.Content)
+            )
+           .ForMember(
+                dest => dest.ReadTime,
+                config => config.MapFrom(src => src.Article.ReadTime)
+            )
+           .ForMember(
+                dest => dest.Thumbnail,
+                config => config.MapFrom(src => src.Article.Thumbnail)
+            );
+
         CreateMap<PublishedArticle, PublishedArticleDetailsDto>()
             .ForMember(
                 dest => dest.ReadTime,

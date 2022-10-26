9edaaaa47d4c70c4f07bbadeb8f10b2f4ec9485e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-27 01:06:10 +0300

    added

---
+++ src/Libraries/BlogApp.Entities.Dtos/PublishedArticles/PublishedArticleListDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogApp.Entities.Dtos.PublishedArticles;
+public class PublishedArticleListDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int ReadTime { get; set; }
+    public string? Thumbnail { get; set; }
+    public string AuthorName { get; set; }
+    public DateTime PublishDate { get; set; }
+    public int ReadingCount { get; set; }
+    public int LikeCount { get; set; }
+    public int CommentCount { get; set; }
+    public List<string> Topics { get; set; }
+}

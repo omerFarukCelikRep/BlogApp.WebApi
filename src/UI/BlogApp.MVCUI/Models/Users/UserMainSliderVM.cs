abc1c6a3c523a382aa73f046d856e8e54a9e69b9
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-29 01:04:04 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Models/Users/UserMainSliderVM.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApp.MVCUI.Models.Users;
+
+public class UserMainSliderVM
+{
+    public string Title { get; set; }
+    public string Thumbnail { get; set; }
+    public List<string> Topics { get; set; }
+}

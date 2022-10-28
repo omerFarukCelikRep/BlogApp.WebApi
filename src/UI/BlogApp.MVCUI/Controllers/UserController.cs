85da5dd9489c60356d8bb5f20571f2376a3a1958
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-29 01:03:59 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.MVCUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVCUI.Controllers;
+public class UserController : BaseController
+{
+    private readonly IUserService _userService;
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public IActionResult Main()
+    {
+        return View();
+    }
+}

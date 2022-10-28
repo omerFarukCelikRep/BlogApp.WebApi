e0d36aaa2090dfd45c7880023711b5c8370cd1b4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-29 01:04:14 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/UserMainSlider/UserMainSliderViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVCUI.Views.Shared.Components.UserMainSlider;
+
+public class UserMainSliderViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View();
+    }
+}

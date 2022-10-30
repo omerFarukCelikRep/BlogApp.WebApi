ae93e6c86f3163f9970696e1565fb5b1289d14e8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-30 22:46:55 +0300

    service added

--- src/UI/BlogApp.MVCUI/Views/Shared/Components/UserMainSlider/UserMainSliderViewComponent.cs
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/UserMainSlider/UserMainSliderViewComponent.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogApp.MVCUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.MVCUI.Views.Shared.Components.UserMainSlider;
 
 public class UserMainSliderViewComponent : ViewComponent
 {
+    private readonly IArticleService _articleService;
+    public UserMainSliderViewComponent(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View();
+        var result = await _articleService.GetAllPublishedShortDetailsRandomly();
+        return View(result?.Data);
     }
 }

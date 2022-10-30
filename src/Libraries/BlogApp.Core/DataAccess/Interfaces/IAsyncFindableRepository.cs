67542f7cc8eeabb70a9faed7ef7b7f9ab7715068
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-30 22:43:45 +0300

    method return type changed to nullable

--- src/Libraries/BlogApp.Core/DataAccess/Interfaces/IAsyncFindableRepository.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Interfaces/IAsyncFindableRepository.cs
@@ -4,7 +4,7 @@ using System.Linq.Expressions;
 namespace BlogApp.Core.DataAccess.Interfaces;
 public interface IAsyncFindableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
 {
-    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression, bool tracking = true);
-    Task<TEntity> GetByIdAsync(Guid id, bool tracking = true);
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression, bool tracking = true);
+    Task<TEntity?> GetByIdAsync(Guid id, bool tracking = true);
     Task<bool> AnyAsync(Expression<Func<TEntity, bool>>? expression = null);
 }

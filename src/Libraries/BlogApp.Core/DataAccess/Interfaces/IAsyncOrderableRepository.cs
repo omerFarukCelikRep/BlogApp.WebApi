5bb064545c84eb5039a601df596c8b2cbd755fbf
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-30 22:44:49 +0300

    GetAllAsync methods declared with take parameters

--- src/Libraries/BlogApp.Core/DataAccess/Interfaces/IAsyncOrderableRepository.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Interfaces/IAsyncOrderableRepository.cs
@@ -5,5 +5,7 @@ namespace BlogApp.Core.DataAccess.Interfaces;
 public interface IAsyncOrderableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
 {
     Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true);
+    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true);
     Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, bool tracking = true);
+    Task<IEnumerable<TEntity>> GetAllAsync<TKey>(Expression<Func<TEntity, bool>> expression, Expression<Func<TEntity, TKey>> orderby, bool orderDesc = false, int takeCount = 0, bool tracking = true);
 }

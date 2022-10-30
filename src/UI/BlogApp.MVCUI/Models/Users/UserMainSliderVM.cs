339ef7fde5522cb96be874e1967e9cc45b49bc70
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-30 22:45:27 +0300

    id property added

--- src/UI/BlogApp.MVCUI/Models/Users/UserMainSliderVM.cs
+++ src/UI/BlogApp.MVCUI/Models/Users/UserMainSliderVM.cs
@@ -2,6 +2,7 @@
 
 public class UserMainSliderVM
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string Thumbnail { get; set; }
     public List<string> Topics { get; set; }

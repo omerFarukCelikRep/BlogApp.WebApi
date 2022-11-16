b78d0c7fefb690b8d931f18c0852ed0ec969b8f8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-16 20:54:05 +0300

    added

---
+++ src/UI/BlogApp.MVCUI/Filters/AllowAnonymousFilter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BlogApp.MVCUI.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AllowAnonymousFilter : AllowAnonymousAttribute, IAllowAnonymous, IAsyncAuthorizationFilter
+{
+    public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        return Task.CompletedTask;
+    }
+}

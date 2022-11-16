d11f6263816c263bc2cac5e1623200785c259a01
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-16 20:52:15 +0300

    AuthorizationFilter attribute added

--- src/UI/BlogApp.MVCUI/Controllers/BaseController.cs
+++ src/UI/BlogApp.MVCUI/Controllers/BaseController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogApp.MVCUI.Filters;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.MVCUI.Controllers;
+
+[AuthorizationFilter]
 public class BaseController : Controller
 {
 }

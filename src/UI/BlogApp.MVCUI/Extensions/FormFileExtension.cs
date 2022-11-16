ee8758f44f51b69f5d42dbc8c8501fcac79a5ab5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-16 20:54:01 +0300

    using changed

--- src/UI/BlogApp.MVCUI/Extensions/FormFileExtension.cs
+++ src/UI/BlogApp.MVCUI/Extensions/FormFileExtension.cs
@@ -4,11 +4,9 @@ public static class FormFileExtension
 {
     public static async Task<string> FileToString(this IFormFile file)
     {
-        using (var stream = new MemoryStream())
-        {
-            await file.CopyToAsync(stream);
-            var fileAsByteArray = stream.ToArray();
-            return Convert.ToBase64String(fileAsByteArray);
-        }
+        using var stream = new MemoryStream();
+        await file.CopyToAsync(stream);
+        var fileAsByteArray = stream.ToArray();
+        return Convert.ToBase64String(fileAsByteArray);
     }
 }

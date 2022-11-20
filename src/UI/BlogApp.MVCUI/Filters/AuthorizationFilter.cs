8b206e059d2cfef8412b51ed9c89cf5178e83502
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-20 22:39:05 +0300

    request path added login action parameter

--- src/UI/BlogApp.MVCUI/Filters/AuthorizationFilter.cs
+++ src/UI/BlogApp.MVCUI/Filters/AuthorizationFilter.cs
@@ -9,10 +9,11 @@ public class AuthorizationFilter : Attribute, IAsyncAuthorizationFilter
     public Task OnAuthorizationAsync(AuthorizationFilterContext context)
     {
         bool isAnonymous = context.Filters.Any(x => x.GetType() == typeof(AllowAnonymousFilter));
-        if (!context.HttpContext.User.Identity.IsAuthenticated && !isAnonymous)
+        if (!context.HttpContext!.User.Identity.IsAuthenticated && !isAnonymous)
         {
+            string path = context.HttpContext.Request.Path;
             context.Result = new RedirectToRouteResult(
-                new RouteValueDictionary(new { controller = "Home", action = "Login" })
+                new RouteValueDictionary(new { controller = "Home", action = "Login", returnUrl = path })
             );
         }
 

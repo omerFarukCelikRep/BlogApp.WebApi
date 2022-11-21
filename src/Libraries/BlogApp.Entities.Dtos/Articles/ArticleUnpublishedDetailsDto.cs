33b08ee7ded228789f180b704a529911e49eeff0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-21 23:07:30 +0300

    initial value added

--- src/Libraries/BlogApp.Entities.Dtos/Articles/ArticleUnpublishedDetailsDto.cs
+++ src/Libraries/BlogApp.Entities.Dtos/Articles/ArticleUnpublishedDetailsDto.cs
@@ -2,11 +2,11 @@
 public class ArticleUnpublishedDetailsDto
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
     public int ReadTime { get; set; }
     public string? Thumbnail { get; set; }
     public DateTime CreatedDate { get; set; }
     public List<string> Topics { get; set; }
-    public string AuthorName { get; set; }
+    public string AuthorName { get; set; } = string.Empty;
 }

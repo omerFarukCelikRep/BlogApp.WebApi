37d973c0c7b9f4333d7d435d81b8f8804dcf4793
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-28 23:56:02 +0300

    added

---
+++ src/Libraries/BlogApp.Core/DataAccess/Interfaces/Models/IPaginate.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.Core.DataAccess.Interfaces.Models;
+public interface IPaginate<TModel>
+{
+    int From { get; }
+    int Index { get; }
+    int Size { get; }
+    int Count { get; }
+    int Pages { get; }
+    IReadOnlyCollection<TModel> Items { get; }
+    bool HasPrevious { get; }
+    bool HasNext { get; }
+}

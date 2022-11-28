670a413b5218eb8bca9ae1865ef2fe3821f7c5d7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-28 23:54:50 +0300

    added

---
+++ src/Libraries/BlogApp.Core/DataAccess/Extensions/IQueryablePaginateExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Core.DataAccess.Extensions;
+public static class IQueryablePaginateExtensions
+{
+    public static async Task<>
+}

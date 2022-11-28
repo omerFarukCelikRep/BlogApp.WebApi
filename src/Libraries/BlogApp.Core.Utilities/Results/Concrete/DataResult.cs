4c5c3f3d2acb9c39d83bed22e95ebbe8d61a89d9
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-28 23:00:02 +0300

    Data marked as nullable

--- src/Libraries/BlogApp.Core.Utilities/Results/Concrete/DataResult.cs
+++ src/Libraries/BlogApp.Core.Utilities/Results/Concrete/DataResult.cs
@@ -4,14 +4,14 @@ using System.Text.Json.Serialization;
 namespace BlogApp.Core.Utilities.Results.Concrete;
 public class DataResult<T> : Result, IDataResult<T>
 {
-    public T Data { get; init; }
-    public DataResult(T data, bool isSuccess) : base(isSuccess)
+    public T? Data { get; init; }
+    public DataResult(T? data, bool isSuccess) : base(isSuccess)
     {
         Data = data;
     }
 
     [JsonConstructor]
-    public DataResult(T data, bool isSuccess, string message) : base(isSuccess, message)
+    public DataResult(T? data, bool isSuccess, string message) : base(isSuccess, message)
     {
         Data = data;
     }

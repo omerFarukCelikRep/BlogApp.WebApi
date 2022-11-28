af8220210c6d0487452b97d63c49338c2f45910f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-28 23:56:48 +0300

    namespace changed

--- src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IArticleRepository.cs
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IArticleRepository.cs
@@ -1,4 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Interfaces;
+﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
 using BlogApp.Entities.DbSets;
 
 namespace BlogApp.DataAccess.Interfaces.Repositories;

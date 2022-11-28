6c77ccd5a954a0d213e7589657e5f1b456068ebb
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-28 23:55:49 +0300

    namespace changed

--- src/Libraries/BlogApp.Core/DataAccess/Interfaces/Repositories/IAsyncInsertableRepository.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Interfaces/Repositories/IAsyncInsertableRepository.cs
@@ -1,6 +1,6 @@
 ﻿using BlogApp.Core.Entities.Base;
 
-namespace BlogApp.Core.DataAccess.Interfaces;
+namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
 public interface IAsyncInsertableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
 {
     Task<TEntity> AddAsync(TEntity entity);

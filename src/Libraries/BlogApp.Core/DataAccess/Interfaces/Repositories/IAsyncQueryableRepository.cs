e27d577d3307a1101cd1ddaa5fd371b750ea5bd7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-28 23:56:23 +0300

    namespace changed

--- src/Libraries/BlogApp.Core/DataAccess/Interfaces/Repositories/IAsyncQueryableRepository.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Interfaces/Repositories/IAsyncQueryableRepository.cs
@@ -1,7 +1,7 @@
 ﻿using BlogApp.Core.Entities.Base;
 using System.Linq.Expressions;
 
-namespace BlogApp.Core.DataAccess.Interfaces;
+namespace BlogApp.Core.DataAccess.Interfaces.Repositories;
 public interface IAsyncQueryableRepository<TEntity> : IAsyncRepository where TEntity : BaseEntity
 {
     Task<IEnumerable<TEntity>> GetAllAsync(bool tracking = true);

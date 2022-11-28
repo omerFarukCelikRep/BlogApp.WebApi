12cc12d3079e3462f21dd3188c7a87146eb2075c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-29 00:54:59 +0300

    ToPaginate methods added

--- src/Libraries/BlogApp.Core/DataAccess/Extensions/IQueryablePaginateExtensions.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Extensions/IQueryablePaginateExtensions.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlogApp.Core.DataAccess.Interfaces.Models;
+using BlogApp.Core.DataAccess.Models.Pagination;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Core.DataAccess.Extensions;
 public static class IQueryablePaginateExtensions
 {
+    public static async Task<IPaginate<T>> ToPaginateAsync<T>(this IQueryable<T> source, int index = 0, int size = 10, CancellationToken cancellationToken = default)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(nameof(source));
+        EnsureInRange(index, size);
+
+        int count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
+        var items = await source.Skip(index * size).Take(index).ToListAsync(cancellationToken: cancellationToken);
+
+        return new Paginate<T>(items, index, size, count);
+    }
+
+    public static IPaginate<T> ToPaginate<T>(this IQueryable<T> source, int index = 0, int size = 10)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(nameof(source));
+        EnsureInRange(index, size);
+
+        int count = source.Count();
+        var items = source.Skip(index * size).Take(index).ToList();
+
+        return new Paginate<T>(items, index, size, count);
+    }
+
+    private static void EnsureInRange(int index, int size)
+    {
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        if (size < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size));
+        }
+    }
 }
+

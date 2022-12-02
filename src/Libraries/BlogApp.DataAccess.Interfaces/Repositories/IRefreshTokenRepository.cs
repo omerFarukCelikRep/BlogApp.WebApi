50d8f1498b4c7f1c2e1c24246d192a7001677de4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:02:05 +0300

    using cleanup

--- src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IRefreshTokenRepository.cs
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -1,7 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-
-namespace BlogApp.DataAccess.Interfaces.Repositories;
+﻿namespace BlogApp.DataAccess.Interfaces.Repositories;
 public interface IRefreshTokenRepository : IAsyncInsertableRepository<RefreshToken>, IAsyncUpdateableRepository<RefreshToken>, IAsyncQueryableRepository<RefreshToken>, IAsyncFindableRepository<RefreshToken>, IAsyncRepository
 {
     Task<RefreshToken?> GetByRefreshTokenAsync(string refreshToken);

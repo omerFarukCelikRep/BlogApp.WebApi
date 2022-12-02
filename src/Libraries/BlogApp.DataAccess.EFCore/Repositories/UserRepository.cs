9bc7a9c7b379076d8035ea22616c487f44573099
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:01:42 +0300

    using cleanup

--- src/Libraries/BlogApp.DataAccess.EFCore/Repositories/UserRepository.cs
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/UserRepository.cs
@@ -1,9 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
-using BlogApp.DataAccess.Contexts;
-using BlogApp.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-
-namespace BlogApp.DataAccess.EFCore.Repositories;
+﻿namespace BlogApp.DataAccess.EFCore.Repositories;
 public class UserRepository : EfBaseRepository<User>, IUserRepository
 {
     public UserRepository(BlogAppDbContext context) : base(context) { }

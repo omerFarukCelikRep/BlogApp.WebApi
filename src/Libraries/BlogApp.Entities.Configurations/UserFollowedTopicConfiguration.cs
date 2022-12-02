678a2162d3ffd3dea6ece4686d810ababbf63c62
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:03:03 +0300

    using cleanup

--- src/Libraries/BlogApp.Entities.Configurations/UserFollowedTopicConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/UserFollowedTopicConfiguration.cs
@@ -1,8 +1,4 @@
-﻿using BlogApp.Core.Entities.Configurations;
-using BlogApp.Entities.DbSets;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace BlogApp.Entities.Configurations;
+﻿namespace BlogApp.Entities.Configurations;
 public class UserFollowedTopicConfiguration : BaseEntityConfiguration<UserFollowedTopic>
 {
     public override void Configure(EntityTypeBuilder<UserFollowedTopic> builder)

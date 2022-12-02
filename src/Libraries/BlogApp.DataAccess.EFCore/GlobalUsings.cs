e886450778796a27d66bbab560ab49a6c65080d8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 22:52:10 +0300

    added

---
+++ src/Libraries/BlogApp.DataAccess.EFCore/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
+global using BlogApp.DataAccess.Contexts;
+global using BlogApp.DataAccess.Interfaces.Repositories;
+global using BlogApp.Entities.DbSets;

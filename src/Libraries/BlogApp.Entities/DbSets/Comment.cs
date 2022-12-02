bd7fbbdafb335650f6aa4349b62d7f35200da063
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 22:56:18 +0300

    marked as nullable

--- src/Libraries/BlogApp.Entities/DbSets/Comment.cs
+++ src/Libraries/BlogApp.Entities/DbSets/Comment.cs
@@ -1,14 +1,12 @@
-﻿using BlogApp.Core.Entities.Base;
-
-namespace BlogApp.Entities.DbSets;
+﻿namespace BlogApp.Entities.DbSets;
 public class Comment : AuditableEntity
 {
-    public string UserName { get; set; }
-    public string Text { get; set; }
-    public string UserIpAdress { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Text { get; set; } = null!;
+    public string UserIpAdress { get; set; } = null!;
 
     //Navigation Prop.
     public Guid? UserId { get; set; }
     public Guid ArticleId { get; set; }
-    public virtual PublishedArticle Article { get; set; }
+    public virtual PublishedArticle? Article { get; set; }
 }

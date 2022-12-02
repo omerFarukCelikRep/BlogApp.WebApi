7ba74fb4c97cc6f0dd69b90b6a132c3ae5e0111a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 22:58:28 +0300

    marked as nullable

--- src/Libraries/BlogApp.Entities/DbSets/UserFollowedTopic.cs
+++ src/Libraries/BlogApp.Entities/DbSets/UserFollowedTopic.cs
@@ -1,12 +1,10 @@
-﻿using BlogApp.Core.Entities.Base;
-
-namespace BlogApp.Entities.DbSets;
+﻿namespace BlogApp.Entities.DbSets;
 
 public class UserFollowedTopic : BaseEntity
 {
     //Navigation Prop.
     public Guid UserId { get; set; }
-    public virtual User User { get; set; }
+    public virtual User? User { get; set; }
     public Guid TopicId { get; set; }
-    public virtual Topic Topic { get; set; }
+    public virtual Topic? Topic { get; set; }
 }

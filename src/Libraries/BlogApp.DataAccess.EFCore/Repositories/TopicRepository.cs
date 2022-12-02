4e6833c8b905d2b523319e991d4a0bc662944a4a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:01:34 +0300

    using cleanup

--- src/Libraries/BlogApp.DataAccess.EFCore/Repositories/TopicRepository.cs
+++ src/Libraries/BlogApp.DataAccess.EFCore/Repositories/TopicRepository.cs
@@ -1,9 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Base.EntityFramework.Repositories;
-using BlogApp.DataAccess.Contexts;
-using BlogApp.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-
-namespace BlogApp.DataAccess.EFCore.Repositories;
+﻿namespace BlogApp.DataAccess.EFCore.Repositories;
 public class TopicRepository : EfBaseRepository<Topic>, ITopicRepository
 {
     public TopicRepository(BlogAppDbContext context) : base(context) { }

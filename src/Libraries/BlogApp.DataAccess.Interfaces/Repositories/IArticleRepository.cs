dfdad732d575d9ed9378d228135865873972b2e1
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:01:48 +0300

    using cleanup

--- src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IArticleRepository.cs
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IArticleRepository.cs
@@ -1,7 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-
-namespace BlogApp.DataAccess.Interfaces.Repositories;
+﻿namespace BlogApp.DataAccess.Interfaces.Repositories;
 public interface IArticleRepository : IAsyncFindableRepository<Article>, IAsyncQueryableRepository<Article>, IAsyncInsertableRepository<Article>, IAsyncRepository
 {
 }

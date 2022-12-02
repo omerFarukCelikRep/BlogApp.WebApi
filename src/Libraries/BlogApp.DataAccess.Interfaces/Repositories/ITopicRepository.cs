3254d9f086c48e14f189ee7a71de99ba103bfbe1
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:02:11 +0300

    using cleanup

--- src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/ITopicRepository.cs
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/ITopicRepository.cs
@@ -1,7 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-
-namespace BlogApp.DataAccess.Interfaces.Repositories;
+﻿namespace BlogApp.DataAccess.Interfaces.Repositories;
 public interface ITopicRepository : IAsyncInsertableRepository<Topic>, IAsyncUpdateableRepository<Topic>, IAsyncDeleteableRepository<Topic>, IAsyncQueryableRepository<Topic>, IAsyncFindableRepository<Topic>, IAsyncRepository
 {
 }

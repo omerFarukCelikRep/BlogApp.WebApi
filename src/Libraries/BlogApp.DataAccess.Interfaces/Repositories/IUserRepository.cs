f5340423d9ac2798062b303f44c51deb51ee2773
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-02 23:02:17 +0300

    using cleanup

--- src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IUserRepository.cs
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IUserRepository.cs
@@ -1,7 +1,4 @@
-﻿using BlogApp.Core.DataAccess.Interfaces.Repositories;
-using BlogApp.Entities.DbSets;
-
-namespace BlogApp.DataAccess.Interfaces.Repositories;
+﻿namespace BlogApp.DataAccess.Interfaces.Repositories;
 public interface IUserRepository : IAsyncInsertableRepository<User>, IAsyncUpdateableRepository<User>, IAsyncQueryableRepository<User>, IAsyncFindableRepository<User>, IAsyncRepository
 {
     Task<User?> GetByIdentityId(Guid identityId);

55f0e90e6ce5b87356bbc04e49719981736cc9b7
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:39:55 +0300

    null forgiving operator added

--- src/UI/BlogApp.MVCUI/Models/Articles/ArticlePublishedListVM.cs
+++ src/UI/BlogApp.MVCUI/Models/Articles/ArticlePublishedListVM.cs
@@ -3,14 +3,14 @@
 public class ArticlePublishedListVM
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public string Title { get; set; } = null!;
+    public string Content { get; set; } = null!;
     public int ReadTime { get; set; }
     public string? Thumbnail { get; set; }
-    public string AuthorName { get; set; }
+    public string AuthorName { get; set; } = null!;
     public DateTime PublishDate { get; set; }
     public int ReadingCount { get; set; }
     public int LikeCount { get; set; }
     public int CommentCount { get; set; }
-    public List<string> Topics { get; set; }
+    public List<string> Topics { get; set; } = null!;
 }

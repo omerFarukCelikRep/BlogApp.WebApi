948de01c81d4f65096bccadb08d57cb91d4a7556
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:53:22 +0300

    ViewComponent attribute removed

--- src/UI/BlogApp.MVCUI/Views/Shared/Components/Header/HeaderViewComponent.cs
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/Header/HeaderViewComponent.cs
@@ -2,11 +2,11 @@
 
 namespace BlogApp.MVCUI.Views.Shared.Components.Header;
 
-[ViewComponent(Name = "Header")]
 public class HeaderViewComponent : ViewComponent
 {
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        return await Task.FromResult(View());
+        await Task.CompletedTask;
+        return View();
     }
 }

8f37ee9ba8e319f18c2e12a948ddbd8e4cf51b1a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:47:42 +0300

    Errors marked as nullable

--- src/UI/BlogApp.MVCUI/Services/Results/AuthResult.cs
+++ src/UI/BlogApp.MVCUI/Services/Results/AuthResult.cs
@@ -5,7 +5,7 @@ public class AuthResult
     public string Token { get; set; } = string.Empty;
     public string RefreshToken { get; set; } = string.Empty;
     public bool Success { get; set; }
-    public List<string> Errors { get; set; }
+    public List<string>? Errors { get; set; }
 
     public override string ToString()
     {

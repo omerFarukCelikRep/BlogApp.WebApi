d7239c3ccbf7608f0710c56ddce309d1a5b73f97
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:41:00 +0300

    null forgiving operator added

--- src/UI/BlogApp.MVCUI/Models/Users/UserMainSliderVM.cs
+++ src/UI/BlogApp.MVCUI/Models/Users/UserMainSliderVM.cs
@@ -3,7 +3,7 @@
 public class UserMainSliderVM
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Thumbnail { get; set; }
-    public List<string> Topics { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Thumbnail { get; set; }
+    public List<string> Topics { get; set; } = null!;
 }

1db5737a55402703ac86fe627adea75111c911d3
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:35:28 +0300

    null forgiving operator added

--- src/UI/BlogApp.MVCUI/Extensions/DependencyInjection.cs
+++ src/UI/BlogApp.MVCUI/Extensions/DependencyInjection.cs
@@ -29,7 +29,7 @@ public static class DependencyInjection
 
         services.AddHttpClient("WebApiClient", client =>
         {
-            client.BaseAddress = new Uri(configuration["WebApiClient:Url"]);
+            client.BaseAddress = new Uri(configuration["WebApiClient:Url"]!);
         })
         .AddHttpMessageHandler<AuthTokenHandler>();
 

4745636c31e686e50984bd293cf3114f917cb4d9
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:53:47 +0300

    InvokeAsync method added

--- src/UI/BlogApp.MVCUI/Views/Shared/Components/UserMainArticles/UserMainArticlesViewComponent.cs
+++ src/UI/BlogApp.MVCUI/Views/Shared/Components/UserMainArticles/UserMainArticlesViewComponent.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogApp.MVCUI.Models.Articles;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.MVCUI.Views.Shared.Components.UserMainArticles;
 
 public class UserMainArticlesViewComponent : ViewComponent
 {
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        await Task.CompletedTask;
+        return View(Enumerable.Empty<UserMainArticleListVM>());
+    }
 }

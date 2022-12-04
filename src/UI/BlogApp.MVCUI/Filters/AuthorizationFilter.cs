3d7935059df4f5132aba8ca5738ba628842ad265
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-04 17:35:34 +0300

    null forgiving operator added

--- src/UI/BlogApp.MVCUI/Filters/AuthorizationFilter.cs
+++ src/UI/BlogApp.MVCUI/Filters/AuthorizationFilter.cs
@@ -9,7 +9,7 @@ public class AuthorizationFilter : Attribute, IAsyncAuthorizationFilter
     public Task OnAuthorizationAsync(AuthorizationFilterContext context)
     {
         bool isAnonymous = context.Filters.Any(x => x.GetType() == typeof(AllowAnonymousFilter));
-        if (!context.HttpContext!.User.Identity.IsAuthenticated && !isAnonymous)
+        if (!context.HttpContext.User.Identity!.IsAuthenticated && !isAnonymous)
         {
             string path = context.HttpContext.Request.Path;
             context.Result = new RedirectToRouteResult(

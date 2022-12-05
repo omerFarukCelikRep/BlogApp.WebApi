26d4b3449e49d4ba6672113308339c0036192937
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-06 00:57:36 +0300

    added

---
+++ src/Libraries/BlogApp.Authentication/Configurations/JwtBearerOptionsSetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BlogApp.Authentication.Configurations;
+public class JwtBearerOptionsSetup : IConfigureOptions<JwtBearerOptions>
+{
+    private readonly JwtOptions _jwtOptions;
+    public JwtBearerOptionsSetup(IOptions<JwtOptions> options)
+    {
+        _jwtOptions = options.Value;
+    }
+
+    public void Configure(JwtBearerOptions options)
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = false, //TODO:Update this later
+            ValidateAudience = false, //TODO:Update this later
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _jwtOptions.Issuer,
+            ValidAudience = _jwtOptions.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Secret)),
+            RequireExpirationTime = false, //TODO: Update this later
+        };
+    }
+}

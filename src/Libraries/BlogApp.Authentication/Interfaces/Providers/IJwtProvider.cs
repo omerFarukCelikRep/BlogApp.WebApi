1baf98c74aed317dc801529ff0de27aadbb7a017
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-06 00:57:51 +0300

    added

---
+++ src/Libraries/BlogApp.Authentication/Interfaces/Providers/IJwtProvider.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlogApp.Authentication.Interfaces.Providers;
+public interface IJwtProvider
+{
+    string Generate(IdentityUser<Guid> identityUser, Guid userId);
+}

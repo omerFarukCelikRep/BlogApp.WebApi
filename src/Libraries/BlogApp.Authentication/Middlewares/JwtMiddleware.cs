2925385b29c46cd85aa1c695434ee27968849c74
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-05 23:35:19 +0300

    using namespace changed

--- src/Libraries/BlogApp.Authentication/Middlewares/JwtMiddleware.cs
+++ src/Libraries/BlogApp.Authentication/Middlewares/JwtMiddleware.cs
@@ -1,4 +1,4 @@
-﻿using BlogApp.Authentication.Services.Interfaces;
+﻿using BlogApp.Authentication.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 
 namespace BlogApp.Authentication.Middlewares;

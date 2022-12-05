e3fd80158cfb1c342fd7bbd9018c7b25a4fecae8
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-06 00:57:40 +0300

    added

---
+++ src/Libraries/BlogApp.Authentication/Configurations/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Authentication.Configurations;
+
+public class JwtOptions
+{
+    public string Audience { get; init; } = null!;
+    public string Issuer { get; init; } = null!;
+    public string Secret { get; init; } = null!;
+    public TimeSpan ExpiryTimeFrame { get; init; }
+}

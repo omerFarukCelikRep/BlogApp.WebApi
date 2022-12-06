f7f7ffb4c7603e768111167904ee79305f011b0e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-07 00:49:11 +0300

    AddAuthentication simplified

--- src/Libraries/BlogApp.Authentication/Extensions/DependencyInjection.cs
+++ src/Libraries/BlogApp.Authentication/Extensions/DependencyInjection.cs
@@ -1,46 +1,23 @@
 ﻿using BlogApp.Authentication.Configurations;
+using BlogApp.Authentication.Interfaces.Providers;
 using BlogApp.Authentication.Interfaces.Services;
+using BlogApp.Authentication.Providers;
 using BlogApp.Authentication.Services;
 using BlogApp.Core.Utilities.Authentication;
-using BlogApp.Core.Utilities.Configurations;
 using BlogApp.DataAccess.Contexts;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace BlogApp.Authentication.Extensions;
 public static class DependencyInjection
 {
     public static IServiceCollection AddAuthenticationServices(this IServiceCollection services)
     {
-        services.Configure<JwtConfig>(Configuration.GetSection("Jwt"));
+        services.AddAuthentication()
+        .AddJwtBearer();
 
-        var secret = Configuration.GetValue("Jwt:Secret");
-
-        var key = Encoding.ASCII.GetBytes(secret!);
-
-        var tokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            ValidateIssuer = false, //TODO:Update this later
-            ValidateAudience = false, //TODO:Update this later
-            RequireExpirationTime = false, //TODO: Update this later
-            ValidateLifetime = true
-        };
-
-        services.AddSingleton(tokenValidationParameters);
-
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.SaveToken = true;
-            options.TokenValidationParameters = tokenValidationParameters;
-        });
-
-        //services.AddAuthentication("Bearer").AddJwtBearer();
+        services.ConfigureOptions<JwtOptionsSetup>();
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
 
         services.AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(options =>
         {
@@ -51,6 +28,7 @@ public static class DependencyInjection
             .AddDefaultTokenProviders();
 
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IJwtProvider, JwtProvider>();
         services.AddSingleton<JwtHelper>();
 
         return services;

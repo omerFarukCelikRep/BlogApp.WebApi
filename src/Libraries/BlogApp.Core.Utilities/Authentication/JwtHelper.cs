be660a4f1c1bd267d2a61bfe070768142c88469f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-08 01:44:32 +0300

    Read method added

--- src/Libraries/BlogApp.Core.Utilities/Authentication/JwtHelper.cs
+++ src/Libraries/BlogApp.Core.Utilities/Authentication/JwtHelper.cs
@@ -3,11 +3,16 @@
 namespace BlogApp.Core.Utilities.Authentication;
 public class JwtHelper
 {
+    private static readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler = new();
     public static string? GetUserIdByToken(string token)
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var decodedToken = tokenHandler.ReadJwtToken(token);
+        var decodedToken = _jwtSecurityTokenHandler.ReadJwtToken(token);
 
         return decodedToken.Claims.FirstOrDefault(x => x.Type == "Id")?.Value;
+    }
+
+    public static JwtSecurityToken? Read(string token)
+    {
+        return _jwtSecurityTokenHandler.ReadJwtToken(token);
     }
 }

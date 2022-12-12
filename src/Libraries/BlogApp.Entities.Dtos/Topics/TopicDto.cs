8a1e607023a6ec257020a01c5541a0be53963c26
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-12 23:22:16 +0300

    Description added

--- src/Libraries/BlogApp.Entities.Dtos/Topics/TopicDto.cs
+++ src/Libraries/BlogApp.Entities.Dtos/Topics/TopicDto.cs
@@ -3,5 +3,6 @@ public class TopicDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
     public string Thumbnail { get; set; } = null!;
 }

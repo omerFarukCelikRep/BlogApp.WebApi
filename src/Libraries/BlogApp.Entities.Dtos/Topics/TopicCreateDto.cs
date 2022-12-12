460b98aa31db36ef2c98062917289a8b88d372fc
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-12 23:22:09 +0300

    Description added

--- src/Libraries/BlogApp.Entities.Dtos/Topics/TopicCreateDto.cs
+++ src/Libraries/BlogApp.Entities.Dtos/Topics/TopicCreateDto.cs
@@ -2,5 +2,6 @@
 public class TopicCreateDto
 {
     public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public string Thumbnail { get; set; } = string.Empty;
 }

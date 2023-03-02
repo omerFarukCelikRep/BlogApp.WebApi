d024c2fa27ee685366d7fc410e5eea4fe7789c0a
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-03-03 02:00:56 +0300

    InMemoryCacheService added

---
+++ src/Libraries/BlogApp.Core.Utilities/Caching/InMemory/InMemoryCacheService.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Core.Utilities.Caching.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BlogApp.Core.Utilities.Caching.InMemory;
+public class InMemoryCacheService : ICacheService
+{
+    private readonly IMemoryCache _memoryCache;
+    public InMemoryCacheService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public Task<T?> Get<T>(string key, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_memoryCache.Get<T>(key));
+    }
+
+    public Task Remove(string key, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(() => _memoryCache.Remove(key));
+    }
+
+    public Task Set<T>(string key, T value, DateTimeOffset expirationTime, CancellationToken cancellationToken = default)
+    {
+        var options = new MemoryCacheEntryOptions().SetAbsoluteExpiration(expirationTime);
+
+        return Task.FromResult(_memoryCache.Set(key, value, options));
+    }
+}

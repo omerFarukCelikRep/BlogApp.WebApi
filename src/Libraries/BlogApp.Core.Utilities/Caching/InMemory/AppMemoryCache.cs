b22ce87030c472aa02fa860cb54e9c3f72aa0598
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-03-03 02:00:43 +0300

    AppMemoryCache added

---
+++ src/Libraries/BlogApp.Core.Utilities/Caching/InMemory/AppMemoryCache.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace BlogApp.Core.Utilities.Caching.InMemory;
+public class AppMemoryCache
+{
+    public MemoryCache Cache { get; } = new MemoryCache(
+       new MemoryCacheOptions
+       {
+           SizeLimit = 1024
+       });
+}

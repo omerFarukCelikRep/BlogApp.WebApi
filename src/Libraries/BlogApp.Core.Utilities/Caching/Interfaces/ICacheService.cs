2b2894321efa23dabb381bd10e645bd90ba0028c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-03-03 02:01:05 +0300

    ICacheService added

---
+++ src/Libraries/BlogApp.Core.Utilities/Caching/Interfaces/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogApp.Core.Utilities.Caching.Interfaces;
+public interface ICacheService
+{
+    Task<T?> Get<T>(string key, CancellationToken cancellationToken = default);
+    Task Set<T>(string key, T value, DateTimeOffset expirationTime, CancellationToken cancellationToken = default);
+    Task Remove(string key, CancellationToken cancellationToken = default);
+}

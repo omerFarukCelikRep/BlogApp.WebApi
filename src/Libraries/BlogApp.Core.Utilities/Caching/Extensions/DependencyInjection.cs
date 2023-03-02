a0b7362846717c43802ca3a7e1e220f513d52c45
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-03-03 02:00:32 +0300

    added

---
+++ src/Libraries/BlogApp.Core.Utilities/Caching/Extensions/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Core.Utilities.Caching.InMemory;
+using BlogApp.Core.Utilities.Caching.Redis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.Core.Utilities.Caching.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCacheServices(this IServiceCollection services)
+    {
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.InstanceName = nameof(RedisCacheService);
+            options.Configuration = "127.0.0.1:6379";
+        });
+
+        services.AddSingleton<AppMemoryCache>();
+        return services;
+    }
+}

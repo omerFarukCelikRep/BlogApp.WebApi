da06c8ae3011398e2c2e67b224dc93dc7ae25eb0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:03:58 +0300

    Article Entity Configurations changed

--- src/Libraries/BlogApp.Entities.Configurations/ArticleConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/ArticleConfiguration.cs
@@ -2,15 +2,26 @@
 
 public class ArticleConfiguration : AuditableEntityConfiguration<Article>
 {
+    private const string TableName = "Articles";
+
     public override void Configure(EntityTypeBuilder<Article> builder)
     {
         base.Configure(builder);
 
-        builder.Property(x => x.Content).IsRequired();
-        builder.Property(x => x.Title).HasMaxLength(512).IsRequired();
-        builder.Property(x => x.Thumbnail).IsRequired(false);
-        builder.Property(x => x.ReadTime).IsRequired();
+        builder.ToTable(TableName);
 
-        builder.HasOne(x => x.User).WithMany(x => x.Articles).HasForeignKey(x => x.UserId);
+        builder.Property(x => x.Content)
+               .IsRequired();
+        builder.Property(x => x.Title)
+               .HasMaxLength(512)
+               .IsRequired();
+        builder.Property(x => x.Thumbnail)
+               .IsRequired(false);
+        builder.Property(x => x.ReadTime)
+               .IsRequired();
+
+        builder.HasOne(x => x.User)
+               .WithMany(x => x.Articles)
+               .HasForeignKey(x => x.UserId);
     }
 }

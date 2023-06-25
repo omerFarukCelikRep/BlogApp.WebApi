3b0f7a6aa6ba7abdf80d85e86ad64831c76c1b35
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:11:59 +0300

    User Followed Topic Entity Configurations changed

--- src/Libraries/BlogApp.Entities.Configurations/UserFollowedTopicConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/UserFollowedTopicConfiguration.cs
@@ -1,11 +1,21 @@
 ﻿namespace BlogApp.Entities.Configurations;
 public class UserFollowedTopicConfiguration : BaseEntityConfiguration<UserFollowedTopic>
 {
+    private const string TableName = "UserFollowedTopics";
     public override void Configure(EntityTypeBuilder<UserFollowedTopic> builder)
     {
         base.Configure(builder);
 
-        builder.HasOne(x => x.User).WithMany(x => x.MemberFollowedTopics).HasForeignKey(x => x.UserId);
-        builder.HasOne(x => x.Topic).WithMany(x => x.MemberFollowedTopics).HasForeignKey(x => x.TopicId);
+        builder.ToTable(TableName);
+
+        builder.HasIndex(x => new { x.TopicId, x.UserId })
+               .IsUnique();
+
+        builder.HasOne(x => x.User)
+               .WithMany(x => x.MemberFollowedTopics)
+               .HasForeignKey(x => x.UserId);
+        builder.HasOne(x => x.Topic)
+               .WithMany(x => x.MemberFollowedTopics)
+               .HasForeignKey(x => x.TopicId);
     }
 }

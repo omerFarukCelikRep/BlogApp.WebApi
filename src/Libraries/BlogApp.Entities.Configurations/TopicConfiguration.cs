97318dcfad33a5007dd672d1042b85fa7589f445
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:11:30 +0300

    Topic  Entity Configurations changed

--- src/Libraries/BlogApp.Entities.Configurations/TopicConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/TopicConfiguration.cs
@@ -1,12 +1,20 @@
 ﻿namespace BlogApp.Entities.Configurations;
 public class TopicConfiguration : BaseEntityConfiguration<Topic>
 {
+    private const string TableName = "Topics";
     public override void Configure(EntityTypeBuilder<Topic> builder)
     {
         base.Configure(builder);
 
-        builder.Property(x => x.Name).HasMaxLength(256).IsRequired();
-        builder.Property(x => x.Description).HasMaxLength(512).IsRequired();
-        builder.Property(x => x.Thumbnail).IsRequired();
+        builder.ToTable(TableName);
+
+        builder.Property(x => x.Name)
+               .HasMaxLength(256)
+               .IsRequired();
+        builder.Property(x => x.Description)
+               .HasMaxLength(512)
+               .IsRequired();
+        builder.Property(x => x.Thumbnail)
+               .IsRequired();
     }
 }

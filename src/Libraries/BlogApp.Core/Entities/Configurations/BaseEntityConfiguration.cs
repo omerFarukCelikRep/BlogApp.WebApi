c0d311c43b7f732e791ea452fcfcad4116f65847
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 20:38:40 +0300

    Entity created date desc order index added

--- src/Libraries/BlogApp.Core/Entities/Configurations/BaseEntityConfiguration.cs
+++ src/Libraries/BlogApp.Core/Entities/Configurations/BaseEntityConfiguration.cs
@@ -9,10 +9,10 @@ public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> w
     {
         builder.HasKey(x => x.Id);
 
+        builder.HasIndex(x => x.CreatedDate).IsDescending();
+
         builder.Property(x => x.Id).ValueGeneratedOnAdd();
-
         builder.Property(x => x.Status).IsRequired();
-
         builder.Property(x => x.CreatedBy).HasMaxLength(128).IsRequired();
         builder.Property(x => x.CreatedDate).IsRequired();
         builder.Property(x => x.ModifiedBy).HasMaxLength(128).IsRequired(false);

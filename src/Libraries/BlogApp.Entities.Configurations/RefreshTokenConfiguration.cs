0a3e807195de1e04e6fef094fe571e731469883c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:11:00 +0300

    Refresh Token  Entity Configurations changed

--- src/Libraries/BlogApp.Entities.Configurations/RefreshTokenConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/RefreshTokenConfiguration.cs
@@ -2,13 +2,20 @@
 
 public class RefreshTokenConfiguration : BaseEntityConfiguration<RefreshToken>
 {
+    private const string TableName = "RefreshTokens";
     public override void Configure(EntityTypeBuilder<RefreshToken> builder)
     {
         base.Configure(builder);
 
-        builder.Property(x => x.Token).IsRequired();
-        builder.Property(x => x.UserId).IsRequired();
-        builder.Property(x => x.ExpiryDate).IsRequired();
-        builder.Property(x => x.CreatedByIp).IsRequired(false); //TODO:Zorunlu olacak
+        builder.ToTable(TableName);
+
+        builder.Property(x => x.Token)
+               .IsRequired();
+        builder.Property(x => x.UserId)
+               .IsRequired();
+        builder.Property(x => x.ExpiryDate)
+               .IsRequired();
+        builder.Property(x => x.CreatedByIp)
+               .IsRequired(false); //TODO:Zorunlu olacak
     }
 }

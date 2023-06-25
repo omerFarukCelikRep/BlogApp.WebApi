1dcff1153c31b5a930e016f3a4087f2455c27111
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:11:20 +0300

    Role Entity Configurations added

---
+++ src/Libraries/BlogApp.Entities.Configurations/RoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace BlogApp.Entities.Configurations;
+public class RoleConfiguration : AuditableEntityConfiguration<Role>
+{
+    public const string TableName = "Roles";
+
+    public override void Configure(EntityTypeBuilder<Role> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable(TableName);
+
+        builder.Property(x => x.Name)
+               .HasMaxLength(256)
+               .IsRequired();
+
+        builder.HasMany<UserRole>()
+               .WithOne()
+               .HasForeignKey(x => x.RoleId);
+    }
+}

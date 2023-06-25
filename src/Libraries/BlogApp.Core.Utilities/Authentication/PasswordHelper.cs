42a55df72aacc8bd892756a6298150a40d913437
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-26 00:35:43 +0300

    Password Helper added

---
+++ src/Libraries/BlogApp.Core.Utilities/Authentication/PasswordHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BlogApp.Core.Utilities.Authentication;
+public class PasswordHelper
+{
+    private const int KeySize = 64;
+    private const int Iterations = 350000;
+
+    public static (byte[] Key, string) HashPassword(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(KeySize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            Iterations,
+            HashAlgorithmName.SHA512,
+            KeySize);
+        return (salt, Convert.ToHexString(hash));
+    }
+
+    public static bool VerifyPassword(string password, string hash, byte[] salt)
+    {
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithmName.SHA512, KeySize);
+        return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(hash));
+    }
+}

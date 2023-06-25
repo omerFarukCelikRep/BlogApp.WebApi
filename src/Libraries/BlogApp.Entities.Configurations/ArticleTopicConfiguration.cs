ac99e5d99063ad737edf283d834c56eddf1501b1
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:04:14 +0300

    Article Topic Entity Configurations changed

--- src/Libraries/BlogApp.Entities.Configurations/ArticleTopicConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/ArticleTopicConfiguration.cs
@@ -1,11 +1,22 @@
 ﻿namespace BlogApp.Entities.Configurations;
 public class ArticleTopicConfiguration : BaseEntityConfiguration<ArticleTopic>
 {
+    private const string TableName = "ArticleTopics";
+
     public override void Configure(EntityTypeBuilder<ArticleTopic> builder)
     {
         base.Configure(builder);
 
-        builder.HasOne(x => x.Article).WithMany(x => x.ArticleTopics).HasForeignKey(x => x.ArticleId);
-        builder.HasOne(x => x.Topic).WithMany(x => x.ArticleTopics).HasForeignKey(x => x.TopicId);
+        builder.ToTable(TableName);
+
+        builder.HasIndex(x => new { x.ArticleId, x.TopicId })
+               .IsUnique();
+
+        builder.HasOne(x => x.Article)
+               .WithMany(x => x.ArticleTopics)
+               .HasForeignKey(x => x.ArticleId);
+        builder.HasOne(x => x.Topic)
+               .WithMany(x => x.ArticleTopics)
+               .HasForeignKey(x => x.TopicId);
     }
 }

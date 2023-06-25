a46c9b1c493c39aed26370eb04a0aea92fe0f49d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:12:12 +0300

    User Role Entity Configurations added

---
+++ src/Libraries/BlogApp.Entities.Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace BlogApp.Entities.Configurations;
+public class UserRoleConfiguration : BaseEntityConfiguration<UserRole>
+{
+    public const string TableName = "UserRoles";
+
+    public override void Configure(EntityTypeBuilder<UserRole> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable(TableName);
+
+        builder.HasIndex(x => new { x.UserId, x.RoleId });
+
+        builder.Property(x => x.UserId)
+               .IsRequired();
+        builder.Property(x => x.RoleId)
+               .IsRequired();
+    }
+}

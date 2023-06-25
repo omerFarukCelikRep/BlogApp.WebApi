805325670d2787c27ebd4296d0688f6965bf6509
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-06-25 22:04:27 +0300

    Comment  Entity Configurations changed

--- src/Libraries/BlogApp.Entities.Configurations/CommentConfiguration.cs
+++ src/Libraries/BlogApp.Entities.Configurations/CommentConfiguration.cs
@@ -1,16 +1,28 @@
 ﻿namespace BlogApp.Entities.Configurations;
 public class CommentConfiguration : AuditableEntityConfiguration<Comment>
 {
+    private const string TableName = "Comments";
+
     public override void Configure(EntityTypeBuilder<Comment> builder)
     {
         base.Configure(builder);
 
-        builder.Property(x => x.UserName).HasMaxLength(512).IsRequired();
-        builder.Property(x => x.Text).IsRequired();
-        builder.Property(x => x.UserIpAdress).IsRequired();
-        builder.Property(x => x.ArticleId).IsRequired();
-        builder.Property(x => x.UserId).IsRequired(false);
+        builder.ToTable(TableName);
 
-        builder.HasOne(x => x.Article).WithMany(x => x.Comments).HasForeignKey(x => x.ArticleId);
+        builder.Property(x => x.UserName)
+               .HasMaxLength(512)
+               .IsRequired();
+        builder.Property(x => x.Text)
+               .IsRequired();
+        builder.Property(x => x.UserIpAdress)
+               .IsRequired();
+        builder.Property(x => x.ArticleId)
+               .IsRequired();
+        builder.Property(x => x.UserId)
+               .IsRequired(false);
+
+        builder.HasOne(x => x.Article)
+               .WithMany(x => x.Comments)
+               .HasForeignKey(x => x.ArticleId);
     }
 }

4618382dc8a0a8d2d46d428dc1dffc4bdba7b2af
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-07-03 18:59:39 +0300

    Validation Message consts added

---
+++ src/Libraries/BlogApp.Business/Validations/ValidationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Business.Validations;
+public struct ValidationMessages
+{
+    public const string NotEmpty = "{0} boş bırakılamaz!"; 
+    public const string Invalid = "Lütfen geçerli bir {0} giriniz!";
+    public const string InvalidMaxLength = "{0} {1} karakterden fazla olamaz!";
+    public const string InvalidMinLength = "{0} {1} karakterden az olamaz!";
+    public const string NotMatch = "{0} eşleşmemektedir!";
+}

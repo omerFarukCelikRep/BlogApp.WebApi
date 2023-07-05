e593306c90f3365cfc844ceb69fcd42ce53a169d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-07-06 01:31:59 +0300

    removed

--- src/Libraries/BlogApp.Authentication/Interfaces/Providers/IJwtProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace BlogApp.Authentication.Interfaces.Providers;
-public interface IJwtProvider
-{
-    string Generate(IdentityUser<Guid> identityUser, Guid userId);
-}

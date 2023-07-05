eaecf079ed42e27da0d4caf208d86dded0cf5848
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-07-06 01:33:11 +0300

    Identity User mapping removed

--- src/Libraries/BlogApp.Business/Mappings/Profiles/UserProfile.cs
+++ src/Libraries/BlogApp.Business/Mappings/Profiles/UserProfile.cs
@@ -3,17 +3,12 @@ using BlogApp.Authentication.Dtos.Incoming;
 using BlogApp.Entities.DbSets;
 using BlogApp.Entities.Dtos.PublishedArticles;
 using BlogApp.Entities.Dtos.Users;
-using Microsoft.AspNetCore.Identity;
 
 namespace BlogApp.Business.Mappings.Profiles;
 public class UserProfile : Profile
 {
     public UserProfile()
     {
-        CreateMap<UserRegistrationRequestDto, IdentityUser<Guid>>()
-            .AfterMap((s, d) => d.EmailConfirmed = true)
-            .AfterMap((s, d) => d.UserName = s.Email);
-
         CreateMap<UserRegistrationRequestDto, User>();
 
         CreateMap<User, UserListDto>();

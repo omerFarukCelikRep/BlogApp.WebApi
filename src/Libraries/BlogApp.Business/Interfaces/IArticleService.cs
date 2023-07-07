d45af5365c0e1c626d67edd145f0407a7246adbc
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-07-08 01:57:44 +0300

    Cancellation token parameter added

--- src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
+++ src/Libraries/BlogApp.Business/Interfaces/IArticleService.cs
@@ -7,7 +7,7 @@ public interface IArticleService
 {
     Task<IDataResult<List<PublishedArticleByUserListDto>>> GetAllPublishedAsync();
     Task<IDataResult<List<PublishedArticleByUserListDto>>> GetAllPublishedByUserIdAsync(Guid userId);
-    Task<IDataResult<List<ArticleDto>>> GetTrendsAsync();
+    Task<IDataResult<List<ArticleDto>>> GetTrendsAsync(CancellationToken cancellationToken = default);
     Task<IDataResult<PublishedArticleDetailsDto>> GetByIdAsync(Guid id);
     Task<IResult> PublishAsync(Guid articleId);
     Task<IResult> AddAsync(ArticleCreateDto createArticleDto);

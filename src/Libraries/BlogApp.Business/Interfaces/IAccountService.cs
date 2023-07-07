dcf2cc1dce1a9166c347a4c0d8571a2e5761477f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-07-08 01:57:27 +0300

    Method parameters changed

--- src/Libraries/BlogApp.Business/Interfaces/IAccountService.cs
+++ src/Libraries/BlogApp.Business/Interfaces/IAccountService.cs
@@ -1,13 +1,13 @@
 ﻿using BlogApp.Authentication.Dtos.Incoming;
 using BlogApp.Authentication.Dtos.Outgoing;
 using BlogApp.Core.Utilities.Results.Interfaces;
-using Microsoft.AspNetCore.Identity;
+using BlogApp.Entities.DbSets;
 
 namespace BlogApp.Business.Interfaces;
 public interface IAccountService
 {
-    Task<AuthResult> AddAsync(UserRegistrationRequestDto registrationRequestDto);
-    Task<AuthResult> AuthenticateAsync(UserLoginRequestDto loginRequestDto, string ipAddress);
-    Task<IDataResult<IdentityUser<Guid>>> FindByEmailAsync(string email);
-    Task<AuthResult> RefreshTokenAsync(TokenRequestDto tokenRequestDto);
+    Task<AuthResult> AddAsync(UserRegistrationRequestDto registrationRequestDto, CancellationToken cancellationToken = default);
+    Task<AuthResult> AuthenticateAsync(UserLoginRequestDto loginRequestDto, string ipAddress, CancellationToken cancellationToken = default);
+    Task<IDataResult<User>> FindByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<AuthResult> RefreshTokenAsync(TokenRequestDto tokenRequestDto, CancellationToken cancellationToken = default);
 }

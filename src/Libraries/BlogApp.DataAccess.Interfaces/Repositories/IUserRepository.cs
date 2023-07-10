65c64a9f72f424035b69bd78dcebafa83c6784c9
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-07-11 00:39:29 +0300

    GetByEmailAsync method parameters changed

--- src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IUserRepository.cs
+++ src/Libraries/BlogApp.DataAccess.Interfaces/Repositories/IUserRepository.cs
@@ -1,5 +1,5 @@
 ﻿namespace BlogApp.DataAccess.Interfaces.Repositories;
 public interface IUserRepository : IAsyncInsertableRepository<User>, IAsyncUpdateableRepository<User>, IAsyncQueryableRepository<User>, IAsyncFindableRepository<User>, IAsyncRepository
 {
-    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByEmailAsync(string email, bool tracking = true, CancellationToken cancellationToken = default);
 }

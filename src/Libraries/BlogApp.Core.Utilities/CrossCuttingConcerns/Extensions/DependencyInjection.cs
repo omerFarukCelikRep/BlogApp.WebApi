602cd10b276778cea606525d1b3e57cc5e96d2b6
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-03 00:28:10 +0300

    packages updated

--- src/Libraries/BlogApp.Core.Utilities/CrossCuttingConcerns/Extensions/DependencyInjection.cs
+++ src/Libraries/BlogApp.Core.Utilities/CrossCuttingConcerns/Extensions/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using BlogApp.Core.Utilities.Caching.InMemory;
 using BlogApp.Core.Utilities.Caching.Interfaces;
-using BlogApp.Core.Utilities.Caching.Redis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,11 +8,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddCacheServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddStackExchangeRedisCache(options =>
-        {
-            options.InstanceName = nameof(RedisCacheService);
-            options.Configuration = configuration["Redis:Host"];
-        });
+        //services.AddStackExchangeRedisCache(options =>
+        //{
+        //    options.InstanceName = nameof(RedisCacheService);
+        //    options.Configuration = configuration["Redis:Host"];
+        //});
 
         services.AddMemoryCache();
         services.AddSingleton<ICacheService, InMemoryCacheService>();

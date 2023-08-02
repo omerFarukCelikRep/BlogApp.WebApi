cc528f97bcfa63abea763ec079592abf9cedda31
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-03 00:21:44 +0300

    ExecuteAsync method added to Cache service

--- src/Libraries/BlogApp.Core.Utilities/CrossCuttingConcerns/Caching/Interfaces/ICacheService.cs
+++ src/Libraries/BlogApp.Core.Utilities/CrossCuttingConcerns/Caching/Interfaces/ICacheService.cs
@@ -4,4 +4,5 @@ public interface ICacheService
     Task<T?> Get<T>(string key, CancellationToken cancellationToken = default);
     Task Set<T>(string key, T value, DateTimeOffset expirationTime, CancellationToken cancellationToken = default);
     Task Remove(string key, CancellationToken cancellationToken = default);
+    Task<TResult> ExecuteAsync<TResult>(Func<TResult> func, string key, DateTimeOffset expirationTime, CancellationToken cancellationToken = default);
 }

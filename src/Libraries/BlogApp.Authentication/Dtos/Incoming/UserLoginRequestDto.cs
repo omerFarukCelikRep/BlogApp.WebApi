be89221251fa88ff8a08de8ab08e1e063a23ac05
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-04 00:46:53 +0300

    UserLoginRequestDto type changed to record

--- src/Libraries/BlogApp.Authentication/Dtos/Incoming/UserLoginRequestDto.cs
+++ src/Libraries/BlogApp.Authentication/Dtos/Incoming/UserLoginRequestDto.cs
@@ -1,6 +1,7 @@
 ﻿namespace BlogApp.Authentication.Dtos.Incoming;
-public class UserLoginRequestDto
+public record UserLoginRequestDto
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public bool RememberMe { get; set; }
 }

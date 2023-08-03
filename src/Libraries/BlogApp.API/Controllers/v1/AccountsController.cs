f946e39c42fa47e9b9a04bbb632fd84b9d2b56b2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-04 00:46:30 +0300

    Remember me actions added

--- src/Libraries/BlogApp.API/Controllers/v1/AccountsController.cs
+++ src/Libraries/BlogApp.API/Controllers/v1/AccountsController.cs
@@ -1,70 +1,82 @@
-﻿using BlogApp.Authentication.Constants;
-using BlogApp.Authentication.Dtos.Incoming;
-using BlogApp.Authentication.Dtos.Outgoing;
+﻿using BlogApp.Authentication.Dtos.Incoming;
 using BlogApp.Business.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace BlogApp.API.Controllers.v1;
 public class AccountsController : BaseController
 {
-    private readonly IAccountService _accountService;
+    private const int DefaultCookieExpireTime = 30;
+    private const string RememberMeKey = "RememberMe";
 
+    private readonly IAccountService _accountService;
     public AccountsController(IAccountService accountService)
     {
         _accountService = accountService;
     }
 
-    [HttpPost]
-    [Route("Register")]
+    [HttpPost("Register")]
     [AllowAnonymous]
-    public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto registrationRequestDto)
+    public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto registrationRequestDto, CancellationToken cancellationToken = default)
     {
         if (!ModelState.IsValid)
-        {
             return BadRequest(ModelState);
-        }
-        
+
         registrationRequestDto.IpAddress = GetIpAddress();
-        var registerResult = await _accountService.AddAsync(registrationRequestDto);
+        var registerResult = await _accountService.AddAsync(registrationRequestDto, cancellationToken);
         return !registerResult.Success ? BadRequest(registerResult) : Ok(registerResult);
     }
 
-    [HttpPost]
-    [Route("Authenticate")]
+    [HttpPost("Authenticate")]
     [AllowAnonymous]
-    public async Task<IActionResult> Authenticate([FromBody] UserLoginRequestDto loginRequestDto)
+    public async Task<IActionResult> Authenticate([FromBody] UserLoginRequestDto loginRequestDto, CancellationToken cancellationToken = default)
     {
         if (!ModelState.IsValid)
-        {
             return BadRequest(ModelState);
-        }
 
-        var authenticationResult = await _accountService.AuthenticateAsync(loginRequestDto, GetIpAddress());
+        var authenticationResult = await _accountService.AuthenticateAsync(loginRequestDto, GetIpAddress(), cancellationToken);
         if (!authenticationResult.Success)
-        {
             return Unauthorized(authenticationResult);
-        }
+
+        if (loginRequestDto.RememberMe)
+            SetRememberMe(loginRequestDto.Email);
 
         return Ok(authenticationResult);
     }
 
-    [HttpPost]
-    [Route("RefreshToken")]
-    public async Task<IActionResult> RefreshToken([FromBody] TokenRequestDto tokenRequestDto)
+    [HttpPost("RefreshToken")]
+    public async Task<IActionResult> RefreshToken([FromBody] TokenRequestDto tokenRequestDto, CancellationToken cancellationToken = default)
     {
         if (!ModelState.IsValid)
-        {
             return BadRequest(ModelState);
-        }
 
         tokenRequestDto.IpAddress = GetIpAddress();
-        var result = await _accountService.RefreshTokenAsync(tokenRequestDto);
+        var result = await _accountService.RefreshTokenAsync(tokenRequestDto, cancellationToken);
         if (!result.Success)
-        {
             return BadRequest(result);
-        }
 
         return Ok(result);
     }
+
+    [HttpPost]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    public async Task<IActionResult> Logout(CancellationToken cancellationToken = default)
+    {
+        RemoveRememberMe();
+
+        return NoContent();
+    }
+
+    private void SetRememberMe(string email)
+    {
+        var options = new CookieOptions
+        {
+            Expires = DateTimeOffset.Now.AddDays(DefaultCookieExpireTime)
+        };
+
+        Response.Cookies.Append(RememberMeKey, email, options);
+    }
+
+    private void RemoveRememberMe() => Response.Cookies.Delete(RememberMeKey);
 }

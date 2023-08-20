e6c3db082c1ccbd1f8ab38c34644724c8393400e
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-20 23:46:03 +0300

    ServiceCollection Constants added

---
+++ src/Libraries/BlogApp.API/Constants/ServiceCollectionConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.API.Constants;
+
+public struct ServiceCollectionConstants
+{
+    internal struct RateLimitConstans
+    {
+        internal const string PolicyName = "Fixed";
+        internal const int PermitLimit = 10;
+        internal const int TimeWindow = 10;
+        internal const int QueueLimit = 2;
+    }
+}

e64a10c5aa445b7e2c90defa979101842ae542a4
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-22 01:24:41 +0300

    Test method commented

--- src/Tests/BlogApp.UnitTests/Systems/Repositories/TestArticleRepository.cs
+++ src/Tests/BlogApp.UnitTests/Systems/Repositories/TestArticleRepository.cs
@@ -10,19 +10,19 @@ namespace BlogApp.UnitTests.Systems.Repositories;
 
 public class TestArticleRepository
 {
-    [Fact]
-    public async Task GetAll_WhenCalled_InvokesHttpGetRequest()
-    {
-        //Arrange
-        var expectedResponse = ArticlesFixture.GetTestArticles();
-        var endpoint = "https://example.com/articles";
+    //[Fact]
+    //public async Task GetAll_WhenCalled_InvokesHttpGetRequest()
+    //{
+    //    //Arrange
+    //    var expectedResponse = ArticlesFixture.GetTestArticles();
+    //    var endpoint = "https://example.com/articles";
 
-        var handlerMock = MockHttpMessageHandler<Article>.SetupBasicGetResourceList(expectedResponse);
+    //    var handlerMock = MockHttpMessageHandler<Article>.SetupBasicGetResourceList(expectedResponse);
 
-        var httpClient = new HttpClient(handlerMock.Object);
+    //    var httpClient = new HttpClient(handlerMock.Object);
 
-        var config = Options.Create(endpoint);
+    //    var config = Options.Create(endpoint);
 
-        //var sut = new 
-    }
+    //    //var sut = new 
+    //}
 }

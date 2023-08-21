5a6814541a032c5025b38be8e82fdf8a2ad77ba2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-22 01:21:13 +0300

    SuccessDataResult type changed record

--- src/Libraries/BlogApp.Core.Utilities/Results/Concrete/SuccessDataResult.cs
+++ src/Libraries/BlogApp.Core.Utilities/Results/Concrete/SuccessDataResult.cs
@@ -1,19 +1,11 @@
 ﻿namespace BlogApp.Core.Utilities.Results.Concrete;
-public class SuccessDataResult<T> : DataResult<T>
+public record SuccessDataResult<T> : DataResult<T>
 {
-    public SuccessDataResult() : base(default, true)
-    {
-    }
+    public SuccessDataResult() : base(default, true) { }
 
-    public SuccessDataResult(string message) : base(default, true, message)
-    {
-    }
+    public SuccessDataResult(string message) : base(default, true, message) { }
 
-    public SuccessDataResult(T data) : base(data, true)
-    {
-    }
+    public SuccessDataResult(T data) : base(data, true) { }
 
-    public SuccessDataResult(T data, string message) : base(data, true, message)
-    {
-    }
+    public SuccessDataResult(T data, string message) : base(data, true, message) { }
 }

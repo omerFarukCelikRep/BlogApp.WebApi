a7c86fe1d4d59b0597f43675ef389d81859f75f9
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-23 00:33:55 +0300

    GenerateJwtToken method return type and parameters changed

--- src/Libraries/BlogApp.Authentication/Interfaces/Services/ITokenService.cs
+++ src/Libraries/BlogApp.Authentication/Interfaces/Services/ITokenService.cs
@@ -5,7 +5,7 @@ using BlogApp.Entities.DbSets;
 namespace BlogApp.Authentication.Interfaces.Services;
 public interface ITokenService
 {
-    string GenerateJwtToken(User user);
+    Task<string> GenerateJwtToken(User user, bool rememberMe = false, CancellationToken cancellationToken = default);
     Task<RefreshToken> GenerateRefreshTokenAsync(User user, string ipAddress, CancellationToken cancellationToken = default);
     Task<RefreshToken?> GetActiveRefreshTokenAsync(User user, CancellationToken cancellationToken = default);
     Task<bool> UpdateRefreshTokenAsUsedAsync(string token, CancellationToken cancellationToken = default);

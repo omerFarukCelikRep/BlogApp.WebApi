8bfea3af7036bb567a40ee9ab32cac2cbad1f656
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-23 00:34:46 +0300

    RememberMe validation added

--- src/Libraries/BlogApp.Business/Validations/UserValidators/UserLoginValidator.cs
+++ src/Libraries/BlogApp.Business/Validations/UserValidators/UserLoginValidator.cs
@@ -6,20 +6,18 @@ public class UserLoginValidator : AbstractValidator<UserLoginRequestDto>
 {
     public UserLoginValidator()
     {
-        RuleFor(x => x.Email).NotNull()
-                             .WithMessage("Email Adresi Boş Geçilemez") //TODO: Magic string
-                             .NotEmpty()
-                             .WithMessage("Email Adresi Boş Geçilemez") //TODO: Magic string
-                             .EmailAddress()
-                             .WithMessage("Lütfen Geçerli Bir Email Adresi Giriniz") //TODO: Magic string 
-                             .MaximumLength(86)
-                             .WithMessage("Email Adresi 86 karakterden fazla olamaz"); //TODO: Magic string
+        RuleFor(x => x.Email)
+            .NotNull().WithMessage("Email Adresi Boş Geçilemez") //TODO: Magic string
+            .NotEmpty().WithMessage("Email Adresi Boş Geçilemez") //TODO: Magic string
+            .EmailAddress().WithMessage("Lütfen Geçerli Bir Email Adresi Giriniz") //TODO: Magic string
+            .MaximumLength(86).WithMessage("Email Adresi 86 karakterden fazla olamaz"); //TODO: Magic string
 
-        RuleFor(x => x.Password).NotNull()
-                                .WithMessage("Şifre Boş Geçilemez")
-                                .NotEmpty()
-                                .WithMessage("Şifre Boş Geçilemez")
-                                .MinimumLength(6) // TODO : Magic number
-                                .WithMessage("Şifre 6 karakterden uzun olmalıdır"); //TODO: Magic string
+        RuleFor(x => x.Password)
+            .NotNull().WithMessage("Şifre Boş Geçilemez") //TODO:Magic string
+            .NotEmpty().WithMessage("Şifre Boş Geçilemez") //TODO:Magic string
+            .MinimumLength(6).WithMessage("Şifre 6 karakterden uzun olmalıdır"); //TODO: Magic string // TODO : Magic number
+
+        RuleFor(x => x.RememberMe)
+            .NotNull().WithMessage("Remember Me Boş Geçilemez"); //TODO: Magic string
     }
 }

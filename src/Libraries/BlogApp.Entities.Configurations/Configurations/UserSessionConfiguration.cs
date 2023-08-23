280f5384b58c5bf85fb0505660568730a1660804
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-24 00:52:11 +0300

    UserSession configuration added

---
+++ src/Libraries/BlogApp.Entities.Configurations/Configurations/UserSessionConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace BlogApp.Entities.Configurations.Configurations;
+public class UserSessionConfiguration : BaseEntityConfiguration<UserSession>
+{
+    public const string TableName = "UserSessions";
+    public override void Configure(EntityTypeBuilder<UserSession> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable(TableName);
+
+        builder.HasIndex(x => x.UserId)
+               .IsUnique();
+
+        builder.Property(x => x.IpAddress)
+               .HasMaxLength(128);
+
+        builder.HasOne<User>()
+               .WithOne()
+               .HasForeignKey<UserSession>(x => x.UserId);
+    }
+}

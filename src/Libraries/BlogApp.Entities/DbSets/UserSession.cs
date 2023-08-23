fce2cd109fa5876f71855bc52212d659ae33bb39
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-24 00:51:59 +0300

    UserSession added

---
+++ src/Libraries/BlogApp.Entities/DbSets/UserSession.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogApp.Entities.DbSets;
+public class UserSession : BaseEntity
+{
+    public string Token { get; set; } = null!;
+    public DateTime ExpireDate { get; set; }
+    public string IpAddress { get; set; } = null!;
+
+    //Navigation Prop.
+    public Guid UserId { get; set; }
+}

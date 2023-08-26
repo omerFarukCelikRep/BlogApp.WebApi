921ccf4587de01da764dcbcc9d0fc14889298625
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-27 01:39:43 +0300

    constant added

--- src/Libraries/BlogApp.Core.Utilities/Authentication/JwtHelper.cs
+++ src/Libraries/BlogApp.Core.Utilities/Authentication/JwtHelper.cs
@@ -3,12 +3,13 @@
 namespace BlogApp.Core.Utilities.Authentication;
 public class JwtHelper
 {
+    private const string Id = "Id";
     private static readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler = new();
     public static string? GetUserIdByToken(string token)
     {
         var decodedToken = _jwtSecurityTokenHandler.ReadJwtToken(token);
 
-        return decodedToken.Claims.FirstOrDefault(x => x.Type == "Id")?.Value;
+        return decodedToken.Claims.FirstOrDefault(x => x.Type == Id)?.Value;
     }
 
     public static JwtSecurityToken? Read(string token)

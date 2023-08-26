a95b93f99568fd4048afdb3adb6adf36a8c29653
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-27 01:39:10 +0300

    IQueryablePaginateExtensions refactor

--- src/Libraries/BlogApp.Core/DataAccess/Extensions/IQueryablePaginateExtensions.cs
+++ src/Libraries/BlogApp.Core/DataAccess/Extensions/IQueryablePaginateExtensions.cs
@@ -7,22 +7,29 @@ public static class IQueryablePaginateExtensions
 {
     public static async Task<IPaginate<T>> ToPaginateAsync<T>(this IQueryable<T> source, int index, int size, CancellationToken cancellationToken = default)
     {
-        ArgumentException.ThrowIfNullOrEmpty(nameof(source));
+        ArgumentNullException.ThrowIfNull(source);
         EnsureInRange(index, size);
 
-        int count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
-        var items = await source.Skip(index * size).Take(index).ToListAsync(cancellationToken: cancellationToken);
+        int count = await source.CountAsync(cancellationToken)
+                                .ConfigureAwait(false);
+
+        var items = await source.Skip(index * size)
+                                .Take(index)
+                                .ToListAsync(cancellationToken: cancellationToken);
 
         return new Paginate<T>(items, index, size, count);
     }
 
     public static IPaginate<T> ToPaginate<T>(this IQueryable<T> source, int index = 0, int size = 10)
     {
-        ArgumentException.ThrowIfNullOrEmpty(nameof(source));
+        ArgumentNullException.ThrowIfNull(source);
+
         EnsureInRange(index, size);
 
         int count = source.Count();
-        var items = source.Skip(index * size).Take(index).ToList();
+        var items = source.Skip(index * size)
+                          .Take(index)
+                          .ToList();
 
         return new Paginate<T>(items, index, size, count);
     }
@@ -30,14 +37,9 @@ public static class IQueryablePaginateExtensions
     private static void EnsureInRange(int index, int size)
     {
         if (index < 0)
-        {
             throw new ArgumentOutOfRangeException(nameof(index));
-        }
 
         if (size < 0)
-        {
             throw new ArgumentOutOfRangeException(nameof(size));
-        }
     }
 }
-

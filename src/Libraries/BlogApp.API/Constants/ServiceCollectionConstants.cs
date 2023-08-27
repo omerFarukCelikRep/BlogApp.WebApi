ac77069d8135d7236633b6fed3493f451eaea35c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2023-08-27 14:52:20 +0300

    HealthCheckConstans added

--- src/Libraries/BlogApp.API/Constants/ServiceCollectionConstants.cs
+++ src/Libraries/BlogApp.API/Constants/ServiceCollectionConstants.cs
@@ -9,4 +9,10 @@ public struct ServiceCollectionConstants
         internal const int TimeWindow = 10;
         internal const int QueueLimit = 2;
     }
+
+    internal struct HealthCheckConstans
+    {
+        internal const string Name = "Database";
+        internal const int TimeoutAsSeconds = 10;
+    }
 }
